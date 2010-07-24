f3326a7c7a6f3b850f9b52c042324bf6b21e71c9
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2010-07-24 21:51:11 +0000

    -Descarte de cambios

--- proteoerp/system/application/views/view_ventanas.php
+++ proteoerp/system/application/views/view_ventanas.php
@@ -3,27 +3,8 @@
 	<meta http-equiv="Content-type" content="text/html; charset=<?=$this->config->item('charset'); ?>" />
 <title>Sistemas DataSIS</title>
 <?=style("ventanas.css");?>
-<style type="text/css">
-#caja   {width:785px;display: none;padding:5px;border:2px solid #D0D0D0;background-color:#FDF4E1;}
-#mostrar{display:block;width:785px;padding:5px;border:2px solid #D0D0D0;background-color:#F0F0F0;}
-</style>
 <?php if (isset($head))   echo $head;   ?>
 <?php if (isset($script)) echo $script; ?>
-
-<script type="text/javascript">
-$(function()
-{
-	$("#mostrar").click(function(event) {
-		Event.preventDefault();
-		$("#caja").slideToggle();
-	});
-	$("#caja a").click(function(event) {
-		Event.preventDefault();
-		$("#caja").slideUp();
-	});
-});
-</script>
-
 </head>
 <body>
 <div id='encabe'></div>
@@ -32,17 +13,6 @@ $(function()
 	<?php if (isset($title)) echo $title; ?>
 	
 	<table width="95%" border=0 align="center">
-	<?php if (isset($filtro)){  ?>
-		<tr>
-			<td></td>
-			<td>
-				<div class="littletableheader"><a href="#" id="mostrar" >Busqueda Avanzada</a></div>
-				<div style="display: none;" id="caja">
-					<?php echo $filtro; ?>
-				</div>
-			</td>				
-		</tr>
-	<?php }; ?>
 		<tr>
 			<td valign=top><?php if (isset($lista)) echo $lista; ?></td>
 			<td><?php if (isset($content)) echo $content; ?></td>
