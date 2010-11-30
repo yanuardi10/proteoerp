8649ca53d34d0f7d24b79b4c61bf1220d1dc58b9
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2010-11-30 02:53:08 +0000

    -Arreglos parciales en el pase de las facturas como compras

--- proteoerp/system/application/controllers/rpcserver.php
+++ proteoerp/system/application/controllers/rpcserver.php
@@ -3,11 +3,14 @@ class Rpcserver extends Controller {
 
 	function index(){
 		$this->load->library('xmlrpc');
+		$this->xmlrpc->xmlrpc_defencoding=$this->config->item('charset');
 		$this->load->library('xmlrpcs');
-		
+		$this->xmlrpcs->xmlrpc_defencoding=$this->config->item('charset');
+
 		$config['functions']['sprecios'] = array('function' => 'Rpcserver.precio_supermer');
 		$config['functions']['ttiket']   = array('function' => 'Rpcserver.traer_tiket');
-		
+		$config['functions']['cea']      = array('function' => 'Rpcserver.ComprasEmpresasAsociadas');
+
 		$this->xmlrpcs->initialize($config);
 		$this->xmlrpcs->serve();
 	}
@@ -42,13 +45,16 @@ class Rpcserver extends Controller {
 	function traer_tiket($request){
 		$parameters = $request->output_parameters();
 		$fechad=$parameters['0'];
-		
+
 		$query = $this->db->query("SELECT id,padre,pertenece,prioridad,usuario,contenido,estampa,actualizado,estado FROM tiket WHERE estampa>'$fechad' AND estampa<=NOW() AND usuario<>'TRANF'");
 		//$query = $this->db->query("SELECT id,padre,pertenece,prioridad,usuario,contenido,estampa,actualizado,estado FROM tiket LIMIT 3");
-		
+
 		$tiket=array();
 		if ($query->num_rows() > 0){ 
 			foreach ($query->result_array() as $row){
+				foreach($row AS $ind=>$val){
+					$row[$ind]=base64_encode($val);
+				}
 				$tiket[] = serialize($row);
 			}
 		}else{
@@ -57,5 +63,42 @@ class Rpcserver extends Controller {
 		$response = array($tiket,'struct');
 		return $this->xmlrpc->send_response($response);
 	}
-}    
-?>
+
+	function ComprasEmpresasAsociadas($request){
+		$parameters = $request->output_parameters();
+
+		$ult_ref=$parameters['0'];
+		$cod_cli=$parameters['1'];
+		$usr    =$parameters['1'];
+		$pwd    =$parameters['2'];
+
+		$mSQL="SELECT numero,fecha,TRIM(nfiscal) AS nfiscal FROM sfac WHERE cod_cli=? AND numero > ? AND tipo_doc='F' LIMIT 5";
+		$query = $this->db->query($mSQL,array($cod_cli,$ult_ref));
+
+		$compras=array();
+		if ($query->num_rows() > 0){ 
+			foreach ($query->result_array() as $row){
+				$mmSQL="SELECT TRIM(codigoa) AS codigoa,TRIM(desca) AS desca,cana,preca,tota,iva FROM sitems WHERE numa=? AND tipoa='F'";
+				$qquery = $this->db->query($mmSQL,array($row['numero']));
+
+				foreach($row AS $ind=>$val){
+					$row[$ind]=base64_encode($val);
+				}
+				$compras[] = serialize($row);
+
+				$it=array();
+				foreach ($qquery->result_array() as $rrow){
+					foreach($rrow AS $ind=>$val){
+						$rrow[$ind]=base64_encode($val);
+					}
+					$it[]=$rrow;
+				}
+
+				$compras[] = serialize($it);
+			}
+		}
+
+		$response = array($compras,'struct');
+		return $this->xmlrpc->send_response($response);
+	}
+}
