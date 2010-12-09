effc93f093a1d96780f6b24a825a7ceee7c6536c
jud.prog <jud.prog@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2010-12-09 14:30:02 +0000

    agregado filter y edit para la tabla b2b_config en b2b.php

--- trunk/proteoerp/system/application/controllers/sincro/b2b.php
+++ trunk/proteoerp/system/application/controllers/sincro/b2b.php
@@ -1,5 +1,6 @@
 <?php
-class b2b extends Controller {
+require_once(BASEPATH.'application/controllers/validaciones.php');
+class b2b extends validaciones {
 
 	function b2b(){
 		parent::Controller();
@@ -9,10 +10,201 @@ class b2b extends Controller {
 
 	function index(){
 		//Aqui va el filtered grid
+		$this->rapyd->load('datafilter','datagrid');
+		$this->rapyd->uri->keep_persistence();
+
+		$mSPRV=array(
+			'tabla'   =>'sprv',
+			'columnas'=>array(
+				'proveed' =>'C&oacute;digo',
+				'nombre'=>'Nombre',
+				'contacto'=>'Contacto'),
+			'filtro'  =>array('proveed'=>'C&oacute;digo','nombre'=>'Nombre'),
+			'retornar'=>array('proveed'=>'proveed'),
+			'titulo'  =>'Buscar Proveedor');
+		$bSPRV=$this->datasis->modbus($mSPRV);
+		
+		$mGRUP=array(
+			'tabla'   =>'grup',
+			'columnas'=>array(
+				'grupo' =>'Grupo',
+				'nom_grup'=>'Nombre'),
+			'filtro'  =>array('grupo'=>'Grupo','nom_grup'=>'Nombre'),
+			'retornar'=>array('grupo'=>'grupo'),
+			'titulo'  =>'Buscar Grupo');
+		$bGRUP=$this->datasis->modbus($mGRUP);
+
+
+		$filter = new DataFilter('Filtro de b2b');
+		$filter->db->select("a.id,a.proveed,a.usuario,a.depo as depo,a.tipo,a.url,a.grupo,
+					b.nombre,c.ubides");
+		$filter->db->from("b2b_config as a");
+		$filter->db->join("sprv as b","b.proveed=a.proveed COLLATE latin1_swedish_ci","left");
+		$filter->db->join("caub as c","c.ubica=a.depo COLLATE latin1_swedish_ci","left");
+		
+
+		$filter->proveed = new inputField('Proveedor', 'proveed');
+		$filter->proveed->append($bSPRV);
+		$filter->proveed->size=25;
+
+		$filter->depo = new dropdownField('Almacen','depo');
+		$filter->depo->option("","Seleccione un almacen");
+		$filter->depo->options("SELECT ubica, ubides FROM caub ORDER BY ubica");
+
+		$filter->tipo = new dropdownField('Tipo','tipo');
+		$filter->tipo->option("","Selecione un tipo");
+		$filter->tipo->option("I","Inventario");
+		$filter->tipo->option("G","Gastos");
+
+		$filter->grupo = new inputField('Grupo', 'grupo');
+		$filter->grupo->append($bGRUP);
+		$filter->grupo->size=25;
+		
+		$filter->buttons('reset','search');
+		$filter->build();
+
+		$link=anchor('/sincro/b2b/dataedit/show/<#id#>','<#id#>');
+		$grid = new DataGrid('b2b');
+		$grid->use_function('dropdown');
+		$grid->order_by('id','asc');
+		$grid->per_page = 15;
+
+		$grid->column_orderby('Id'   ,$link  ,'id');
+		$grid->column_orderby('Proveedor'   ,'nombre'     ,'proveed');
+		$grid->column_orderby('Url', 'url' ,'url');
+		$grid->column_orderby('Usuario', 'usuario' ,'usuario');
+		$grid->column_orderby('Clave',"clave",'clave');
+		$grid->column_orderby('Tipo'        ,'tipo','tipo');
+		$grid->column_orderby('Almacen'        ,'ubides','depo');
+		$grid->column_orderby('Grupo'        ,'grupo','grupo');
+
+		$grid->add('sincro/b2b/dataedit/create');
+		$grid->build();
+
+		
+		$data['content'] =$filter->output. $grid->output;
+		$data['title']   = '<h1>B2B</h1>';
+		$data['head']    = $this->rapyd->get_head();
+		$this->load->view('view_ventanas', $data);
 	}
 
-	function dataedit{
+	function dataedit(){
 		//aqui va el dataedit
+		$this->rapyd->load('dataedit');
+
+		$mGRUP=array(
+			'tabla'   =>'grup',
+			'columnas'=>array(
+				'grupo' =>'Grupo',
+				'nom_grup'=>'Nombre'),
+			'filtro'  =>array('grupo'=>'Grupo','nom_grup'=>'Nombre'),
+			'retornar'=>array('grupo'=>'grupo'),
+			'titulo'  =>'Buscar Grupo');
+		$bGRUP=$this->datasis->modbus($mGRUP);
+
+		$mSPRV=array(
+			'tabla'   =>'sprv',
+			'columnas'=>array(
+				'proveed' =>'C&oacute;digo',
+				'nombre'=>'Nombre',
+				'contacto'=>'Contacto'),
+			'filtro'  =>array('proveed'=>'C&oacute;digo','nombre'=>'Nombre'),
+			'retornar'=>array('proveed'=>'proveed'),
+			'titulo'  =>'Buscar Proveedor');
+		$bSPRV=$this->datasis->modbus($mSPRV);
+
+		$script='
+		<script language="javascript" type="text/javascript">
+		$(function(){
+				$(".inputnum").numeric(".");
+		});
+		</script>';
+
+		$edit = new DataEdit('B2B', 'b2b_config');
+		$edit->back_url = site_url('sincro/b2b/index/');
+		
+		$edit->id = new inputField('id', 'id');
+		$edit->id->size      =  15;
+		$edit->id->maxlength =  15;
+		$edit->id->rule      = 'required';
+		
+		$edit->proveed = new inputField('Proveedor', 'proveed');
+		$edit->proveed->size      =  15;
+		$edit->proveed->maxlength =  15;
+		$edit->proveed->rule      = 'required';
+		$edit->proveed->append($bSPRV);
+		
+		$edit->url = new inputField('Url', 'url');
+		$edit->url->size      =  50;
+		$edit->url->maxlength =  50;
+//		$edit->url->rule      = 'required';
+
+		$edit->usuario = new inputField('Usuario', 'usuario');
+		$edit->usuario->size      =  20;
+		$edit->usuario->maxlength =  20;
+		$edit->usuario->rule      = 'required';
+		
+		$edit->clave = new inputField('Clave', 'clave');
+		$edit->clave->size      =  10;
+		$edit->clave->maxlength =  10;
+		$edit->clave->rule      = 'required';
+		
+		$edit->tipo = new dropdownField("Tipo","tipo");
+		$edit->tipo->option("","Seleccione un tipo");
+		$edit->tipo->option("I","Inventario");
+		$edit->tipo->option("G","Gastos");
+		$edit->tipo->style="50px";
+		
+		$edit->depo = new dropdownField("Almacen","depo");
+		$edit->depo->option("","Seleccione un almacen");
+		$edit->depo->options("SELECT ubica,ubides FROM caub");
+		$edit->depo->style="250px";
+		$edit->depo->rule="required";
+		
+		$edit->margen1 = new inputField('Margen1', 'margen1');
+		$edit->margen1->size      = 15;
+		$edit->margen1->maxlength = 15;
+		$edit->margen1->css_class = 'inputnum';
+		$edit->margen1->rule      = 'callback_chporcent';
+		
+		$edit->margen2 = new inputField('Margen2', 'margen2');
+		$edit->margen2->size      = 15;
+		$edit->margen2->maxlength = 15;
+		$edit->margen2->css_class = 'inputnum';
+		$edit->margen2->rule      = 'callback_chporcent';
+		
+		$edit->margen3 = new inputField('Margen3', 'margen3');
+		$edit->margen3->size      = 15;
+		$edit->margen3->maxlength = 15;
+		$edit->margen3->css_class = 'inputnum';
+		$edit->margen3->rule      = 'callback_chporcent';
+		
+		$edit->margen4 = new inputField('Margen4', 'margen4');
+		$edit->margen4->size      = 15;
+		$edit->margen4->maxlength = 15;
+		$edit->margen4->css_class = 'inputnum';
+		$edit->margen4->rule      = 'callback_chporcent';
+		
+		$edit->margen5 = new inputField('Margen5(Supermercado)', 'margen5');
+		$edit->margen5->size      = 15;
+		$edit->margen5->maxlength = 15;
+		$edit->margen5->css_class = 'inputnum';
+		$edit->margen5->rule      = 'callback_chporcent';
+		
+		$edit->grupo = new inputField('Grupo', 'grupo');
+		$edit->grupo->size      =  10;
+		$edit->grupo->maxlength =  6;
+		$edit->grupo->rule      = 'required';
+		$edit->grupo->append($bGRUP);
+		
+		$edit->buttons('modify', 'save','undo', 'back');
+		$edit->build();
+
+		$data['content'] = $edit->output;
+		$data['head']    = script('jquery.js').script('jquery-ui.js').script("plugins/jquery.numeric.pack.js").script('plugins/jquery.meiomask.js').style('vino/jquery-ui.css').$this->rapyd->get_head().$script;
+		$data['title']   = '<h1>Editar b2b</h1>';
+		$this->load->view('view_ventanas', $data);
+		
 	}
 
 	function cargar(){
@@ -53,7 +245,7 @@ class b2b extends Controller {
 					if(!$maestro) continue;
 					foreach($arr AS $in => $aarr){
 						foreach($aarr AS $i=>$val)
-							$arr[$in][$i]=base64_decode($val);
+						$arr[$in][$i]=base64_decode($val);
 						$ddata['id_scst']  = $id_scst;
 						$ddata['proveed']  = $proveed;
 						$ddata['fecha']    = $data['fecha'];
@@ -83,7 +275,7 @@ class b2b extends Controller {
 					//print_r($arr);
 				}else{
 					foreach($arr AS $in => $val)
-						$arr[$in]=base64_decode($val);
+					$arr[$in]=base64_decode($val);
 
 					//$control=$this->datasis->fprox_numero('nscst');
 					//$transac=$this->datasis->fprox_numero('ntransac');
@@ -163,7 +355,7 @@ class b2b extends Controller {
 				$row['montotot'] =$tt['montotot'];
 				$row['montoiva'] =$tt['montoiva'];
 				$row['montonet'] =$tt['montonet'];
-				
+
 				$mSQL=$this->db->insert_string('scst',$row);
 				//echo $mSQL;
 				$rt=$this->db->simple_query($mSQL);
@@ -177,13 +369,13 @@ class b2b extends Controller {
 			if(!$rt){
 				memowrite($mSQL,'B2B');
 			}
-		
+
 		}
 	}
 
 	function _cargagasto(){
-		
-		
+
+
 	}
 
 	function instala(){
@@ -217,42 +409,42 @@ class b2b extends Controller {
 
 
 /*if ($query->num_rows()==1){
-	$lcontrol=$this->datasis->fprox_numero('nscst');
-	$transac =$this->datasis->fprox_numero('ntransac');
+ $lcontrol=$this->datasis->fprox_numero('nscst');
+ $transac =$this->datasis->fprox_numero('ntransac');
 
-	$row=$query->row_array();
-	$row['control']=$lcontrol;
-	$row['transac']=$transac;
-	$row['nfiscal']=$nfiscal;
-	$row['depo']   =$almacen;
-	$row['vence']  =$vence;
-	unset($row['pcontrol']);
+ $row=$query->row_array();
+ $row['control']=$lcontrol;
+ $row['transac']=$transac;
+ $row['nfiscal']=$nfiscal;
+ $row['depo']   =$almacen;
+ $row['vence']  =$vence;
+ unset($row['pcontrol']);
 
-	$mSQL[]=$this->db->insert_string('scst', $row);
+ $mSQL[]=$this->db->insert_string('scst', $row);
 
-	$itquery = $farmaxDB->query("SELECT * FROM itscst WHERE control=$control");
-	foreach ($itquery->result_array() as $itrow){
-		$itrow['control']=$lcontrol;
-		unset($itrow['id']);
-		$mSQL[]=$this->db->insert_string('itscst', $itrow);
-	}
-	foreach($mSQL AS $sql){
-		$rt=$this->db->simple_query($sql);
-		if(!$rt){ memowrite('scstfarma',$sql);}
-	}
-	$sql="UPDATE scst SET pcontrol='${lcontrol}' WHERE control=$control";
-	$rt=$farmaxDB->simple_query($sql);
-	if(!$rt) memowrite('farmaejec',$sql);
+ $itquery = $farmaxDB->query("SELECT * FROM itscst WHERE control=$control");
+ foreach ($itquery->result_array() as $itrow){
+ $itrow['control']=$lcontrol;
+ unset($itrow['id']);
+ $mSQL[]=$this->db->insert_string('itscst', $itrow);
+ }
+ foreach($mSQL AS $sql){
+ $rt=$this->db->simple_query($sql);
+ if(!$rt){ memowrite('scstfarma',$sql);}
+ }
+ $sql="UPDATE scst SET pcontrol='${lcontrol}' WHERE control=$control";
+ $rt=$farmaxDB->simple_query($sql);
+ if(!$rt) memowrite('farmaejec',$sql);
 
-	$mSQL="UPDATE 
-	  ${localdb}.itscst AS a
-	  JOIN ${localdb}.farmaxasig AS b ON a.codigo=b.barras AND a.proveed=b.proveed
-	  SET a.codigo=b.abarras
-	WHERE a.control='$lcontrol'";
-	$rt=$this->db->simple_query($mSQL);
-	if(!$rt){ memowrite('farmaejec1',$sql);}
+ $mSQL="UPDATE
+ ${localdb}.itscst AS a
+ JOIN ${localdb}.farmaxasig AS b ON a.codigo=b.barras AND a.proveed=b.proveed
+ SET a.codigo=b.abarras
+ WHERE a.control='$lcontrol'";
+ $rt=$this->db->simple_query($mSQL);
+ if(!$rt){ memowrite('farmaejec1',$sql);}
 
-	$retorna='Compra guardada con el control '.anchor("compras/scst/dataedit/show/$lcontrol",$lcontrol);
-}else{
-	$retorna="Al parecer la factura fue ya pasada";
-}*/
+ $retorna='Compra guardada con el control '.anchor("compras/scst/dataedit/show/$lcontrol",$lcontrol);
+ }else{
+ $retorna="Al parecer la factura fue ya pasada";
+ }*/
