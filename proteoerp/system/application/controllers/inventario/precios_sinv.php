dada23922021be13d38f32e70fe90342f033ae10
jud.prog <jud.prog@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2010-12-13 21:19:48 +0000

    modificado modulo precios_sinv

--- proteoerp/system/application/controllers/inventario/precios_sinv.php
+++ proteoerp/system/application/controllers/inventario/precios_sinv.php
@@ -1,10 +1,11 @@
 <?php
 require_once(BASEPATH.'application/controllers/validaciones.php');
-class precios_sinv extends validaciones {
+class precios_sinv extends validaciones{
 
 	function precios_sinv(){
 		parent::Controller();
 		$this->load->library("rapyd");
+		$this->load->library("pi18n");
 		//$this->datasis->modulo_id(302,1);
 		//define ("THISFILE",   APPPATH."controllers/inventario/". $this->uri->segment(2).EXT);
 	}
@@ -16,52 +17,144 @@ class precios_sinv extends validaciones {
 
 	function precios(){
 		$this->rapyd->load("dataedit","dataform");
+		$script='
+		<script language="javascript" type="text/javascript">
+		$(function(){
+				$(".inputnum").numeric(".");
+		});
+		</script>';
 
+		$mSINV=array(
+			'tabla'   =>'sinv',
+			'columnas'=>array(
+			'codigo' =>'C&oacute;odigo','barras'=>'Barras','alterno'=>'Alterno',
+			'descrip'=>'Descripci&oacute;n'),
+			'filtro'  =>array('codigo'=>'C&oacute;digo','barras'=>'Barras','alterno'=>'Alterno','descrip'=>'Descripci&oacute;n'),
+			'retornar'=>array('codigo'=>'codigo','barras'=>'barras','alterno'=>'alterno'),
+			'titulo'  =>'Buscar Producto');
+		$bSINV=$this->datasis->modbus($mSINV);
+
+
+		$mSINVP=array(
+			'tabla'   =>'sinvpromo',
+			'columnas'=>array(
+			'codigo' =>'C&oacute;odigo','margen'=>'Margen','cantidad'=>'Cantidad'),
+			'filtro'  =>array('codigo'=>'C&oacute;digo'),
+			'retornar'=>array('codigo'=>'promo'),
+			'titulo'  =>'Buscar Codigo Promocion');
+		$bSINVP=$this->datasis->modbus($mSINVP);
+		$pr='
+			$(document).ready(function(){
+				$(".button").click(function(){
+						
+						if($("#codigo").val() == "" && $("#barras").val() == ""  && $("#alterno").val() == ""  && $("#promo").val() == ""){
+							alert("Debe ingresar al menos un codigo de producto");
+							return false;
+						}
+					}
+					);
+			});
+			
+		';
 
 		$form = new DataForm("inventario/precios_sinv/precios/modifica");
-
-		$form->codigo = new inputField2("Ingrese producto a consultar", "codigo");
+		$form->script($pr);
+		$form->codigo = new inputField2("Consultar por C&oacute;digo de Producto", "codigo");
 		$form->codigo->size = 15;
 		$form->codigo->maxlength=15;
-		$form->codigo->rule = 'required';
+		$form->codigo->append($bSINV);
+		$form->codigo->rule = 'trim';
+
+		$form->barras = new inputField2("Consultar por C&oacute;digo de Barras", "barras");
+		$form->barras->size = 15;
+		$form->barras->maxlength=15;
+		$form->barras->css_class = 'inputnum';
+		$form->barras->append($bSINV);
+		$form->barras->rule = 'trim';
+
+		$form->alterno = new inputField2("Consultar por C&oacute;digo Alterno", "alterno");
+		$form->alterno->size = 15;
+		$form->alterno->maxlength=15;
+		$form->alterno->rule = 'trim';
+		$form->alterno->append($bSINV);
+
+		$form->promo = new inputField2("Consultar por C&oacute;digo de Promosi&oacute;n", "promo");
+		$form->promo->size = 15;
+		$form->promo->maxlength=15;
+		$form->promo->rule = 'trim';
+		$form->promo->append($bSINVP);
 
 		$form->submit("btnsubmit","CONSULTAR");
 		$form->build_form();
+		$msj="";
 		if ($form->on_success()){
-			$codigo=trim($_POST['codigo']);
-			redirect("inventario/precios_sinv/verifica/$codigo");
+			set_time_limit(600);
+			$codigo= $this->input->post("codigo");
+			$barras= $this->input->post("barras");
+			$alterno= $this->input->post("alterno");
+			$promo= $this->input->post("promo");
+			$campo="";
+			$valor="";
+			$band=1;
+
+			if($codigo !="" && $band==1){
+				$resul1=$this->db->query("SELECT count(*) as cant,id from sinv	where codigo='$codigo' ");
+				$row1=$resul1->row();
+				if($row1->cant > 0){
+					$band=0;
+					redirect("inventario/precios_sinv/dataedit/modify/$row1->id");
+				}
+			}
+
+			if($barras != "" && $band==1){
+				$resul2=$this->db->query("SELECT count(*) as cant,id from sinv	where barras='$barras'" );
+				$row2=$resul2->row();
+				if($row2->cant > 0 ){
+					redirect("inventario/precios_sinv/dataedit/modify/$row2->id");
+					$band=0;
+				}
+			}
+			if($alterno !="" && $band==1){
+				$resul3=$this->db->query("SELECT count(*) as cant,id from sinv	where alterno='$alterno'" );
+				$row3=$resul3->row();
+				if($row3->cant > 0 ){
+					redirect("inventario/precios_sinv/dataedit/modify/$row3->id");
+					$band=0;
+				}
+			}
+			if($promo !="" && $band==1){
+				$resul3=$this->db->query("SELECT count(*) as cant,a.codigo as codigo,b.id as id
+											from sinvpromo as a
+											join sinv as b on b.codigo=a.codigo 
+											where a.codigo='$promo'" );
+				$row3=$resul3->row();
+				if($row3->cant > 0 ){
+					redirect("inventario/precios_sinv/dataedit/modify/$row3->id");
+					$band=0;
+				}
+			}
+			$msj.= "NO se encontro el producto  <br>";
+			$atras=site_url('inventario/precios_sinv/precios');
+			$data['smenu']="<a href=".$atras.">ATRAS</a>";
+
 		}
 
-		$data['content'] =$form->output;
-		$data['head']    = script('jquery.js').script('jquery-ui.js').script("plugins/jquery.numeric.pack.js").script('plugins/jquery.meiomask.js').style('vino/jquery-ui.css').$this->rapyd->get_head();
+
+		$data['content'] =$form->output.$msj;
+		$data['head']    = script('jquery.js').script('jquery-ui.js').script("plugins/jquery.numeric.pack.js").script('plugins/jquery.meiomask.js').style('vino/jquery-ui.css').$this->rapyd->get_head().$script;
 
 		$data['title']   = '<h1>Cambiar Precios</h1>';
 		$this->load->view('view_ventanas', $data);
 	}
 
-	function campo($cod=""){
-		$resul1=$this->db->query("SELECT count(*) as cant from sinv
-								where codigo='$cod' ");
-		$row1=$resul1->row();
-		if($row1->cant > 0) return 1;
+	function dataedit(){
+		$this->pi18n->cargar('sinv','dataedit');
+		$this->rapyd->load("dataedit");
+		
+		$edit = new DataEdit('Cambios de precios',"sinv");
+		$edit->pre_process('update' ,'_pre_update');
+		$edit->post_process('update','_modifica');
 
-		$resul2=$this->db->query("SELECT count(*) as cant from sinv
-								where barras='$cod'" );
-		$row2=$resul2->row();
-		if($row2->cant > 0 ) return 2;
-
-		$resul3=$this->db->query("SELECT count(*) as cant from sinv
-								where alterno='$cod'" );
-		$row3=$resul3->row();
-		if($row3->cant > 0 ) return 3;
-
-		return 0;
-
-
-	}
-
-	function verifica($cod=''){
-		//		$this->rapyd->load("dataedit","dataform");
 		$script='
                 <script language="javascript" type="text/javascript">
                 $(function(){
@@ -81,127 +174,84 @@ class precios_sinv extends validaciones {
 				} 
                 </script>
                 ';
-		$out="";
-		$camp=$this->campo($cod);
-		$js = 'onClick="return valida()"';
-		if($camp > 0){
-			$precio1 = array('name'      => 'precio1','id'        => 'precio1',
-			'value'     => '','size'      => '16','class'=>'inputnum',
-			);
-			$precio2 = array('name'      => 'precio2','id'        => 'precio2',
-			'value'     => '','size'      => '16','class'=>'inputnum',
-			);
-			$precio3 = array('name'      => 'precio3','id'        => 'precio3',
-			'value'     => '','size'      => '16','class'=>'inputnum',
-			);
-			$precio4 = array('name'      => 'precio4','id'        => 'precio4',
-			'value'     => '','size'      => '16','class'=>'inputnum',
-			);
-			$out  = '<h1>'.form_open('inventario/precios_sinv/modifica');
-			$out .= "Introduzca Nuevos Precio:<br> </h1>";
-			$out .= "Precio1   ".form_input($precio1)."<br>";
-			$out .= "Precio2   ".form_input($precio2)."<br>";
-			$out .= "Precio3   ".form_input($precio3)."<br>";
-			$out .= "Precio4   ".form_input($precio4)."<br>";
-			$out .= form_hidden('tabla', 'sinv');
-			$out .= form_hidden('campo', $camp);
-			$out .= form_hidden('codigo', $cod);
-			$out .= form_submit('btnsubmit','Modificar',$js).form_close().'</h1>';
 
-		}else{
-			$resul2=$this->db->query("SELECT count(*) as cant,codigo from sinvpromo where codigo='$cod'");
-			$row2=$resul2->row();
-			if($row2->cant > 0){
-				$precio1 = array('name'      => 'precio1','id'        => 'precio1',
-			'value'     => '','size'      => '16',
-				);
-				$precio2 = array('name'      => 'precio2','id'        => 'precio2',
-			'value'     => '','size'      => '16',
-				);
-				$precio3 = array('name'      => 'precio3','id'        => 'precio3',
-			'value'     => '','size'      => '16',
-				);
-				$precio4 = array('name'      => 'precio4','id'        => 'precio4',
-			'value'     => '','size'      => '16',
-				);
-				$out  = '<h1>'.form_open('inventario/precios_sinv/modifica');
-				$out .= "Introduzca Nuevos Precio:<br></h1> ";
-				$out .= "Precio1   ".form_input($precio1)."<br>";
-				$out .= "Precio2   ".form_input($precio2)."<br>";
-				$out .= "Precio3   ".form_input($precio3)."<br>";
-				$out .= "Precio4   ".form_input($precio4)."<br>";
-				$out .= form_hidden('tabla', 'sinvpromo');
-				$out .= form_hidden('campo', '1');
-				$out .= form_hidden('codigo', $cod);
-				$out .= form_submit('btnsubmit','Modificar').form_close().'</h1>';
-
-			}else{
-				$out .= "NO se encontro el producto  $cod<br>";
-				$atras=site_url('inventario/precios_sinv/precios');
-				$data['smenu']="<a href=".$atras.">ATRAS</a>";
-			}
+		for($i=1;$i<5;$i++){
+			$obj='precio'.$i;
+			$edit->$obj = new inputField('Precio '.$i, $obj);
+			$edit->$obj->css_class='inputnum';
+			$edit->$obj->rule ='numeric|trim|required';
+			$edit->$obj->size = 10;
+			$edit->$obj->maxlength=10;
 		}
-		$data['content'] =$out;
+
+		$edit->buttons('modify','save');
+		$edit->build();
+
+		$atras=site_url('inventario/precios_sinv/precios');
+		$data['smenu']="<a href=".$atras.">ATRAS</a>";
+		$data['content'] =$edit->output;
 		$data['head']    = script('jquery.js').script('jquery-ui.js').script("plugins/jquery.numeric.pack.js").script('plugins/jquery.meiomask.js').style('vino/jquery-ui.css').$this->rapyd->get_head().$script;
 
 		$data['title']   = '<h1>Cambiar Precio</h1>';
 		$this->load->view('view_ventanas', $data);
+
+
 	}
 
-	function modifica(){
+	function _modifica($do){
 
-		$tabla= $this->input->post('tabla');
-		$camp=$this->input->post('campo');
-		$codigo=$this->input->post('codigo');
-		$precio1=$this->input->post('precio1');
-		$precio2=$this->input->post('precio2');
-		$precio3=$this->input->post('precio3');
-		$precio4=$this->input->post('precio4');
-		
-		$cod=$this->input->post('codigo');
-		$campo="";
-		switch ($camp){
-			case 1:$campo ='codigo';
-			break;
-			case 2:$campo ='barras';
-			break;
-			case 3:$campo ='alterno';
-			break;
-		}
+		$codigo=$do->get('codigo');
+		$precio1=$do->get('precio1');
+		$precio2=$do->get('precio2');
+		$precio3=$do->get('precio3');
+		$precio4=$do->get('precio4');
+
 		//base=precio*100/(100+iva)
 		//margen=100-(costo*100/base)
-		$query1="UPDATE sinv SET 
-				precio1=$precio1,
-				precio2=$precio2,
-				precio3=$precio3,
-				precio4=$precio4
-        WHERE $campo='$codigo' ";
-		$this->db->query($query1);
-		
+//		$query1="UPDATE sinv SET
+//				precio1=$precio1,
+//				precio2=$precio2,
+//				precio3=$precio3,
+//				precio4=$precio4
+//        WHERE $campo='$codigo' ";
+//		$this->db->query($query1);
+
 		$query2="UPDATE sinv SET
 				base1=precio1*100/(100+iva),
                 base2=precio2*100/(100+iva),
                 base3=precio3*100/(100+iva),
                 base4=precio4*100/(100+iva)
-                WHERE $campo='$codigo'
+                WHERE codigo='$codigo'
                 ";
 		$this->db->query($query2);
-		
+
 		$query3="UPDATE sinv SET
                 margen1=100-(pond*100/base1),
                 margen2=100-(pond*100/base2),
                 margen3=100-(pond*100/base3),
                 margen4=100-(pond*100/base4)
-                WHERE $campo='$codigo'
+                WHERE codigo='$codigo'
                 ";
 		$this->db->query($query3);
 
-		$atras=site_url('inventario/precios_sinv/precios');
-		$data['content'] ="MODIFICADO PRECIOS";
-		$data['smenu']="<a href=".$atras.">ATRAS</a>";
-		$data['title']   = "<h1>Actualizaci&oacute;n de Inventario</h1>";
-		$data["head"]    = script("jquery.pack.js").$this->rapyd->get_head();
-		$this->load->view('view_ventanas', $data);
+	}
+
+	function _pre_update($do){
+//		print("<pre>");
+//		print_R($do);
+		
+		$p1=$do->get('precio1');
+		$p2=$do->get('precio2');
+		$p3=$do->get('precio3');
+		$p4=$do->get('precio4');
+		$do->error_message_ar['pre_upd'] = 'Los precios deben cumplir con:<br> Precio 1 > Precio 2 > Precio 3 > Precio 4';
+		if($p1>$p2 && $p2>$p3 && $p3>$p4){
+			
+			return true;
+		}else{
+			 
+			return false;
+		}
 	}
 }
 ?>
