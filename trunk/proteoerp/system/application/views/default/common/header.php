c03b059358623e91eb3bd1c92441269f5eada27e
datasql <datasql@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-02-09 14:24:17 +0000

    Implementacion de masonry en Proteo

--- trunk/proteoerp/system/application/views/default/common/header.php
+++ trunk/proteoerp/system/application/views/default/common/header.php
@@ -1,2 +1,19 @@
-	<div id="header"><?=$idus ?></div>
+	<div id="header">
+	<table align="center" border=0 width="99%" cellpadding=0 cellspacing=0>
+	<tr>
+	    <td width="170px">&nbsp</td>
+	    <td align='center'><h2><?=$this->datasis->traevalor("TITULO1")  ?></h2></td>
+	    <td></td>
+	</tr>
+	<tr>
+	    <td>&nbsp</td>
+	    <td align='center'><p class="miniblanco1"><?=$this->datasis->traevalor("TITULO2")."<br>".$this->datasis->traevalor("TITULO3")."<br>RIF ".$this->datasis->traevalor("RIF")?><p></td>
+	    <td><?=$idus ?></td>
+	</tr>
+
+	
+	</table>
+	
+	
+	</div>
 	<?php echo $menu ?>
