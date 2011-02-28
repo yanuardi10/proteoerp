41b03d76f9cfad3d7f5b41048006842790c719ab
datasql <datasql@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-02-28 03:14:53 +0000

    Filtros en Ventanas

--- proteoerp/system/application/views/view_ventanas.php
+++ proteoerp/system/application/views/view_ventanas.php
@@ -1,18 +1,47 @@
 <html>
 <head>
-	<meta http-equiv="Content-type" content="text/html; charset=<?=$this->config->item('charset'); ?>" >
-	<title>ProteoERP<?php if(isset($title)) echo ':'.preg_replace('/<[^>]*>/', '', $title); ?></title>
-	<?=style("ventanas.css");?>
-	<?php if (isset($head))   echo $head;   ?>
-	<?php if (isset($script)) echo $script; ?>
+<meta http-equiv="Content-type" content="text/html; charset=<?=$this->config->item('charset'); ?>" >
+<title>ProteoERP<?php if(isset($title)) echo ':'.preg_replace('/<[^>]*>/', '', $title); ?></title>
+<?=style("ventanas.css");?>
+<?php if(!isset($tabla))   $tabla=''; ?>
+<style type="text/css">
+<?php if($tabla=="") {?>
+#cajafiltro {width: 100%;display: block;padding: 5px;border: 2px solid #D0D0D0;background-color: #ECFCFC;}
+<?php } else{?>
+#cajafiltro {width: 100%;display: block;padding: 5px;border: 2px solid #D0D0D1;background-color: #FDFDFD;}	
+<?php }?>
+#mostrafiltro {display: block;width: 100%;padding: 5px;border: 2px solid #D0D0D0;background-color: #D0D0D0;}
+</style>
+
+<?=script("jquery.js") ?>
+
+<?php if (isset($head))   echo $head;   ?>
+<?php if (isset($script)) echo $script; ?>
+
+<script type="text/javascript">
+$(function(){
+	$("#mostrafiltro").click(function(event) {
+		estado = $("#cajafiltro").css("display");
+		if(estado == "none"){ $("#cajafiltro").show();
+		}else{ $("#cajafiltro").hide();		}
+	});
+	$("#cajafiltro").hide();
+});
+</script>
 </head>
 <body>
-<div id='encabe'></div>
+<div id='encabe'><?php if (isset($title)) echo $title; ?></div>
 <?php if(isset($smenu)) echo '<div id="smenu">'.$smenu.'</div>'; ?>
 <div id='contenido'>
-	<?php if (isset($title)) echo $title; ?>
-
 	<table width="95%" border=0 align="center">
+		<tr>
+			<td></td>
+			<td><?php if (isset($filtro)) { ?>
+			<div><a href="#" id="mostrafiltro">Buscar y Filtrar <?=image("visible_16.png", "#", array("border"=>"none")); ?></a>
+			</div>
+			<div id="cajafiltro"><?=$filtro.$tabla; ?></div>
+			<?php } ?></td>
+		</tr>
 		<tr>
 			<td valign=top><?php if (isset($lista)) echo $lista; ?></td>
 			<td><?php if (isset($content)) echo $content; ?></td>
