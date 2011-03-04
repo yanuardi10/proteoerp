fa3f0217194aa796b574acf478f8413c49df4c56
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-03-04 16:20:04 +0000

    -Arreglos en kardex cuando las tablas tienen distintos id

--- trunk/proteoerp/system/application/controllers/inventario/kardex.php
+++ trunk/proteoerp/system/application/controllers/inventario/kardex.php
@@ -105,7 +105,7 @@ class Kardex extends Controller {
 			$grid->db->orderby('almacen, fecha, origen');
 			$grid->per_page = 20;
 			$grid->column('Fecha'        ,'<dbdate_to_human><#fecha#></dbdate_to_human>');
-			$grid->column('Or&iacute;gen','<convierte><#origen#>|$link</convierte>','align=left' );
+			$grid->column('Or&iacute;gen',"<convierte><#origen#>|$link</convierte>",'align=left' );
 			$grid->column('Cantidad'     ,'<nformat><#cantidad#></nformat>' ,'align=right');
 			$grid->column('Acumulado'    ,'<nformat><#salcant#></nformat>'  ,'align=right');
 			$grid->column('Monto'        ,'<nformat><#monto#></nformat>'    ,'align=right');
@@ -141,9 +141,14 @@ class Kardex extends Controller {
 		if($tipo=='3I' or $tipo=='3M'){  //ventas de caja
 			$fields = $this->db->field_data('sfac');
 			$ppk=array();
+			$select=array('a.numa','a.tipoa','a.numa AS numero','CONCAT("(",b.cod_cli,") ",b.nombre) cliente','a.cana','a.fecha','a.vendedor','a.preca','a.tota','tipo_doc');
 			foreach ($fields as $field){
 				if($field->primary_key==1){
 					$ppk[]='<#'.$field->name.'#>';
+					$pknombre='a.'.$field->name;
+					if(array_search($pknombre, $select)===false){
+						$select[]=$pknombre;
+					}
 				}
 			}
 			$grid->title('Facturas');
@@ -155,7 +160,7 @@ class Kardex extends Controller {
 			$grid->column('Vendedor'     ,'vendedor','align=center');
 			$grid->column('Precio'       ,'<nformat><#preca#></nformat>','align=\'right\'');
 			$grid->column('Total'        ,'<nformat><#tota#></nformat>' ,'align=\'right\'');
-			$grid->db->select(array('a.numa','a.tipoa','a.numa AS numero','CONCAT("(",b.cod_cli,") ",b.nombre) cliente','a.cana','a.fecha','a.vendedor','a.preca','a.tota','tipo_doc'));
+			$grid->db->select($select);
 			$grid->db->from('sitems a');
 			$grid->db->join('sfac b','b.numero=a.numa  AND b.tipo_doc=a.tipoa');
 			$grid->db->where('a.fecha',$fecha);
@@ -181,9 +186,14 @@ class Kardex extends Controller {
 		}elseif($tipo=='1T'){ //Transferencias
 			$fields = $this->db->field_data('stra');
 			$ppk=array();
+			$select=array('a.numero','b.envia','b.recibe','a.cantidad','b.fecha','b.observ1','a.costo');
 			foreach ($fields as $field){
 				if($field->primary_key==1){
 					$ppk[]='<#'.$field->name.'#>';
+					$pknombre='a.'.$field->name;
+					if(array_search($pknombre, $select)===false){
+						$select[]=$pknombre;
+					}
 				}
 			}
 
@@ -196,7 +206,7 @@ class Kardex extends Controller {
 			$grid->column("Fecha"             ,'<dbdate_to_human><#fecha#></dbdate_to_human>','align=\'center\'');
 			$grid->column("Observaci&oacute;n",'observ1');
 			//$grid->column("Costo"             ,"<nformat><#costo#></nformat>",'align=right');
-			$grid->db->select(array('a.numero','b.envia','b.recibe','a.cantidad','b.fecha','b.observ1','a.costo'));
+			$grid->db->select($select);
 			$grid->db->from('itstra a');
 			$grid->db->join('stra b','a.numero=b.numero','LEFT');
 			$grid->db->where('b.fecha' ,$fecha );
@@ -207,6 +217,11 @@ class Kardex extends Controller {
 			foreach ($fields as $field){
 				if($field->primary_key==1){
 					$ppk[]='<#'.$field->name.'#>';
+					$pknombre='a.'.$field->name;
+					$select=array('a.numero', 'a.fecha','a.proveed', 'a.depo', 'a.cantidad', 'a.costo', 'a.importe','a.control');
+					if(array_search($pknombre, $select)===false){
+						$select[]=$pknombre;
+					}
 				}
 			}
 
@@ -219,7 +234,7 @@ class Kardex extends Controller {
 			$grid->column("Cantidad" ,"<nformat><#cantidad#></nformat>",'align=right');
 			$grid->column("Costo"    ,"<nformat><#costo#></nformat>",'align=right');
 			$grid->column("Importe"  ,"<nformat><#importe#></nformat>",'align=right');
-			$grid->db->select('a.numero, a.fecha, a.proveed, a.depo, a.cantidad, a.costo, a.importe,a.control');
+			$grid->db->select($select);
 			$grid->db->from('itscst a');
 			$grid->db->join('scst b','a.control=b.control');
 			$grid->db->where('a.codigo',$codigo);
@@ -228,9 +243,14 @@ class Kardex extends Controller {
 		}elseif($tipo=='4N'){ //Nota de entrega
 			$fields = $this->db->field_data('snte');
 			$ppk=array();
+			$select=array('a.numero','a.fecha','a.nombre','b.cana','b.precio','b.importe');
 			foreach ($fields as $field){
 				if($field->primary_key==1){
 					$ppk[]='<#'.$field->name.'#>';
+					$pknombre='a.'.$field->name;
+					if(array_search($pknombre, $select)===false){
+						$select[]=$pknombre;
+					}
 				}
 			}
 			$link=anchor('ventas/snte/dataedit/show/'.implode('/',$ppk),"<#numero#>");
@@ -241,7 +261,7 @@ class Kardex extends Controller {
 			$grid->column("Cantidad" ,"<nformat><#cana#>|2|,|.   </nformat>",'align=right');
 			$grid->column("Costo"    ,"<nformat><#precio#>|2|,|. </nformat>",'align=right');
 			$grid->column("Importe"  ,"<nformat><#importe#>|2|,|.</nformat>",'align=right');
-			$grid->db->select(array('a.numero','a.fecha','a.nombre','b.cana','b.precio','b.importe'));
+			$grid->db->select($select);
 			$grid->db->from('snte a');
 			$grid->db->join('itsnte b','a.numero=b.numero');
 			$grid->db->where('b.codigo',$codigo);
@@ -249,9 +269,14 @@ class Kardex extends Controller {
 		}elseif($tipo=='6C'){ //Conversiones
 			$fields = $this->db->field_data('conv');
 			$ppk=array();
+			$select=array('a.numero','a.estampa','b.entrada','b.salida','b.codigo');
 			foreach ($fields as $field){
 				if($field->primary_key==1){
 					$ppk[]='<#'.$field->name.'#>';
+					$pknombre='a.'.$field->name;
+					if(array_search($pknombre, $select)===false){
+						$select[]=$pknombre;
+					}
 				}
 			}
 			$link=anchor('inventario/conversiones/dataedit/show/'.implode('/',$ppk),'<#numero#>');
@@ -260,7 +285,7 @@ class Kardex extends Controller {
 			$grid->column("Fecha"    ,"<dbdate_to_human><#estampa#></dbdate_to_human>",'align=center');
 			$grid->column("Entrada"  ,"<nformat><#entrada#>|2|,|.</nformat>",'align=right');
 			$grid->column("Salida"   ,"<nformat><#salida#>|2|,|. </nformat>",'align=right');
-			$grid->db->select(array('a.numero','a.estampa','b.entrada','b.salida','b.codigo'));
+			$grid->db->select($select);
 			$grid->db->from('conv AS a');
 			$grid->db->join('itconv AS b','a.numero=b.numero');
 			$grid->db->where('b.codigo' ,$codigo);
