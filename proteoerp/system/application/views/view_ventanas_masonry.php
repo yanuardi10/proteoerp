598a6568e3870f650bb1419a3acf3056722770cf
datasql <datasql@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-03-08 17:16:20 +0000

    Ventana con Masonry

---
+++ proteoerp/system/application/views/view_ventanas_masonry.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=<?=$this->config->item('charset'); ?>" >
+<title>ProteoERP<?php if(isset($title)) echo ':'.preg_replace('/<[^>]*>/', '', $title); ?></title>
+<?=style("ventanas.css");?>
+<?=style("masonry.css"); ?>
+<?php if( !isset($tabla) )   $tabla=''; ?>
+
+<?=script("jquery.js") ?>
+<?=script("plugins/jquery.masonry.min.js"); ?>
+
+<script type="text/javascript" charset=<?=$this->config->item('charset'); ?>">
+$(window).load(function() {
+	$('#maso').masonry({ singleMode: true,	itemSelector: '.box'});
+});
+</script>
+
+
+<?php if (isset($head))   echo $head;   ?>
+<?php if (isset($script)) echo $script; ?>
+
+</head>
+<body>
+<div id='encabe'><?php if (isset($title)) echo $title; ?></div>
+<?php if(isset($smenu)) echo '<div id="smenu">'.$smenu.'</div>'; ?>
+
+<div id='contenido' align="center">
+	<?php if (isset($content)) echo $content; ?>
+	<div class="footer">
+		<p>Proteo ERP </p>
+	</div>
+</div>
+
+<?php if (isset($extras)) echo $extras; ?>
+</body>
+</html>
