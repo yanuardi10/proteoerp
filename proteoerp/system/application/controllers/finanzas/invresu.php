a4398eb4af0784587581926047c5b4393b73758d
datasql <datasql@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-03-11 01:39:09 +0000

    Block UI

--- proteoerp/system/application/controllers/finanzas/invresu.php
+++ proteoerp/system/application/controllers/finanzas/invresu.php
@@ -11,38 +11,8 @@ class invresu extends Controller {
 		$this->rapyd->load('datafilter','datagrid','fields');
 		$this->rapyd->uri->keep_persistence();
 
-		/*$mSQL="SELECT 
-		SUBSTR(`invresu`.`mes`,1,4) AS `anno`, 
-		SUBSTR(`invresu`.`mes`,5,2) AS `mes`, 
-		SUM(if((`invresu`.`minicial` > 0),`invresu`.`minicial`,0)) AS `inicial`,
-		SUM(`invresu`.`mcompras`) AS `compras`,SUM(`invresu`.`mventas`) AS `ventas`,
-		((SUM(`invresu`.`fisico`)-SUM(`invresu`.`mtrans`))+SUM(`invresu`.`mnotas`)) AS `retiros`,
-		ABS(SUM(IF((`invresu`.`mfinal` < 0),`invresu`.`mfinal`,0))) AS `despachar`,
-		SUM(IF((`invresu`.`mfinal` > 0),`invresu`.`mfinal`,0)) AS `final` 
-		FROM `invresu` 
-		GROUP BY `invresu`.`mes`";*/
-
-		$error='';
-		$cerror=0;
-		if($this->input->post('pros')!==FALSE){
-			$montos=$this->input->post('monto');
-			foreach($montos as $anno => $mval){
-				foreach($mval AS $mes=>$monto){
-					$sqlmes = $anno.$mes;
-					$mSQL="CALL sp_invresufix($sqlmes,$monto)";
-
-					$ban=$this->db->simple_query($mSQL);
-					if($ban==false){ memowrite($mSQL,'invresu'); $cerror++; }
-				}
-				
-			}
-			if($cerror>0){
-				$error='Hubo algunos errores, se genero un centinela';
-			}
-		}
-
 		$filter = new DataFilter('Filtro del libro de inventario','view_invresutotal');
-		$filter->error_string=$error;
+		//$filter->error_string=$error;
 
 		$filter->fecha = new inputField('A&ntilde;o', 'anno');
 		$filter->fecha->size     = 4;
@@ -64,9 +34,10 @@ class invresu extends Controller {
 		$grid = new DataGrid('Lista');
 		$grid->per_page = 12;
 
-		$uri2  =  anchor('finanzas/invresu/calcula/<#anno#><#mes#>',img(array('src'=>'images/engrana.png','border'=>'0','alt'=>'Editar')));
+		//$uri2  =  anchor('finanzas/invresu/calcula/<#anno#><#mes#>',img(array('src'=>'images/engrana.png','border'=>'0','alt'=>'Editar')));
+		$uri2 = anchor('#',img(array('src'=>'images/engrana.png','border'=>'0','alt'=>'Calcula')),array('onclick'=>'bobo(\''.base_url().'finanzas/invresu/calcula/<#anno#><#mes#>\');return false;'));
 		$uri2 .= "&nbsp;&nbsp;";
-		$uri2 .= anchor('#',img(array('src'=>'images/refresh.png','border'=>'0','alt'=>'PDF')),array('onclick'=>'foo(\''.base_url().'/finanzas/invresu/recalcula/<#anno#><#mes#>\');return false;'));
+		$uri2 .= anchor('#',img(array('src'=>'images/refresh.png','border'=>'0','alt'=>'Rebaja')),array('onclick'=>'foo(\''.base_url().'finanzas/invresu/recalcula/<#anno#><#mes#>\');return false;'));
 
 		$grid->column('A&ntilde;o','anno' ,'align="center"');
 		$grid->column('Mes'       ,'mes'  ,'align="center"');
@@ -94,21 +65,45 @@ class invresu extends Controller {
 		});
 		function foo(url){
 			valor=$("#porcent").val();
-			uurl=url+"/"+valor;
-			//alert(uurl);
+			uurl=url+"/"+valor;'."
+			$.blockUI({
+				message: $('#displayBox'), 
+				css: { 
+				top:  ($(window).height() - 400) /2 + 'px', 
+				left: ($(window).width() - 400) /2 + 'px', 
+				width: '400px' 
+				}".' 			
+			}); 
 			$.get(uurl, function(data) {
+				setTimeout($.unblockUI, 2); 
+				alert(data);
+			});
+			return false;
+		}
+		function bobo(url){'."
+			$.blockUI({
+				message: $('#displayBox'), 
+				css: { 
+				top:  ($(window).height() - 400) /2 + 'px', 
+				left: ($(window).width() - 400) /2 + 'px', 
+				width: '400px' 
+				}".' 			
+			}); 
+			$.get(url, function(data) {
+				setTimeout($.unblockUI, 2); 
 				alert(data);
 			});
 			return false;
 		}
 		</script>';
+		$espera = '<div id="displayBox" style="display:none" ><p>Espere.....</p><img  src="'.base_url().'images/doggydig.gif" width="131px" height="79px"  /></div>';
 		$porcent = "<div align='right'>Porcentaje de Variacion ";
 		$porcent .= form_input(array('name'=>'porcent','id'=>'porcent','value'=>'0','size'=>'10','style'=>'text-align:right' ) );
 		$porcent .= "</div>";
-		$data['content'] = $filter->output.$porcent.$ggrid;
+		$data['content'] = $filter->output.$porcent.$ggrid.$espera;
 		$data['title']   = heading('Libro de inventario');
-		$data['script']  = $script;
-		$data['head']    = $this->rapyd->get_head().script('jquery.js').script('plugins/jquery.numeric.pack.js').script('plugins/jquery.floatnumber.js');
+		$data['script']  = script('plugins/jquery.blockUI.js').$script;
+		$data['head']    = $this->rapyd->get_head().script('plugins/jquery.numeric.pack.js').script('plugins/jquery.floatnumber.js');
 		$this->load->view('view_ventanas', $data);
 	}
 
@@ -119,7 +114,8 @@ class invresu extends Controller {
 			$this->db->simple_query("CALL sp_invresu(".$meco.")");
 			$meco++;
 		}
-		redirect('finanzas/invresu');
+		echo "Calculo Concluido";
+		//redirect('finanzas/invresu');
 	}
 
 	function recalcula(){
