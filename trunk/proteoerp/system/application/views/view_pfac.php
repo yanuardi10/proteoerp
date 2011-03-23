9c9696d9380147c0cfd9ee6ca8602d460243ed1a
jud.prog <jud.prog@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-03-23 15:46:39 +0000

    vista para el modulo pfacc.php

---
+++ trunk/proteoerp/system/application/views/view_pfac.php
@@ -0,0 +1,345 @@
+<?php
+
+$container_bl=join("&nbsp;", $form->_button_container["BL"]);
+$container_br=join("&nbsp;", $form->_button_container["BR"]);
+$container_tr=join("&nbsp;", $form->_button_container["TR"]);
+
+if ($form->_status=='delete' || $form->_action=='delete' || $form->_status=='unknow_record'):
+	echo $form->output;
+else:
+
+$campos=$form->template_details('itpfac');
+$scampos  ='<tr id="tr_itpfac_<#i#>">';
+$scampos .='<td class="littletablerow" align="left" >'.$campos['codigoa']['field'].'</td>';
+$scampos .='<td class="littletablerow" align="left" >'.$campos['desca']['field'].'</td>';
+$scampos .='<td class="littletablerow" align="right">'.$campos['cana']['field'].  '</td>';
+$scampos .='<td class="littletablerow" align="right">'.$campos['preca']['field']. '</td>';
+$scampos .='<td class="littletablerow" align="right">'.$campos['tota']['field'];
+for($o=1;$o<5;$o++){
+	$it_obj   = "precio${o}";
+	$scampos .= $campos[$it_obj]['field'];
+}
+$scampos .= $campos['itiva']['field'];
+$scampos .= $campos['sinvtipo']['field'];
+$scampos .= $campos['itpvp']['field'];
+$scampos .= $campos['itcosto']['field'];
+$scampos .= $campos['sinvpeso']['field'].'</td>';
+$scampos .= '<td class="littletablerow"><a href=# onclick="del_itpfac(<#i#>);return false;">Eliminar</a></td></tr>';
+$campos=$form->js_escape($scampos);
+
+if(isset($form->error_string)) echo '<div class="alert">'.$form->error_string.'</div>';
+
+//echo $form_scripts;
+echo $form_begin;
+if($form->_status!='show'){ ?>
+
+<script language="javascript" type="text/javascript">
+itpfac_cont=<?php echo $form->max_rel_count['itpfac']; ?>;
+
+$(function(){
+	$(".inputnum").numeric(".");
+	totalizar();
+	for(var i=0;i < <?php echo $form->max_rel_count['itpfac']; ?>;i++){
+		cdropdown(i);
+		cdescrip(i);
+	}
+});
+
+function importe(id){
+	var ind     = id.toString();
+	var cana    = Number($("#cana_"+ind).val());
+	var preca   = Number($("#preca_"+ind).val());
+	var tota = roundNumber(cana*preca,2);
+	$("#tota_"+ind).val(tota);
+
+	totalizar();
+}
+
+function totalizar(){
+	var iva    =0;
+	var totalg =0;
+	var itiva  =0;
+	var itpeso =0;
+	var totals =0;
+	var tota=0;
+	var peso   =0;
+	var cana   =0;
+	var arr=$('input[name^="tota_"]');
+	jQuery.each(arr, function() {
+		nom=this.name
+		pos=this.name.lastIndexOf('_');
+		if(pos>0){
+			ind     = this.name.substring(pos+1);
+			cana    = Number($("#cana_"+ind).val());
+			itiva   = Number($("#itiva_"+ind).val());
+			itpeso  = Number($("#sinvpeso_"+ind).val());
+			tota    = Number(this.value);
+
+			peso    = peso+(itpeso*cana);
+			iva     = iva+tota*(itiva/100);
+			totals  = totals+tota;
+		}
+	});
+	$("#peso").val(roundNumber(peso,2));
+	$("#totalg").val(roundNumber(totals+iva,2));
+	$("#totals").val(roundNumber(totals,2));
+	$("#iva").val(roundNumber(iva,2));
+}
+
+function add_itpfac(){
+	var htm = <?=$campos ?>;
+	can = itpfac_cont.toString();
+	con = (itpfac_cont+1).toString();
+	htm = htm.replace(/<#i#>/g,can);
+	htm = htm.replace(/<#o#>/g,con);
+	$("#__UTPL__").before(htm);
+	$("#cana_"+can).numeric(".");
+	itpfac_cont=itpfac_cont+1;
+}
+
+function post_precioselec(ind,obj){
+	if(obj.value=='o'){
+		otro = prompt('Precio nuevo','');
+		otro = Number(otro);
+		if(otro>0){
+			var opt=document.createElement("option");
+			opt.text = nformat(otro,2);
+			opt.value= otro;
+			obj.add(opt,null);
+			obj.selectedIndex=obj.length-1;
+		}
+	}
+	importe(ind);
+}
+
+function post_modbus_scli(){
+	var tipo  =Number($("#sclitipo").val()); if(tipo>0) tipo=tipo-1;
+	//var cambio=confirm('¿Deseas cambiar los precios por los que tenga asginado el cliente?');
+
+	var arr=$('select[name^="preca_"]');
+	jQuery.each(arr, function() {
+		nom=this.name;
+		pos=this.name.lastIndexOf('_');
+		if(pos>0){
+			ind = this.name.substring(pos+1);
+			id  = Number(ind);
+			this.selectedIndex=tipo;
+			importe(id);
+		}
+	});
+	totalizar();
+}
+
+function post_modbus_sinv(nind){
+	ind=nind.toString();
+	var tipo =Number($("#sclitipo").val()); if(tipo>0) tipo=tipo-1;
+	$("#preca_"+ind).empty();
+	var arr=$('#preca_'+ind);
+	cdropdown(nind);
+	
+	jQuery.each(arr, function() { this.selectedIndex=tipo; });
+	importe(nind);
+	totalizar();
+}
+
+function cdropdown(nind){
+	var ind=nind.toString();
+	var preca=$("#preca_"+ind).val();
+	var pprecio  = document.createElement("select");
+
+	pprecio.setAttribute("id"    , "preca_"+ind);
+	pprecio.setAttribute("name"  , "preca_"+ind);
+	pprecio.setAttribute("class" , "select");
+	pprecio.setAttribute("style" , "width: 100px");
+	pprecio.setAttribute("onchange" , "post_precioselec("+ind+",this)");
+
+	var ban=0;
+	var ii=0;
+	var id='';
+	
+	if(preca==null || preca.length==0) ban=1;
+	for(ii=1;ii<5;ii++){
+		id =ii.toString();
+		val=$("#precio"+id+"_"+ind).val();
+		opt=document.createElement("option");
+		opt.text =nformat(val,2);
+		opt.value=val;
+		pprecio.add(opt,null);
+		if(val==preca){
+			ban=1;
+			pprecio.selectedIndex=ii-1;
+		}
+	}
+	if(ban==0){
+		opt=document.createElement("option");
+		opt.text = nformat(preca,2);
+		opt.value= preca;
+		pprecio.add(opt,null);
+		pprecio.selectedIndex=4;
+	}
+
+	opt=document.createElement("option");
+	opt.text = 'Otro';
+	opt.value= 'o';
+	pprecio.add(opt,null);
+
+	$("#preca_"+ind).replaceWith(pprecio);
+}
+
+//Cambia el campo descripcion en caso ser servicio
+function cdescrip(nind){
+	var ind=nind.toString();
+	var tipo =$("#sinvtipo_"+ind).val();
+
+	if(tipo=='Servicio'){
+		var desca  =$("#desca_"+ind).val();
+		var ddesca = document.createElement("textarea");
+		ddesca.setAttribute("id"    , "desca_"+ind);
+		ddesca.setAttribute("name"  , "desca_"+ind);
+		ddesca.setAttribute("class" , "textarea");
+		ddesca.setAttribute("cols"  , 34);
+		ddesca.setAttribute("rows"  , 3);
+		//ddesca.setAttribute("readonly" ,"readonly");
+		
+		$("#desca_"+ind).replaceWith(ddesca);
+		$("#desca_"+ind).val(desca);
+
+	}else{
+
+		var desca = $("#desca_"+ind).val();
+		var ddeca = document.createElement("input");
+		ddeca.setAttribute("id"    , "desca_"+ind);
+		ddeca.setAttribute("name"  , "desca_"+ind);
+		ddeca.setAttribute("class" , "input");
+		ddeca.setAttribute("size"  , 36);
+		ddeca.setAttribute("maxlength", 50);
+		ddeca.setAttribute("readonly" ,"readonly");
+		ddeca.setAttribute("value"    ,desca);
+		$("#desca_"+ind).replaceWith(ddeca);
+	}
+}
+
+function del_itpfac(id){
+	id = id.toString();
+	$('#tr_itpfac_'+id).remove();
+	totalizar();
+}
+</script>
+<?php } ?>
+
+<table align='center' width="95%">
+	<tr>
+		<td align=right><?php echo $container_tr?></td>
+	</tr>
+	<tr>
+		<td>
+		<table width="100%" style="margin: 0; width: 100%;">
+			<tr>
+				<th colspan='5' class="littletableheader">Pedidos <b><?php if($form->_status=='show' or $form->_status=='modify' ) echo str_pad($form->numero->output,8,0,0); ?></b></th>
+			</tr>
+			<tr>
+				<td class="littletableheader"><?php echo $form->fecha->label;    ?>*&nbsp;</td>
+				<td class="littletablerow">   <?php echo $form->fecha->output;   ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->cliente->label;  ?>&nbsp;</td>
+				<td class="littletablerow">   <?php echo $form->cliente->output,$form->sclitipo->output; ?>&nbsp;</td>
+				<td class="littletablerow">   <?php echo $form->nombre->output;  ?>&nbsp;</td>
+			</tr>
+			<tr>
+				<td class="littletableheader"><?php echo $form->vd->label     ?>&nbsp;</td>
+				<td class="littletablerow">   <?php echo $form->vd->output    ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->rifci->label; ?>&nbsp;</td>
+				<td class="littletablerow" colspan='2'><?php echo $form->rifci->output;   ?>&nbsp;</td>
+			</tr>
+			<tr>
+				<td class="littletableheader"><?=$form->peso->label  ?>&nbsp;</td>
+				<td class="littletablerow" align="left"><?=$form->peso->output ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->direc->label  ?>&nbsp;</td>
+				<td class="littletablerow" colspan='2'><?php echo $form->direc->output ?>&nbsp;</td>
+			</tr>
+		</table><br>
+		</td>
+	</tr>
+	<tr>
+		<td>
+		<table width='100%'>
+			<tr>
+				<th colspan='6' class="littletableheader">Lista de Art&iacute;culos</th>
+			</tr>
+			<tr>
+				<td class="littletableheader">C&oacute;digo</td>
+				<td class="littletableheader">Descripci&oacute;n</td>
+				<td class="littletableheader">Cantidad</td>
+				<td class="littletableheader">Precio</td>
+				<td class="littletableheader">Importe</td>
+				<?php if($form->_status!='show') {?>
+					<td class="littletableheader">&nbsp;</td>
+				<?php } ?>
+			</tr>
+
+			<?php for($i=0;$i<$form->max_rel_count['itpfac'];$i++) {
+				$it_codigoa  = "codigoa_$i";
+				$it_desca   = "desca_$i";
+				$it_cana    = "cana_$i";
+				$it_preca   = "preca_$i";
+				$it_tota = "tota_$i";
+				$it_iva     = "itiva_$i";
+				$it_peso    = "sinvpeso_$i";
+				$it_tipo    = "sinvtipo_$i";
+				$it_costo    = "itcosto_$i";
+				$it_pvp    = "itpvp_$i";
+
+				$pprecios='';
+				for($o=1;$o<5;$o++){
+					$it_obj   = "precio${o}_${i}";
+					$pprecios.= $form->$it_obj->output;
+				}
+				$pprecios .= $form->$it_iva->output;
+				$pprecios .= $form->$it_peso->output;
+				$pprecios .= $form->$it_tipo->output;
+				$pprecios .= $form->$it_costo->output;
+				$pprecios .= $form->$it_pvp->output;
+			?>
+
+			<tr id='tr_itpfac_<?php echo $i; ?>'>
+				<td class="littletablerow" align="left" ><?php echo $form->$it_codigoa->output; ?></td>
+				<td class="littletablerow" align="left" ><?php echo $form->$it_desca->output;  ?></td>
+				<td class="littletablerow" align="right"><?php echo $form->$it_cana->output;   ?></td>
+				<td class="littletablerow" align="right"><?php echo $form->$it_preca->output;  ?></td>
+				<td class="littletablerow" align="right"><?php echo $form->$it_tota->output.$pprecios;?></td>
+
+				<?php if($form->_status!='show') {?>
+				<td class="littletablerow">
+					<a href='#' onclick='del_itpfac(<?=$i ?>);return false;'>Eliminar</a>
+				</td>
+				<?php } ?>
+			</tr>
+			<?php } ?>
+
+			<tr id='__UTPL__'>
+				<td id='cueca'></td>
+			</tr>
+		</table>
+		<?php echo $container_bl ?>
+		<?php echo $container_br ?>
+		</td>
+	</tr>
+	<tr>
+		<td>
+		<table width='100%'>
+			<tr>
+				<th colspan='6' class="littletableheader">Res&uacute;men Financiero</th>
+			</tr>
+			<tr>
+				<td class="littletableheader">           <?php echo $form->ivat->label;    ?></td>
+				<td class="littletablerow" align='right'><?php echo $form->ivat->output;   ?></td>
+				<td class="littletableheader">           <?php echo $form->totals->label;  ?></td>
+				<td class="littletablerow" align='right'><?php echo $form->totals->output; ?></td>
+				<td class="littletableheader">           <?php echo $form->totalg->label;  ?></td>
+				<td class="littletablerow" align='right'><?php echo $form->totalg->output; ?></td>
+			</tr>
+		</table>
+		<?php echo $form_end; ?>
+		</td>
+	</tr>
+</table>
+<?php endif; ?>
