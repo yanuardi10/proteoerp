db1850cceb3a3de0f64cee3347bf4a60492831d3
datasql <datasql@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-03-26 02:04:49 +0000

    Forma viewa

--- trunk/proteoerp/system/application/rapyd/views/proteo/dataformfiltro.php
+++ trunk/proteoerp/system/application/rapyd/views/proteo/dataformfiltro.php
@@ -1,27 +1,27 @@
 <?php echo $form_begin ?>
-<table style="margin:0;width:100%;border-collapse:collapse;padding:0;">
-  <tr>
-    <td>
+			<table style="margin:0;width:100%;border-collapse:collapse;padding:0;">
+				<tr>
+					<td>
 <?php if($title!=""):?>
-      <table  style="margin:0;width:100%;border-collapse:collapse;padding:0;">
-        <tr>
-          <td class="mainheader"><?php echo $title?></td>
-          <td class="mainheader" align="right"><?php echo $container_tr?></td>
-        </tr>
-      </table>
+						<table  style="margin:0;width:100%;border-collapse:collapse;padding:0;">
+							<tr>
+								<td class="mainheader"><?php echo $title?></td>
+								<td class="mainheader" align="right"><?php echo $container_tr?></td>
+							</tr>
+						</table>
 <?php endif;?>
-      <div class="mainbackground" style="padding:2px;clear:both">
-      <div class="alert"><?php echo $error_string?></div>
-      <table style="margin:0;width:98%;">
+						<div class="mainbackground" style="padding:2px;clear:both">
+						<div class="alert"><?php echo $error_string?></div>
+						<table style="margin:0;width:98%;">
 <?php if (isset($groups)):?>
 
 <?php $iii = 0; ?>
 <?php foreach ($groups as $group)://groups?>
 <?php //@BOFS ?>
   <?php if ($group["group_name"] != "ungrouped"):?>
-      <?php if ($iii==0)  echo "<tr ".$group["group_tr"].">"; ?>
-        <td  colspan="2" style="padding:0;">
-        <table style="margin:0;width:100%;">
+							<?php if ($iii==0)  echo "<tr ".$group["group_tr"].">"; ?>
+								<td  colspan="2" style="padding:0;" valign='top'>
+								<table style="margin:0;width:100%;">
 <?php
 //          <tr>
 //            <td colspan="2" class="micro">< ? p hp echo $group["group_name"]? ></td>
@@ -37,20 +37,18 @@
       <?php endforeach;?>
 
     <?php else://non hidden?>
-          <tr <?php echo $field_series["series_tr"]?>>
-
+							<tr <?php echo $field_series["series_tr"]?>>
       <?php if(isset($field_series["fields"])): ?>
         <?php $first_field=true?>
         <?php foreach ($field_series["fields"] as $field)://fields?>
-
           <?php if($first_field):?>
             <?php $first_field=false?>
             <?php if (($field["type"] == "container")||($field["type"] == "iframe")):?>
-              <td colspan="2">
+								<td colspan="2">
               <?php echo $field["field"]?>
             <?php else:?>
-              <td style="width:120px;" class="littletablefilter"><?php echo $field["label"]?></td>
-              <td style="padding:1px;" class="littletablerow" <?php echo $field["field_td"]?>>
+								<td style="width:120px;" class="littletablefilter"><?php echo $field["label"]?></td>
+								<td style="padding:1px;" class="littletablerow" <?php echo $field["field_td"]?>>
               <?php echo $field["field"]?>&nbsp;
             <?php endif;?>
           <?php else:?>
@@ -58,42 +56,40 @@
           <?php endif;?>
         <?php endforeach;//fields?>
       <?php endif;?>
-              </td>
-            <tr>
+								</td>
+							</tr>
     <?php endif;//hidden?>
   <?php endforeach;//field_series?>
-
   <?php if ($group["group_name"] != "ungrouped"):?>
-            <tr>
-              <td colspan="2"></td>
-            </tr>
-          </table>
-
-        </td>
-      <? if ($iii > 0) echo "</tr>"; $iii++; ?>
-  <?php endif;?>
+										<tr>
+											<td colspan="2"></td>
+										</tr>
+									</table>
+								</td>
+<?php //if ($iii > 0) echo "</tr>1"; $iii++; ?>
+<?php endif;?>
 <?php //@EOFS ?>
 <?php endforeach;//groups?>
 </tr>
 <?php endif;?>
-        <tr id='__UTPL__'>
-          <td colspan="2" ></td>
-        </tr>
+							<tr id='__UTPL__'>
+								<td colspan="2" ></td>
+							</tr>
 <?php if(isset($message)):?>
-        <tr>
-          <td colspan="2" class="tablerow"><?php echo $message?> <div style="float:left"></div></td>
-        </tr>
+							<tr>
+								<td colspan="2" class="tablerow"><?php echo $message?> <div style="float:left"></div></td>
+							</tr>
 <?php endif;?>
-      </table>
-      <?php echo $form_scripts?>
-      </div>
-      <div class="mainfooter" style='background-color: #DDDDFF;'>
-        <div>
-          <div style="float:left;"><?php echo  $container_bl ?></div>
-          <div style="float:right"><?php echo $container_br?></div>
-        </div><div style="clear:both;"></div>
-      </div>
-    </td>
-  </tr>
-</table>
+						</table>
+<?php echo $form_scripts?>
+						</div>
+						<div class="mainfooter" style='background-color: #DDDDFF;'>
+						<div>
+						<div style="float:left;"><?php echo $container_bl?></div>
+						<div style="float:right"><?php echo $container_br?></div>
+						</div><div style="clear:both;"></div>
+						</div>
+					</td>
+				</tr>
+			</table>
 <?php echo $form_end?>
