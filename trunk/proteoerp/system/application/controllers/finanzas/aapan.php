4384617caf504930d7d632d29b633d6a9ec26fda
jud.prog <jud.prog@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-04-25 16:23:40 +0000

    modificado aapan.php para vista solamente

--- trunk/proteoerp/system/application/controllers/finanzas/aapan.php
+++ trunk/proteoerp/system/application/controllers/finanzas/aapan.php
@@ -1,7 +1,7 @@
 <?php require_once(BASEPATH.'application/controllers/validaciones.php');
 
 class aapan extends validaciones {
-	
+
 	function aapan(){
 		parent::Controller();
 		$this->load->library("rapyd");
@@ -35,8 +35,8 @@ class aapan extends validaciones {
 		$filter->buttons("reset","search");
 		$filter->build('dataformfiltro');
 
-		$uri = anchor('finanzas/aapan/dataedit/show/<#id#>','<#numero#>');
-		$uri_2 = anchor('finanzas/aapan/dataedit/show/<#id#>',img(array('src'=>'images/editar.png','border'=>'0','alt'=>'Editar','height'=>'12')));
+		$uri = anchor('finanzas/aapan/dataedit/<#tipo#>/show/<#id#>','<#numero#>');
+		$uri_2 = anchor('finanzas/aapan/dataedit/<#tipo#>/show/<#id#>',img(array('src'=>'images/editar.png','border'=>'0','alt'=>'Editar','height'=>'12')));
 
 		$grid = new DataGrid("Lista de Aplicaci&oacute;n de Anticipos");
 		$grid->order_by("numero","asc");
@@ -55,8 +55,8 @@ class aapan extends validaciones {
 			
 		//$grid->add("finanzas/apan/dataedit/create");
 		$grid->build('datagridST');
-		
-//********** SUPER TABLE ************* 
+
+		//********** SUPER TABLE *************
 		$extras = '
 <script type="text/javascript">
 //<![CDATA[
@@ -84,10 +84,10 @@ class aapan extends validaciones {
 }
 </style>	
 ';
-//****************************************
+		//****************************************
 		$data['style']   = $style;
 		$data['style']  .= style('superTables.css');
-		$data['extras']  = $extras;		
+		$data['extras']  = $extras;
 
 		$data['content'] = $grid->output;
 		$data['filtro']  = $filter->output;
@@ -98,19 +98,25 @@ class aapan extends validaciones {
 		$data['head']  .= $this->rapyd->get_head();
 		$this->load->view('view_ventanas', $data);
 	}
-	
-	function dataedit()	{
+
+	function dataedit($tipo)	{
 		$this->rapyd->load('dataobject','datadetails');
-		
 		$do = new DataObject("apan");
-		$do->rel_one_to_many('itppro', 'itppro', array('transac'));
-		$do->rel_one_to_many('itccli', 'itccli', array('transac'));
-		
+		$title="";
+		if($tipo=='P'){
+			$do->rel_one_to_many('itppro', 'itppro', array('transac'=>'transac'));
+			$title='itppro';
+		}
+		else {
+			$do->rel_one_to_many('itccli', 'itccli', array('transac'=>'transac'));
+			$title='itccli';
+		}
+
+
 		$edit = new DataDetails('Aplicaci&oacute;n de Anticipos', $do);
 		$edit->back_url = site_url('finanzas/aapan/filteredgrid');
-		$edit->set_rel_title('itppro','Anticipo <#o#>');
-		$edit->set_rel_title('itccli','Anticipo <#o#>');
-		
+		$edit->set_rel_title($title,'Anticipo <#o#>');
+
 		$edit->numero = new inputField("N&uacute;mero", "numero");
 		$edit->numero->mode="autohide";
 		$edit->numero->size =12;
@@ -120,9 +126,9 @@ class aapan extends validaciones {
 		$edit->fecha = new DateonlyField("Fecha", "fecha");
 		$edit->fecha->size = 12;
 		$edit->fecha->rule="required|chfecha";
+		$edit->fecha->insertValue = date("Y-m-d");
 
 		$edit->tipo = new dropdownField("Tipo", "tipo");
-		$edit->tipo->option("C","Cliente");
 		$edit->tipo->option("C","Cliente");
 		$edit->tipo->option("P","Proveedor");
 		$edit->tipo->style="width:100px";
@@ -161,45 +167,124 @@ class aapan extends validaciones {
 		$edit->observa2->rule='trim';
 		$edit->observa2->size =50;
 		$edit->observa2->maxlength=50;
-		
-		//Detalles itppro
-		
-		$edit->tipoppro = new inputField("Tipo <#o#>","tipoppro_<#i#>");
-		$edit->tipoppro->db_name = "tipoppro";
-		$edit->tipoppro->rel_id  = 'itppro'; 
-		$edit->tipoppro->rule='trim|required';
-		$edit->tipoppro->size =10;
-		$edit->tipoppro->readonly=true;
-		
-		$edit->tipo_doc = new inputField("Tipo Documento <#o#>","tipo_doc_<#i#>");
-		$edit->tipo_doc->db_name = "tipo_doc";
-		$edit->tipo_doc->rel_id  = 'itppro';
-		$edit->tipo_doc->rule='trim|required';
-		$edit->tipo_doc->size =10;
-		$edit->tipo_doc->readonly=true;
-		
-		$edit->itnumero = new inputField("N&uacute;mero <#o#>","itnumero_<#i#>");
-		$edit->itnumero->db_name = "numero";
-		$edit->itnumero->rel_id  = 'itppro';
-		$edit->itnumero->rule='trim|required';
-		$edit->itnumero->size =10;
-		$edit->itnumero->readonly=true;
-		
-		//Detalles itccli
 
+		//Detalles itppro
+		if($tipo=='P'){
+			$edit->tipoppro = new inputField("Tipo <#o#>","tipoppro_<#i#>");
+			$edit->tipoppro->db_name = "tipoppro";
+			$edit->tipoppro->rel_id  = 'itppro';
+			$edit->tipoppro->rule='trim|required';
+			$edit->tipoppro->size =10;
+			$edit->tipoppro->readonly=true;
+
+			$edit->tipo_doc = new inputField("Tipo Documento <#o#>","tipo_doc_<#i#>");
+			$edit->tipo_doc->db_name = "tipo_doc";
+			$edit->tipo_doc->rel_id  = 'itppro';
+			$edit->tipo_doc->rule='trim|required';
+			$edit->tipo_doc->size =10;
+			$edit->tipo_doc->readonly=true;
+
+			$edit->itnumero = new inputField("N&uacute;mero <#o#>","itnumero_<#i#>");
+			$edit->itnumero->db_name = "numero";
+			$edit->itnumero->rel_id  = 'itppro';
+			$edit->itnumero->rule='trim|required';
+			$edit->itnumero->size =10;
+			$edit->itnumero->readonly=true;
+
+			$edit->itnumppro = new inputField("N&uacute;mero <#o#>","itnumppro_<#i#>");
+			$edit->itnumppro->db_name = "numppro";
+			$edit->itnumppro->rel_id  = 'itppro';
+			$edit->itnumppro->rule='trim|required';
+			$edit->itnumppro->size =10;
+			$edit->itnumppro->readonly=true;
+
+			$edit->itfechap = new DateonlyField("Fecha", "itfechap_<#i#>");
+			$edit->itfechap->db_name = "fecha";
+			$edit->itfechap->rel_id  = 'itppro';
+			$edit->itfechap->size = 12;
+			$edit->itfechap->rule="required|chfecha";
+			$edit->itfechap->insertValue = date("Y-m-d");
+
+			$edit->itmontop = new inputField("Monto <#o#>", "itmontop_<#i#>");
+			$edit->itmontop->db_name='monto';
+			$edit->itmontop->css_class='inputnum';
+			$edit->itmontop->rel_id   ='itppro';
+			$edit->itmontop->size=3;
+			$edit->itmontop->rule='positive';
+
+			$edit->itabonop = new inputField("Abono <#o#>", "itabonop_<#i#>");
+			$edit->itabonop->db_name='abono';
+			$edit->itabonop->css_class='inputnum';
+			$edit->itabonop->rel_id   ='itppro';
+			$edit->itabonop->size=3;
+			$edit->itabonop->rule='positive';
+		}
+		//Detalles itccli
+		if($tipo=='C'){
+			$edit->tipoccli = new inputField("Tipo <#o#>","tipoccli_<#i#>");
+			$edit->tipoccli->db_name = "tipoccli";
+			$edit->tipoccli->rel_id  = 'itccli';
+			$edit->tipoccli->rule='trim|required';
+			$edit->tipoccli->size =10;
+			$edit->tipoccli->readonly=true;
+
+			$edit->tipo_doc_c = new inputField("Tipo Documento <#o#>","tipo_doc_C<#i#>");
+			$edit->tipo_doc_c->db_name = "tipo_doc";
+			$edit->tipo_doc_c->rel_id  = 'itccli';
+			$edit->tipo_doc_c->rule='trim|required';
+			$edit->tipo_doc_c->size =10;
+			$edit->tipo_doc_c->readonly=true;
+
+			$edit->itnumero_c = new inputField("N&uacute;mero <#o#>","itnumero_c_<#i#>");
+			$edit->itnumero_c->db_name = "numero";
+			$edit->itnumero_c->rel_id  = 'itccli';
+			$edit->itnumero_c->rule='trim|required';
+			$edit->itnumero_c->size =10;
+			$edit->itnumero_c->readonly=true;
+
+			$edit->numccli = new inputField("N&uacute;mero <#o#>","numccli_<#i#>");
+			$edit->numccli->db_name = "numccli";
+			$edit->numccli->rel_id  = 'itccli';
+			$edit->numccli->rule='trim|required';
+			$edit->numccli->size =10;
+			$edit->numccli->readonly=true;
+
+			$edit->itfechac = new DateonlyField("Fecha", "itfechac_<#i#>");
+			$edit->itfechac->db_name = "fecha";
+			$edit->itfechac->rel_id  = 'itccli';
+			$edit->itfechac->size = 12;
+			$edit->itfechac->rule="required|chfecha";
+			$edit->itfechac->insertValue = date("Y-m-d");
+
+			$edit->itmontoc = new inputField("Monto <#o#>", "itmontoc_<#i#>");
+			$edit->itmontoc->db_name='monto';
+			$edit->itmontoc->css_class='inputnum';
+			$edit->itmontoc->rel_id   ='itccli';
+			$edit->itmontoc->size=3;
+			$edit->itmontoc->rule='positive';
+
+			$edit->itabonoc = new inputField("Abono <#o#>", "itabonoc_<#i#>");
+			$edit->itabonoc->db_name='abono';
+			$edit->itabonoc->css_class='inputnum';
+			$edit->itabonoc->rel_id   ='itccli';
+			$edit->itabonoc->size=3;
+			$edit->itabonoc->rule='positive';
+		}
+		///fin de detalles
 		$edit->buttons("modify", "save", "undo", "delete", "back");
 		$edit->build();
 
-		$data['content'] = $edit->output;
+		$conten['form']  =&  $edit;
+		$data['content'] = $this->load->view('view_apan', $conten,true);
 		$data['title']   = "<h1>Aplicaci&oacute;n de Anticipos</h1>";
 		$data["head"]    = script("jquery.pack.js").script("plugins/jquery.numeric.pack.js").script("plugins/jquery.floatnumber.js").$this->rapyd->get_head();
 		$this->load->view('view_ventanas', $data);
 	}
-	
-	function instalar(){
-		$sql="ALTER TABLE `apan`  ADD COLUMN `id` INT(10) NULL AUTO_INCREMENT AFTER `usuario`,  ADD PRIMARY KEY (`id`)";
-		$this->db->query($sql);
 
+		function instalar(){
+			$sql="ALTER TABLE `apan`  ADD COLUMN `id` INT(10) NULL AUTO_INCREMENT AFTER `usuario`,  ADD PRIMARY KEY (`id`)";
+			$this->db->query($sql);
+
+		}
 	}
-}
-?>
+	?>
