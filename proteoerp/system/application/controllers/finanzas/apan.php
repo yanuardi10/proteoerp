a1138f88669c71c9c2e47e1bae1b5e55771910d1
jud.prog <jud.prog@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-05-05 15:54:00 +0000

    intercambiados contenidos de apan.php con aapan.php, en apan.php se encuentran las modificaciones realizadas y arreglo de vista y en aapan.php esta l contenido del anterior modulo

--- proteoerp/system/application/controllers/finanzas/apan.php
+++ proteoerp/system/application/controllers/finanzas/apan.php
@@ -1,11 +1,11 @@
 <?php require_once(BASEPATH.'application/controllers/validaciones.php');
-//anticipos
-class Apan extends validaciones {
-		function apan(){
-		parent::Controller(); 
+
+class apan extends validaciones {
+
+	function apan(){
+		parent::Controller();
 		$this->load->library("rapyd");
-		define ("THISFILE",   APPPATH."controllers/nomina". $this->uri->segment(2).EXT);
-   }
+	}
 
 	function index(){
 		$this->datasis->modulo_id(505,1);
@@ -17,254 +17,276 @@ class Apan extends validaciones {
 		$this->rapyd->uri->keep_persistence();
 
 		$filter = new DataFilter("Filtro por N&uacute;mero", 'apan');
-		
+
 		$filter->numero = new inputField("N&uacute;mero", "numero");
 		$filter->numero->size=12;
-		$filter->numero->maxlength=8;		
+		$filter->numero->maxlength=8;
 
 		$filter->nombre = new inputField("Nombre", "nombre");
 		$filter->nombre->size=12;
 		$filter->nombre->maxlength=30;
-		
+
 		$filter->tipo = new dropdownField("Tipo", "tipo");
 		$filter->tipo->option("","");
 		$filter->tipo->option("C","C");
 		$filter->tipo->option("P","P");
 		$filter->tipo->style="width:100px";
-		
-		$filter->buttons("reset","search");
-		$filter->build();
 
-		$uri = anchor('finanzas/apan/dataedit/show/<#id#>','<#numero#>');
+		$filter->buttons("reset","search");
+		$filter->build('dataformfiltro');
+
+		$uri = anchor('finanzas/apan/dataedit/<#tipo#>/show/<#id#>','<#numero#>');
+		$uri_2 = anchor('finanzas/apan/dataedit/<#tipo#>/show/<#id#>',img(array('src'=>'images/editar.png','border'=>'0','alt'=>'Editar','height'=>'12')));
 
 		$grid = new DataGrid("Lista de Aplicaci&oacute;n de Anticipos");
 		$grid->order_by("numero","asc");
-		$grid->per_page = 20;
+		$grid->per_page = 50;
 
+		$grid->column('Acci&oacute;n',$uri_2,'align=center');
 		$grid->column_orderby("N&uacute;mero",$uri,'numero');
 		$grid->column_orderby("Fecha","<dbdate_to_human><#fecha#></dbdate_to_human>",'fecha',"align='center'");
 		$grid->column_orderby("Tipo","tipo",'tipo');
 		$grid->column_orderby("Clipro","clipro",'clipro');
 		$grid->column_orderby("Nombre","nombre",'nombre');
-		$grid->column("Monto","<number_format><#monto#>|2|,|.</number_format>" ,'align=right');
-								 
-		$grid->add("finanzas/apan/dataedit/create");
-		$grid->build();
-		
-		$data['content'] = $filter->output.$grid->output;
-		$data['title']   = "<h1>Aplicaci&oacute;n de Anticipos</h1>";
-		$data["head"]    = $this->rapyd->get_head();
-		$this->load->view('view_ventanas', $data);	
+		$grid->column_orderby("Observaci&oacute;n 1","observa1",'observa1');
+		$grid->column_orderby("Observaci&oacute;n 2","observa2",'observa2');
+		$grid->column("Monto","<nformat><#monto#>|2|,|.</nformat>" ,'align=right');
+		$grid->column("Reinte","<nformat><#reinte#>|2|,|.</nformat>" ,'align=right');
+			
+		//$grid->add("finanzas/apan/dataedit/create");
+		$grid->build('datagridST');
+
+		//********** SUPER TABLE *************
+		$extras = '
+<script type="text/javascript">
+//<![CDATA[
+(function() {
+	var mySt = new superTable("demoTable", {
+	cssSkin : "sSky",
+	fixedCols : 1,
+	headerRows : 1,
+	onStart : function () {	this.start = new Date();},
+	onFinish : function () {document.getElementById("testDiv").innerHTML += "Finished...<br>" + ((new Date()) - this.start) + "ms.<br>";}
+	});
+})();
+//]]>
+</script>
+';
+		$style ='
+<style type="text/css">
+.fakeContainer { /* The parent container */
+    margin: 5px;
+    padding: 0px;
+    border: none;
+    width: 740px; /* Required to set */
+    height: 320px; /* Required to set */
+    overflow: hidden; /* Required to set */
+}
+</style>	
+';
+		//****************************************
+		$data['style']   = $style;
+		$data['style']  .= style('superTables.css');
+		$data['extras']  = $extras;
+
+		$data['content'] = $grid->output;
+		$data['filtro']  = $filter->output;
+
+		$data['title']  = heading('Aplicaci&oacute;n de Anticipos');
+		$data['script']   = script('jquery.js');
+		$data["script"]  .= script('superTables.js');
+		$data['head']  = $this->rapyd->get_head();
+		$this->load->view('view_ventanas', $data);
 	}
-	function dataedit()
- 	{
-		$this->rapyd->load("dataedit");		
-		$link=site_url('finanzas/apan/uanticipos');
-		$script ='
-		function sellupa(){
-			nombreintes.value="";
-			nombre.value="";
-			vtipo=$("#tipo").val();			
-				if(vtipo=="P"){
-					$("#tr_clipro").show();
-					$("#tr_clipro2").hide();
-					$("#tr_reinte2").show();
-					$("#tr_reinte").hide();
-					clipro2.value="";
-					reinte.value="";
-				}
-				if(vtipo=="C"){
-					$("#tr_clipro2").show();
-					$("#tr_clipro").hide();
-					$("#tr_reinte").show();
-					$("#tr_reinte2").hide();
-					clipro.value="";
-					reinte2.value="";
-				}		
+
+	function dataedit($tipo)	{
+		$this->rapyd->load('dataobject','datadetails');
+		$do = new DataObject("apan");
+		$title="";
+		if($tipo=='P'){
+			$do->rel_one_to_many('itppro', 'itppro', array('transac'=>'transac'));
+			$title='itppro';
 		}
-					
-		function ultimo(){			
-			$.ajax({
-				url: "'.$link.'",
-				success: function(msg){
-				  alert( "El ultimo codigo ingresado fue: " + msg );
-				}
-			});
+		else {
+			$do->rel_one_to_many('itccli', 'itccli', array('transac'=>'transac'));
+			$title='itccli';
 		}
-		$(function() {
-			$(".inputnum").numeric(".");
-			$("#tipo").change(function () { sellupa(); }).change();
-		}		
-		);
-		';
 
-		$clipro=array(
-		'tabla'   =>'sprv',
-		'columnas'=>array(
-		'proveed' =>'C&oacute;digo Proveedor',
-		'nombre'=>'Nombre',
-		'rif'=>'RIF'),
-		'filtro'  =>array('proveed' =>'C&oacute;digo Proveedor','nombre'=>'Nombre','rif'=>'RIF'),
-		'retornar'=>array('proveed'=>'clipro','nombre'=>'nombre'),
-		'titulo'  =>'Buscar Proveedor');
-		$CLIPRO =$this->datasis->modbus($clipro,'clipro');
 
-		$clipro2=array(
-		'tabla'   =>'scli',
-		'columnas'=>array(
-		'cliente' =>'C&oacute;digo Cliente',
-		'nombre'=>'Nombre',
-		'rifci'=>'RIF'),
-		'filtro'  =>array('cliente' =>'C&oacute;digo Cliente','nombre'=>'Nombre','rifci'=>'RIF'),
-		'retornar'=>array('cliente'=>'clipro2','nombre'=>'nombre'),
-		'titulo'  =>'Buscar Cliente');		
-		$CLIPRO2 =$this->datasis->modbus($clipro2,'clipro2');
+		$edit = new DataDetails('Aplicaci&oacute;n de Anticipos', $do);
+		$edit->back_url = site_url('finanzas/apan/filteredgrid');
+		$edit->set_rel_title($title,'Anticipo <#o#>');
 
-		$reinte=array(
-		'tabla'   =>'sprv',
-		'columnas'=>array(
-		'proveed' =>'C&oacute;digo Proveedor',
-		'nombre'=>'Nombre',
-		'rif'=>'RIF'),
-		'filtro'  =>array('proveed' =>'C&oacute;digo Proveedor','nombre'=>'Nombre','rif'=>'RIF'),
-		'retornar'=>array('proveed'=>'reinte','nombre'=>'nombreintes'),
-		'titulo'  =>'Buscar Proveedor');
-		$REINTE =$this->datasis->modbus($reinte,'reinte');
-		
-		$reinte2=array(
-		'tabla'   =>'scli',
-		'columnas'=>array(
-		'cliente' =>'C&oacute;digo Cliente',
-		'nombre'=>'Nombre',
-		'rifci'=>'RIF'),
-		'filtro'  =>array('cliente' =>'C&oacute;digo Cliente','nombre'=>'Nombre','rifci'=>'RIF'),
-		'retornar'=>array('cliente'=>'reinte2','nombre'=>'nombreintes'),
-		'titulo'  =>'Buscar Cliente');
-		$REINTE2 =$this->datasis->modbus($reinte2,'reinte2');
-
-		$edit = new DataEdit("Aplicaci&oacute;n de Anticipos", "apan");
-		$edit->back_url = site_url("finanzas/apan/filteredgrid");
-		$edit->script($script, "create");
-		$edit->script($script, "modify");
-		
-		$edit->post_process('insert','_post_insert');
-		$edit->post_process('update','_post_update');
-		$edit->post_process('delete','_post_delete');            		
-		
-		$lnum='<a href="javascript:ultimo();" title="Consultar ultimo codigo ingresado" onclick="">Consultar ultimo codigo</a>';
 		$edit->numero = new inputField("N&uacute;mero", "numero");
 		$edit->numero->mode="autohide";
-		$edit->numero->size =12;       
-		$edit->numero->rule="trim|required|callback_chexiste";
+		$edit->numero->size =12;
+		$edit->numero->rule="trim|required";
 		$edit->numero->maxlength=8;
-		$edit->numero->append($lnum);
-		
+
 		$edit->fecha = new DateonlyField("Fecha", "fecha");
 		$edit->fecha->size = 12;
 		$edit->fecha->rule="required|chfecha";
+		$edit->fecha->insertValue = date("Y-m-d");
 
 		$edit->tipo = new dropdownField("Tipo", "tipo");
 		$edit->tipo->option("C","Cliente");
-		$edit->tipo->option("C","Cliente");
 		$edit->tipo->option("P","Proveedor");
-		$edit->tipo->style="width:100px";		
+		$edit->tipo->style="width:100px";
 			
 		$edit->clipro =new inputField("Codigo", "clipro");
-		$edit->clipro->db->name="clipro";
 		$edit->clipro->rule='trim|required';
 		$edit->clipro->size =12;
 		$edit->clipro->readonly=true;
-		$edit->clipro->append($CLIPRO);		
-		$edit->clipro->append("Proveedor");		
-			
-		$edit->clipro2 =   new inputField("Codigo", "clipro2");
-		$edit->clipro2->db->name="clipro";
-		$edit->clipro2->rule='trim|required';
-		$edit->clipro2->size =12;
-		$edit->clipro2->readonly=true;                     
-		$edit->clipro2->append($CLIPRO2);
-		$edit->clipro2->append("Cliente");
-		
-		$edit->nombre =   new inputField("Nombre", "nombre");		
+
+		$edit->nombre =   new inputField("Nombre", "nombre");
 		$edit->nombre->size =30;
 		$edit->nombre->rule = "trim|strtoupper";
 		$edit->nombre->readonly=true;
-		
+
 		$edit->monto =    new inputField("Monto", "monto");
 		$edit->monto->size = 12;
 		$edit->monto->css_class='inputnum';
 		$edit->monto->rule='trim|numeric';
 		$edit->monto->maxlengxlength=0;
+		$edit->monto->rule='positive';
 
 		$edit->reinte =   new inputField("Convertido", "reinte");
-		$edit->reinte->db->name="reinte";
 		$edit->reinte->rule='trim|required';
 		$edit->reinte->size =12;
 		$edit->reinte->readonly=true;
-		$edit->reinte->append($REINTE);
-		$edit->reinte->append("Proveedor");		
-		
-		$edit->reinte2 =new inputField("Convertido", "reinte2");
-		$edit->reinte2->db->name="reinte";
-		$edit->reinte2->rule='trim|required';
-		$edit->reinte2->size =12;
-		$edit->reinte2->readonly=true;
-		$edit->reinte2->append($REINTE2);
-		$edit->reinte2->append("Cliente");
-		
+
 		$edit->nombreintes=new inputField("Nombre","nombreintes");
 		$edit->nombreintes->size=30;
 		$edit->nombreintes->readonly=true;
-				
+
 		$edit->observa1 = new inputField("Observaciones", "observa1");
 		$edit->observa1->rule='trim';
 		$edit->observa1->size =50;
 		$edit->observa1->maxlength=50;
-		
+
 		$edit->observa2 = new inputField("", "observa2");
 		$edit->observa2->rule='trim';
-	  $edit->observa2->size =50;
+		$edit->observa2->size =50;
 		$edit->observa2->maxlength=50;
-		
+
+		//Detalles itppro
+		if($tipo=='P'){
+			$edit->tipoppro = new inputField("Tipo <#o#>","tipoppro_<#i#>");
+			$edit->tipoppro->db_name = "tipoppro";
+			$edit->tipoppro->rel_id  = 'itppro';
+			$edit->tipoppro->rule='trim|required';
+			$edit->tipoppro->size =10;
+			$edit->tipoppro->readonly=true;
+
+			$edit->tipo_doc = new inputField("Tipo Documento <#o#>","tipo_doc_<#i#>");
+			$edit->tipo_doc->db_name = "tipo_doc";
+			$edit->tipo_doc->rel_id  = 'itppro';
+			$edit->tipo_doc->rule='trim|required';
+			$edit->tipo_doc->size =10;
+			$edit->tipo_doc->readonly=true;
+
+			$edit->itnumero = new inputField("N&uacute;mero <#o#>","itnumero_<#i#>");
+			$edit->itnumero->db_name = "numero";
+			$edit->itnumero->rel_id  = 'itppro';
+			$edit->itnumero->rule='trim|required';
+			$edit->itnumero->size =10;
+			$edit->itnumero->readonly=true;
+
+			$edit->itnumppro = new inputField("N&uacute;mero <#o#>","itnumppro_<#i#>");
+			$edit->itnumppro->db_name = "numppro";
+			$edit->itnumppro->rel_id  = 'itppro';
+			$edit->itnumppro->rule='trim|required';
+			$edit->itnumppro->size =10;
+			$edit->itnumppro->readonly=true;
+
+			$edit->itfechap = new DateonlyField("Fecha", "itfechap_<#i#>");
+			$edit->itfechap->db_name = "fecha";
+			$edit->itfechap->rel_id  = 'itppro';
+			$edit->itfechap->size = 12;
+			$edit->itfechap->rule="required|chfecha";
+			$edit->itfechap->insertValue = date("Y-m-d");
+
+			$edit->itmontop = new inputField("Monto <#o#>", "itmontop_<#i#>");
+			$edit->itmontop->db_name='monto';
+			$edit->itmontop->css_class='inputnum';
+			$edit->itmontop->rel_id   ='itppro';
+			$edit->itmontop->size=3;
+			$edit->itmontop->rule='positive';
+
+			$edit->itabonop = new inputField("Abono <#o#>", "itabonop_<#i#>");
+			$edit->itabonop->db_name='abono';
+			$edit->itabonop->css_class='inputnum';
+			$edit->itabonop->rel_id   ='itppro';
+			$edit->itabonop->size=3;
+			$edit->itabonop->rule='positive';
+		}
+		//Detalles itccli
+		if($tipo=='C'){
+			$edit->tipoccli = new inputField("Tipo <#o#>","tipoccli_<#i#>");
+			$edit->tipoccli->db_name = "tipoccli";
+			$edit->tipoccli->rel_id  = 'itccli';
+			$edit->tipoccli->rule='trim|required';
+			$edit->tipoccli->size =10;
+			$edit->tipoccli->readonly=true;
+
+			$edit->tipo_doc_c = new inputField("Tipo Documento <#o#>","tipo_doc_C<#i#>");
+			$edit->tipo_doc_c->db_name = "tipo_doc";
+			$edit->tipo_doc_c->rel_id  = 'itccli';
+			$edit->tipo_doc_c->rule='trim|required';
+			$edit->tipo_doc_c->size =10;
+			$edit->tipo_doc_c->readonly=true;
+
+			$edit->itnumero_c = new inputField("N&uacute;mero <#o#>","itnumero_c_<#i#>");
+			$edit->itnumero_c->db_name = "numero";
+			$edit->itnumero_c->rel_id  = 'itccli';
+			$edit->itnumero_c->rule='trim|required';
+			$edit->itnumero_c->size =10;
+			$edit->itnumero_c->readonly=true;
+
+			$edit->numccli = new inputField("N&uacute;mero <#o#>","numccli_<#i#>");
+			$edit->numccli->db_name = "numccli";
+			$edit->numccli->rel_id  = 'itccli';
+			$edit->numccli->rule='trim|required';
+			$edit->numccli->size =10;
+			$edit->numccli->readonly=true;
+
+			$edit->itfechac = new DateonlyField("Fecha", "itfechac_<#i#>");
+			$edit->itfechac->db_name = "fecha";
+			$edit->itfechac->rel_id  = 'itccli';
+			$edit->itfechac->size = 12;
+			$edit->itfechac->rule="required|chfecha";
+			$edit->itfechac->insertValue = date("Y-m-d");
+
+			$edit->itmontoc = new inputField("Monto <#o#>", "itmontoc_<#i#>");
+			$edit->itmontoc->db_name='monto';
+			$edit->itmontoc->css_class='inputnum';
+			$edit->itmontoc->rel_id   ='itccli';
+			$edit->itmontoc->size=3;
+			$edit->itmontoc->rule='positive';
+
+			$edit->itabonoc = new inputField("Abono <#o#>", "itabonoc_<#i#>");
+			$edit->itabonoc->db_name='abono';
+			$edit->itabonoc->css_class='inputnum';
+			$edit->itabonoc->rel_id   ='itccli';
+			$edit->itabonoc->size=3;
+			$edit->itabonoc->rule='positive';
+		}
+		///fin de detalles
 		$edit->buttons("modify", "save", "undo", "delete", "back");
 		$edit->build();
- 
-		$data['content'] = $edit->output;           
-    $data['title']   = "<h1>Aplicaci&oacute;n de Anticipos</h1>";        
-    $data["head"]    = script("jquery.pack.js").script("plugins/jquery.numeric.pack.js").script("plugins/jquery.floatnumber.js").$this->rapyd->get_head();
-    $this->load->view('view_ventanas', $data);  
+
+		$conten['form']  =&  $edit;
+		$data['content'] = $this->load->view('view_apan', $conten,true);
+		$data['title']   = "<h1>Aplicaci&oacute;n de Anticipos</h1>";
+		$data["script"]  = script("jquery.pack.js").script("plugins/jquery.numeric.pack.js").script("plugins/jquery.floatnumber.js");
+		$data["head"]    = $this->rapyd->get_head();
+		$this->load->view('view_ventanas', $data);
 	}
-	function _post_insert($do){
-		$codigo=$do->get('numero');
-		logusu('apan',"ANTICIPO $codigo CREADO");
+
+		function instalar(){
+			$sql="ALTER TABLE `apan`  ADD COLUMN `id` INT(10) NULL AUTO_INCREMENT AFTER `usuario`,  ADD PRIMARY KEY (`id`)";
+			$this->db->query($sql);
+
+		}
 	}
-	function _post_update($do){
-		$codigo=$do->get('numero');
-		logusu('apan',"ANTICIPO $codigo  MODIFICADO");
-	}
-	function _post_delete($do){
-		$codigo=$do->get('numero');
-		logusu('apan',"ANTICIPO $codigo  ELIMINADO ");
-	}
-	function chexiste($codigo){
-		$codigo=$this->input->post('numero');
-		$chek=$this->datasis->dameval("SELECT COUNT(*) FROM apan WHERE numero='$codigo'");
-		if ($chek > 0){
-			$this->validation->set_message('chexiste',"El anticipo $codigo ya existe");
-			return FALSE;
-		}else {
-  		return TRUE;
-		}	
-	}
-	function uanticipos(){
-		$consul=$this->datasis->dameval("SELECT numero FROM apan ORDER BY numero DESC");
-		echo $consul;
-	}
-	
-	function instalar(){
-		$sql="ALTER TABLE `apan`  ADD COLUMN `id` INT(10) NULL AUTO_INCREMENT AFTER `usuario`,  ADD PRIMARY KEY (`id`)";
-		$this->db->query($sql);
-		
-	}
-}
 ?>
