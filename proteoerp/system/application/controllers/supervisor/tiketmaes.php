e6fe428ba1cd2a0d195e8972277ca3010ece29ef
jud.prog <jud.prog@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-05-12 13:30:52 +0000

    cambios menores

--- proteoerp/system/application/controllers/supervisor/tiketmaes.php
+++ proteoerp/system/application/controllers/supervisor/tiketmaes.php
@@ -265,13 +265,13 @@ class tiketmaes extends Controller {
 		$data['style']  .= style('superTables.css');
 		$data['extras']  = $extras;	
 		
-		//$data['content'] = $grid->output;
+		$data['content'] = $grid->output;
 		$data['filtro']  = $filter->output;
 		
-		$data['script'] = script('jquery.js');
+		$data['script'].= script('jquery.js');
 		$data["script"].= script('superTables.js').script("jquery-1.2.6.pack.js").script("plugins/jquery.checkboxes.pack.js");
 
-		$data['content']= form_open('').$grid->output.form_close().$script;
+		//$data['content']= form_open('').$grid->output.form_close().$script;
 		$data['title']  = "<h1>Ticket $cliente</h1>";
 		$data["head"]   = $this->rapyd->get_head();
 		$this->load->view('view_ventanas', $data);
