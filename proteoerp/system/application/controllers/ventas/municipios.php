19cd9d8eb4966c3b5ce217dd415f2b0e59cc1a24
datasql <datasql@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-07-26 00:36:48 +0000

    Certificados

--- proteoerp/system/application/controllers/ventas/municipios.php
+++ proteoerp/system/application/controllers/ventas/municipios.php
@@ -5,49 +5,50 @@ class municipios extends Controller {
 		parent::Controller(); 
 		$this->load->library("rapyd");
 	}
-	function index()
-	{
+
+	function index(){
 		redirect("ventas/municipios/filteredgrid");
 	}
-		function filteredgrid(){
-			
+
+	function filteredgrid(){
+
 		$this->rapyd->load("datafilter","datagrid");
 		$this->rapyd->uri->keep_persistence();
 
 		$filter = new DataFilter("Filtro");
-		$select=array("a.codigo","a.nombre","b.nombre as mestado","c.pais as pais","b.zona as zona","a.estado as estado");		
+		$select=array("a.codigo","a.nombre","b.nombre as mestado","c.pais as pais","b.zona as zona","a.estado as estado");
 		$filter->db->select($select);
 		$filter->db->from("municipio AS a");
 		$filter->db->join("estado AS b","b.codigo=a.estado");
 		$filter->db->join("zona AS c","c.codigo=b.zona");
 		$filter->db->join("pais AS d","d.codigo=c.pais");
-		
+
 		$filter->codigo = new inputField("Codigo", "a.codigo");
 		$filter->codigo->size=15;
-			
+
 		$filter->nombre = new inputField("Nombre", "a.nombre");
 		$filter->nombre->size=25;
-	
+
 		$filter->pais = new dropdownField("Pa&iacute;s","pais");
 		$filter->pais->style = "width:150px";
 		$filter->pais->option("","Seleccionar");
 		$filter->pais->options("SELECT codigo, nombre FROM pais ORDER BY codigo");
 		$filter->pais->group = "Ubicaci&oacute;n";
 		$filter->pais->onchange = "get_zona();";
-		
+
 		$filter->zona = new dropdownField("Zona", "zona");
 		$filter->zona->style = "width:150px";
 		$filter->zona->option("","Seleccionar");
 		$filter->zona->options("SELECT codigo, nombre FROM zona ORDER BY nombre");
 		$filter->zona->group = "Ubicaci&oacute;n";
 		$filter->zona->onchange = "get_estados();";
-	
+
 		$filter->estado = new dropdownField("Estado","estado");
 		$filter->estado->style = "width:150px";
 		$filter->estado->option("","Seleccione una Zona");
 		$filter->estado->options("SELECT codigo, nombre FROM estado ORDER BY codigo");
 		$filter->estado->group = "Ubicaci&oacute;n";
-		
+
 		$filter->buttons("reset","search");
 		$filter->build();
 
@@ -63,7 +64,7 @@ class municipios extends Controller {
 
 		$grid->add("ventas/municipios/dataedit/create");
 		$grid->build();
-		
+
 		$link1=site_url('ventas/sclifyco/get_estados');
 		$link2=site_url('ventas/sclifyco/get_zona');
 		$data['script']  =<<<script
@@ -91,15 +92,14 @@ class municipios extends Controller {
 		}
 		</script>
 script;
-		
 		$data['content'] = $filter->output.$grid->output;
 		$data['title']   = "<h1>Municipios</h1>";
 		$data["head"]    = script("jquery.pack.js").script("plugins/jquery.numeric.pack.js").script("plugins/jquery.floatnumber.js");
 		$data["head"]   .= $this->rapyd->get_head();
 		$this->load->view('view_ventanas', $data);
 	}
-	function dataedit()
-	{ 
+
+	function dataedit(){
 		$this->rapyd->load("dataedit");
 
 		$edit = new DataEdit("Agregar", "municipio");
@@ -132,18 +132,18 @@ script;
 			$edit->estado->options("SELECT codigo, nombre FROM estado ORDER BY codigo");
 		}else{
 			$edit->estado->option("","Seleccione una Zona");
-		}	
+		}
 		$edit->estado->group = "Ubicaci&oacute;n";
 		$edit->estado->when=array('create','modify');
-				
+
 		$edit->nombre = new inputField("Nombre", "nombre");
 		$edit->nombre->size=60;
 		$edit->nombre->rule="trim|required|strtoupper";
 		$edit->nombre->maxlength=90;
-		  
+
 		$edit->buttons("modify", "save", "undo", "delete", "back");
-  	$edit->build();
-  	
+		$edit->build();
+
 		$link1=site_url('ventas/sclifyco/get_estados');
 		$link2=site_url('ventas/sclifyco/get_zona');
 		$data['script']  =<<<script
@@ -171,12 +171,12 @@ script;
 		}
 		</script>
 script;
-		
-		$data['content'] = $edit->output;           
-    $data['title']   = "<h1>Municipios</h1>";        
+
+		$data['content'] = $edit->output;
+		$data['title']   = "<h1>Municipios</h1>";
 		$data["head"]    = script("jquery.pack.js").script("plugins/jquery.numeric.pack.js").script("plugins/jquery.floatnumber.js");
 		$data["head"]   .= $this->rapyd->get_head();
-    $this->load->view('view_ventanas', $data);  
+		$this->load->view('view_ventanas', $data);  
 	}
 }
 ?>
