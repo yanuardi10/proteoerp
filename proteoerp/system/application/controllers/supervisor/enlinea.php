c61c0e76a2fc353e63e5afd8ec4a449f55b22500
enderochoa <enderochoa@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-08-02 23:27:34 +0000

    modificacion del metodo ventana

--- proteoerp/system/application/controllers/supervisor/enlinea.php
+++ proteoerp/system/application/controllers/supervisor/enlinea.php
@@ -14,39 +14,78 @@ class Enlinea extends Controller{
             $this->rapyd->load("datagrid");
 
             $uri = anchor_popup('http://<#url#>/<#proteo#>','<#sucursal#>');
-    
+            
             function linea($url,$proteoerp){
-                $senal = http_get("http://$url"."/".$proteoerp."/supervisor/enlinea/senal",array("timeout"=>5,"connecttimeout"=>5));
-                if($senal=='SI'){
-                    return 'SI';
-                }else{
-                    return 'NO';
+                $CI =& get_instance();
+                
+                $CI->load->library('xmlrpc');
+                $CI->xmlrpc->xmlrpc_defencoding=$CI->config->item('charset');
+                
+                $server_url = reduce_double_slashes("http://$url"."/$proteoerp/".'rpcserver');
+                $CI->xmlrpc->server($server_url , 80);
+                $CI->xmlrpc->method('ventanainf');
+                
+                $request = array('a');
+                $CI->xmlrpc->request($request);
+                
+                if(!$CI->xmlrpc->send_request())
+                    return 'No Disponible';
+                else{
+                    $ima="http://".$url."/".$proteoerp."/supervisor/enlinea/ventana";
+                    return "<iframe src='$ima' width='500px' height='200px'/></iframe>";
                 }
             }
     
-            $grid = new DataGrid("Lista de Sucursales");
+            $grid = new DataGrid();
             $grid->db->from('sucu');
-            $grid->order_by("codigo","asc");
+            $grid->order_by("codigo","desc");
             $grid->per_page=15;
-            $grid->use_function('linea');
+            $grid->use_function('linea','ver');
     
-            $grid->column("Sucursal"    ,$uri                                     );
-            $grid->column("Ver"         ,'<linea><#url#>|<#proteoerp#></linea>'   );
+            $grid->column("Sucursal"    ,$uri                                       );
+            $grid->column("En Linea"    ,'<linea><#url#>|<#proteo#></linea>'        );
+            
             
             $grid->build();
+            
+            //$out .= '<div class=\'box col1\'><h3>'.htmlentities($panel).'</h3>';
     
-            $data['content'] = $grid->output;
-            $data['title']   = heading('Sucursal');
-            $data['head']    = $this->rapyd->get_head();
+            $conten['grid' ]  =$grid->output;
+            $data['content'] = $this->load->view('view_enlinea', $conten,true);
+            $data['title']   = heading('Informacion de Sucursales');
+            $data["head"]    = $this->rapyd->get_head();
             $this->load->view('view_ventanas', $data);
         }
         
         function ventana(){
-            echo "HOLA";
+            $this->rapyd->load("datagrid");
+            
+            $sql="SELECT nombre descrip,valor FROM valores LIMIT 5";
+            $sql=$this->db->query($sql);
+            $arr=$sql->result_array($sql);
+            
+            $grid = new DataGrid("",$arr);
+            
+            $grid->order_by("codigo","desc");
+            $grid->per_page=15;
+            $grid->use_function('linea','ver');
+    
+            $grid->column("Descripci&oacute;n" ,'descrip'   );
+            $grid->column("Valor"              ,'valor'     );
+
+            $grid->build();
+            
+            $data['content'] =$grid->output;
+            $data["head"]    = $this->rapyd->get_head();
+            $this->load->view('view_ventanas_sola', $data);
         }
         
         function senal(){
-            echo "SI";
+            echo base_url().'images/N.gif';
+        }
+        
+        function prueba(){
+            
         }
 }
 ?>
