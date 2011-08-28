948b10f440ddbeec8621c08a269a6eaa12ea2924
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-08-28 13:35:44 +0000

    -Modulos para colombia

--- proteoerp/system/application/controllers/ventas/pfac.php
+++ proteoerp/system/application/controllers/ventas/pfac.php
@@ -455,6 +455,113 @@ class pfac extends validaciones{
 		$this->load->view('view_ventanas_sola', $data);
 	}
 
+	function posmayor(){
+		$this->rapyd->load('dataobject','datadetails');
+
+		$mSCLId=array(
+		'tabla'   =>'scli',
+		'columnas'=>array(
+			'cliente' =>'C&oacute;digo Cliente',
+			'nombre'=>'Nombre', 
+			'cirepre'=>'Rif/Cedula',
+			'dire11'=>'Direcci&oacute;n',
+			'tipo'=>'Tipo'),
+		'filtro'  =>array('cliente'=>'C&oacute;digo Cliente','nombre'=>'Nombre'),
+		'retornar'=>array('cliente'=>'cod_cli','nombre'=>'nombre','rifci'=>'rifci',
+					'dire11'=>'direc'),
+		'titulo'  =>'Buscar Cliente',
+		'script'  => array('post_modbus_scli()'));
+		$boton =$this->datasis->modbus($mSCLId);
+
+		$query = $this->db->query("SELECT tipo,nombre FROM tarjeta ORDER BY tipo");
+		foreach ($query->result() as $row){
+			$sfpa[$row->tipo]=$row->nombre;
+		}
+
+		$tban['']='Banco';
+		$query = $this->db->query("SELECT cod_banc,nomb_banc FROM tban WHERE cod_banc<>'CAJ' ORDER BY nomb_banc");
+		foreach ($query->result() as $row){
+			$tban[$row->cod_banc]=$row->nomb_banc;
+		}
+
+		$conten=array();
+		$conten['sfpa']  = $sfpa;
+		$conten['tban']  = $tban;
+		$data['content'] = $this->load->view('view_pos_pfac_mayor', $conten,true);
+		$data['title']   = '';
+		$data['head']    = style('redmond/jquery-ui-1.8.1.custom.css');
+		$data['head']   .= style('ui.jqgrid.css');
+		$data['head']   .= style('ui.multiselect.css');
+		$data['head']   .= script('jquery.js');
+		$data['head']   .= script('interface.js');
+		$data['head']   .= script('jquery-ui.js');
+		$data['head']   .= script('plugins/jquery.ui.autocomplete.autoSelectOne.js');
+		$data['head']   .= script('jquery.layout.js');
+		$data['head']   .= script('i18n/grid.locale-sp.js');
+		$data['head']   .= script('ui.multiselect.js');
+		$data['head']   .= script('jquery.jqGrid.min.js');
+		$data['head']   .= script('jquery.tablednd.js');
+		$data['head']   .= script('jquery.contextmenu.js');
+		$data['head']   .= script('plugins/jquery.numeric.pack.js');
+		$data['head']   .= script('plugins/jquery.floatnumber.js');
+		$data['head']   .= phpscript('nformat.js');
+
+		$this->load->view('view_ventanas_sola', $data);
+	}
+
+	// Busca Productos para autocomplete
+	function buscasinv(){
+		$data = '{[ ]}';
+		$mid  = $this->input->post('q');
+		$cod  = $this->input->post('codigo');
+		$scli = $this->input->post('cod_cli');
+		if(strlen($scli)==0){ echo $data; return; }
+		
+		$sql='SELECT mmargen FROM scli WHERE cliente='.$this->db->escape($scli);
+		$scli_margen=$this->datasis->dameval($sql);
+		$scli_margen=$scli_margen/100;
+
+		$qdb  = $this->db->escape('%'.$mid.'%');
+		$qba  = $this->db->escape($mid);
+		$coddb= $this->db->escape($cod);
+
+		$pp='precio1*(1-(mmargen/100))*(1-'.$scli_margen.')';
+
+		if($mid !== false){
+			$retArray = $retorno = array();
+
+			if(preg_match('/\+(?P<cana>\d+)/', $mid, $matches)>0 && $cod!==false){
+				$mSQL="SELECT TRIM(a.descrip) AS descrip, TRIM(a.codigo) AS codigo, a.$pp AS precio, a.iva,a.existen
+				FROM sinv AS  a
+				WHERE a.codigo=$coddb LIMIT 1";
+				$cana=$matches['cana'];
+			}else{
+				$mSQL="SELECT DISTINCT TRIM(a.descrip) AS descrip, TRIM(a.codigo) AS codigo, a.$pp AS precio, a.iva,a.existen
+				FROM sinv AS a
+				LEFT JOIN barraspos AS b ON a.codigo=b.codigo
+				WHERE (a.codigo LIKE $qdb OR a.descrip LIKE  $qdb OR a.barras LIKE $qdb OR b.suplemen=$qba) AND a.activo='S'
+				ORDER BY a.descrip LIMIT 10";
+				$cana=1;
+			}
+
+			$query = $this->db->query($mSQL);
+			if ($query->num_rows() > 0){
+				foreach( $query->result_array() as  $row ) {
+					$retArray['label']   = '('.$row['codigo'].') '.$row['descrip'].' '.$row['precio'].' Bs. - '.$row['existen'];
+					$retArray['codigo']  = $row['codigo'];
+					$retArray['cana']    = $cana;
+					$retArray['precio']  = round($row['precio'],2);
+					$retArray['descrip'] = $row['descrip'];
+					//$retArray['descrip'] = wordwrap($row['descrip'], 25, '<br />');
+					$retArray['iva']     = $row['iva'];
+					array_push($retorno, $retArray);
+				}
+				$data = json_encode($retorno);
+	        }
+		}
+		echo $data;
+	}
+
 	function creapfac(){
 		foreach($_POST as $ind=>$val){
 			$matches=array();
