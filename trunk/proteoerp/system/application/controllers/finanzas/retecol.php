fa508fa7fe46267ff97128b4b936f704a5f5be21
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-08-30 06:10:59 +0000

    -Gastos con las retenciones correspondientes

--- trunk/proteoerp/system/application/controllers/finanzas/retecol.php
+++ trunk/proteoerp/system/application/controllers/finanzas/retecol.php
@@ -9,12 +9,14 @@
 		$this->load->helper('text');
 		//rapyd library
 		$this->load->library("rapyd");
-   }
-   function index(){
-    	$this->datasis->modulo_id(515,1);
-    	redirect("finanzas/retecol/filteredgrid");
-    }
- 	function filteredgrid(){
+	}
+
+	function index(){
+		$this->datasis->modulo_id(515,1);
+		redirect("finanzas/retecol/filteredgrid");
+	}
+
+	function filteredgrid(){
 		$this->rapyd->load("datafilter","datagrid");
 		$this->rapyd->uri->keep_persistence();
 
@@ -41,11 +43,11 @@
 
 		$grid->add('finanzas/retecol/dataedit/create');
 		$grid->build();
-		
+
 		$data['content'] = $filter->output.$grid->output;
 		$data['title']   = "<h1>Retenciones</h1>";
-		$data["head"]    = $this->rapyd->get_head();
-		$this->load->view('view_ventanas', $data);	
+		$data['head']    = $this->rapyd->get_head();
+		$this->load->view('view_ventanas', $data);
 	}
 
 	function dataedit(){
@@ -57,7 +59,7 @@
 		});
 		';
 
-		$edit = new DataEdit("Retenciones", "rete");		
+		$edit = new DataEdit("Retenciones", "rete");
 		$edit->back_url = site_url("finanzas/retecol/filteredgrid");
 		$edit->script($script, "create");
 		$edit->script($script, "modify");
@@ -71,42 +73,42 @@
 		$edit->codigo->size =7;
 		$edit->codigo->maxlength=4;
 		$edit->codigo->rule ="required|callback_chexiste";
-		
+
 		$edit->activida = new inputField("Pago de", "activida");
 		$edit->activida->size =55;
 		$edit->activida->maxlength=45;
 		$edit->activida->rule= "strtoupper|required";
-		
+
 		$edit->tipo =  new dropdownField("Tipo", "tipo");
 		$edit->tipo->option("JD","Juridico");
 		$edit->tipo->option("NR","Natural");
 		$edit->tipo->style='width:160px';
-		
+
 		$edit->tipocol =  new dropdownField("Renta", "tipocol");
 		$edit->tipocol->option('D','Declarante');
 		$edit->tipocol->option('N','No declarante');
 		$edit->tipocol->style='width:160px';
-		
+
 		$edit->base1 = new inputField("Base Imponible", "base1");
 		$edit->base1->size =13;
 		$edit->base1->maxlength=9;
 		$edit->base1->css_class='inputnum';
 		$edit->base1->rule='numeric';
-		
+
 		$edit->tari1 =new inputField("Porcentaje de Retenci&oacute;n", "tari1");
 		$edit->tari1->size =13;
 		$edit->tari1->maxlength=10;
 		$edit->tari1->css_class='inputnum';
 		$edit->tari1->rule='numeric';
-		
+
 		$edit->pama1 = new inputField("Para pagos mayores a", "pama1");
 		$edit->pama1->size =13;
 		$edit->pama1->maxlength=13;
 		$edit->pama1->css_class='inputnum';
 		$edit->pama1->rule='numeric';
 
-	  $edit->la1 = new containerField('alert','Unidades Tributarias');
-	  $edit->la1->in='pama1';
+		$edit->la1 = new containerField('alert','Unidades Tributarias');
+		$edit->la1->in='pama1';
 
 		$edit->ut = new inputField("Unidades Tributarias", 'ut');
 		$edit->ut->size =13;
@@ -122,39 +124,47 @@
 
 		$smenu['link']=barra_menu('515');
 		$data['smenu'] = $this->load->view('view_sub_menu', $smenu,true);
-		$data['content'] = $edit->output;           
-   $data['title']   = "<h1>Retenciones</h1>";        
-    $data["head"]    = script("jquery.pack.js").script("plugins/jquery.numeric.pack.js").script("plugins/jquery.floatnumber.js").$this->rapyd->get_head();
-    $this->load->view('view_ventanas', $data);  
- 	}
+		$data['content'] = $edit->output;
+		$data['title']   = "<h1>Retenciones</h1>";
+		$data['head']    = script("jquery.pack.js").script("plugins/jquery.numeric.pack.js").script("plugins/jquery.floatnumber.js").$this->rapyd->get_head();
+		$this->load->view('view_ventanas', $data);  
+	}
 
- 	function _post_insert($do){
+	function _post_insert($do){
 		$codigo=$do->get('codigo');
 		$nombre=$do->get('activida');
 		logusu('rete',"RETENCION $codigo $nombre CREADO");
 	}
+
 	function _post_update($do){
 		$codigo=$do->get('codigo');
 		$nombre=$do->get('activida');
 		logusu('rete',"RETENCION $codigo $nombre MODIFICADO");
 	}
+
 	function _post_delete($do){
 		$codigo=$do->get('codigo');
 		$nombre=$do->get('activida');
 		logusu('rete',"RETENCION $codigo $nombre  ELIMINADO ");
 	}
+
 	function chexiste($codigo){
 		$codigo=$this->input->post('codigo');
-		//echo 'aquiii'.$fecha;
 		$chek=$this->datasis->dameval("SELECT COUNT(*) FROM rete WHERE codigo='$codigo'");
 		if ($chek > 0){
 			$activida=$this->datasis->dameval("SELECT activida FROM rete WHERE codigo='$codigo'");
 			$this->validation->set_message('chexiste',"La retencion $codigo ya existe para la actividad $activida");
 			return FALSE;
 		}else {
-  		return TRUE;
-		}	
+			return TRUE;
+		}
 	}
-	
+
+	function instalar(){
+		if (!$this->db->field_exists('tipocol','mgas')) {
+			$mSQL='';
+			$this->db->simple_query($mSQL);
+		}
+	}
 }
 ?>
