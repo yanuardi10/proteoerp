664a6c00a00a8e7312e7ec0eaf99c9e509b77fd4
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2011-11-30 05:15:54 +0000

    -Mejoras en sitemslog

--- trunk/proteoerp/system/application/controllers/supervisor/sitemslog.php
+++ trunk/proteoerp/system/application/controllers/supervisor/sitemslog.php
@@ -1,5 +1,6 @@
-<?php
-class sitemslog extends Controller {
+<?php require_once(BASEPATH.'application/controllers/ia.php');
+//class sitemslog extends Controller {
+class sitemslog extends ia {
 	function sitemslog(){
 		parent::Controller();
 		$this->load->library('rapyd');
@@ -99,7 +100,6 @@ class sitemslog extends Controller {
 		$grid->per_page=20;
 
 		$grid->column("Codigo"   ,"codigoa" ,"align=left");
-		$grid->column("Codigo"   ,"codigoa" ,"align=left");
 		$grid->column("Descripci&oacute;n"  ,"desca","align=left");
 		$grid->column("Cantidad" ,"cana"    ,"align=center");
 		$grid->column("Precio  " ,"preca"   ,"align=right");
@@ -116,5 +116,115 @@ class sitemslog extends Controller {
 		$data['title']   = heading('Detalle');
 		$data['head']    = $this->rapyd->get_head();
 		$this->load->view('view_ventanas', $data);
+	}
+
+	function analisis($mes=null){
+		if(empty($mes)) return '';
+		$this->load->helper('date');
+		$udia=days_in_month(substr($mes,4),substr($mes,0,4));
+		$fdesde=$mes.'01';
+		$fhasta=$mes.$udia;
+
+		$mSQL="SELECT a.usuario,COUNT(*) AS reg , a.tipo FROM sitemslog AS a
+		WHERE a.fecha BETWEEN $fdesde AND $fhasta
+		GROUP BY a.usuario,a.tipo
+		UNION ALL
+		SELECT usuario,COUNT(*) AS reg,'FACTURADO' FROM sitems 
+		WHERE fecha BETWEEN $fdesde AND $fhasta GROUP BY usuario";
+
+		$query = $this->db->query($mSQL);
+		foreach ($query->result() as $row){
+			$campo= strtolower($row->tipo);
+			$usr  = $this->db->escape($row->usuario);
+			$reg  = $this->db->escape($row->reg);
+
+			$sql="INSERT INTO sitemsest (periodo,usuario,$campo) VALUES ($mes,$usr,$reg) ON DUPLICATE KEY UPDATE $campo=$reg";
+			$ban=$this->db->simple_query($sql);
+		}
+	}
+
+	function estudio(){
+		$this->rapyd->load('datafilter','datagrid');
+		$this->rapyd->uri->keep_persistence();
+
+		$filter = new DataFilter(' ', 'sitemsest');
+
+		$filter->periodo = new inputField('Per&iacute;odo','periodo');
+		$filter->periodo->rule      ='max_length[6]';
+		$filter->periodo->size      =8;
+		$filter->periodo->maxlength =6;
+
+		$filter->buttons('reset', 'search');
+		$filter->build();
+
+		function estu($p1,$p2,$p3,$p4){
+			$CI =& get_instance();
+			$neuro=$CI->neurona('sitemslog',$p1,$p2,$p3,$p4);
+			if($neuro>0){
+				$rt='<b style="color:red">Activo</b>';
+			}else{
+				$rt='<b style="color:green">Inactivo</b>';
+			}
+			return $rt;
+		}
+
+		$grid = new DataGrid('');
+		$grid->use_function('estu');
+		$grid->order_by('usuario');
+		$grid->per_page = 40;
+
+		$url=anchor('supervisor/sitemslog/maprende/<#id#>','Aprender');
+		$grid->column_orderby('Usuario'  ,'usuario','usuario','align="left"');
+		$grid->column_orderby('Per&iacute;odo'  ,'periodo','periodo','align="left"');
+		$grid->column_orderby('Agregado'  ,'<nformat><#agrega#></nformat>','agrega','align="right"');
+		$grid->column_orderby('Eliminado' ,'<nformat><#eliminado#></nformat>','eliminado','align="right"');
+		$grid->column_orderby('Modificado','<nformat><#modifica#></nformat>','modifica','align="right"');
+		$grid->column_orderby('Facturado' ,'<nformat><#facturado#></nformat>','facturado','align="right"');
+		$grid->column('Activo' ,'<estu><#agrega#>|<#eliminado#>|<#modifica#>|<#facturado#></estu>','align="center"');
+		$grid->column('Entrenar' ,$url,'align="center"');
+		$grid->build();
+
+		//$data['filtro']  = $filter->output;
+		$data['content'] = $filter->output.$grid->output;
+		$data['head']    = $this->rapyd->get_head().script('jquery.js');
+		$data['title']   = heading('Estudio del movimiento de cajas');
+		$this->load->view('view_ventanas', $data);
+	}
+
+	function maprende($id){
+		$this->E=0.5;
+		$url='supervisor/sitemslog/estudio';
+		$this->rapyd->uri->keep_persistence();
+		$persistence = $this->rapyd->session->get_persistence($url, $this->rapyd->uri->gfid);
+		$back= (isset($persistence['back_uri'])) ?$persistence['back_uri'] : $url;
+		$row=$this->datasis->damerow('SELECT * FROM sitemsest WHERE id='.$this->db->escape($id));
+
+		$neuro=$this->neurona('sitemslog',$row['agrega'],$row['eliminado'],$row['modifica'] ,$row['facturado']);
+		$learn = ($neuro>0)? -1: 1;
+
+		$this->aprende('sitemslog',array($row['agrega'],$row['eliminado'],$row['modifica'],$row['facturado']),$learn);
+
+		redirect($back);
+	}
+
+	function instalar(){
+		if(!$this->db->table_exists('sitemsest')){
+			$mSQL="CREATE TABLE `sitemsest` (
+			`id` INT(10) NOT NULL AUTO_INCREMENT,
+			`usuario` VARCHAR(50) NOT NULL DEFAULT '0',
+			`periodo` VARCHAR(6) NOT NULL DEFAULT '0',
+			`agrega` INT(10) NOT NULL DEFAULT '0',
+			`eliminado` INT(10) NOT NULL DEFAULT '0',
+			`modifica` INT(10) NOT NULL DEFAULT '0',
+			`facturado` INT(10) NOT NULL DEFAULT '0',
+			PRIMARY KEY (`id`),
+			UNIQUE INDEX `usuario_periodo` (`usuario`, `periodo`)
+			)
+			COMMENT='Resumen de sitems log'
+			COLLATE='latin1_swedish_ci'
+			ENGINE=MyISAM
+			AUTO_INCREMENT=1";
+			$this->db->simple_query($mSQL);
+		}
 	}
 }
