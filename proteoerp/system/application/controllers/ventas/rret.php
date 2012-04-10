ef624641903ed86741f4e8c6f550a621eef7aa0c
datasql <datasql@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2012-04-10 22:10:57 +0000

    Retiros de caja

--- proteoerp/system/application/controllers/ventas/rret.php
+++ proteoerp/system/application/controllers/ventas/rret.php
@@ -236,7 +236,7 @@ class Rret extends validaciones {
 					}
 				}
 			}
-			redirect('ventas/rret/retirohecho/'.$cajero.'/'.$monto);
+			redirect('ventas/rret/retirohechocaj/'.$cajero.'/'.$monto);
 		}
 
 		$attr=array(
@@ -258,6 +258,15 @@ class Rret extends validaciones {
 
 		$this->load->view('view_ventanas', $data);
 	}
+
+	function retirohechocaj($cajero,$monto){
+		$data['content'] = "<H1>Retiro realizado al cajero <b>$cajero</b></H1> <H1>por un monto de <b>".nformat($monto)."</b></H1>".br();
+		$data['content'].= '<center>'.anchor('ventas/rret/retirocaj','Regresar').'</center>';
+		$data['title']   = '<h1>Retiros de Caja</h1>';
+		$data['head']    = $this->rapyd->get_head();
+		$this->load->view('view_ventanas', $data);
+	}
+
 
 	function retirohecho($cajero,$monto){
 		$data['content'] = "Retiro realizado al cajero <b>$cajero</b> por un monto de <b>".nformat($monto)."</b>".br();
@@ -282,4 +291,187 @@ class Rret extends validaciones {
 
 		$this->db->simple_query($mSQL);
 	}
+
+	function retirocaj(){
+		//Para cuando venga de datasis y sin parametros
+		$cajero = $this->datasis->dameval("SELECT cajero FROM usuario WHERE us_codigo=".trim($this->db->escape($this->session->userdata('usuario')))." ");
+		if( is_null($cajero) ){
+			echo "Cajero Invalido";
+			return;
+		}
+
+		$this->rapyd->load('dataform');
+
+		$form = new DataForm('ventas/rret/retiro/'.$cajero.'/process');
+
+		$attr=array(
+			'class'  => 'ui-state-default ui-corner-all',
+			'onclick'=> "javascript:window.location='".site_url('ventas/rcaj/filteredgrid')."'",
+			'value'  => 'Regresar'
+		);
+
+		//Inicio del efectivo
+		$denomi=array();
+		$c_efe=0;
+		$query = $this->db->query('SELECT a.tipo,a.denomina,b.cambiobs*a.denomina equivalencia,a.nombre FROM monebillet a JOIN mone b ON a.moneda=b.moneda ORDER BY a.tipo,a.moneda,a.denomina DESC');
+		foreach ($query->result() as $i=>$row){
+			$arr=array('EFE','cEFE');
+			$nn=$arr[0];
+			$denomi[$nn]=$row->denomina;
+			$c_efe++;
+			foreach($arr AS $o=>$nobj){
+				$obj = $nobj.$i;
+				$denomi[$obj]=$row->denomina;
+				$form->$obj = new inputField($row->nombre, $obj);
+				$form->$obj->group=($row->tipo=='BI') ? 'Billetes': 'Monedas';
+				$form->$obj->size=5+5*$o;
+				$form->$obj->style='text-align:right';
+				$form->$obj->rule='numeric';
+				$form->$obj->autocomplete=false;
+				if($o==1){
+					$form->$obj->in=$sobj;
+					$form->$obj->readonly=true;
+				}else{
+					$form->$obj->css_class='cefectivo';
+				}
+				$sobj=$obj;
+			}
+		}
+		$arr=array('OEFE'=>'Otras denominaciones');
+		foreach($arr AS $obj=>$titulo){
+			$form->$obj = new inputField($titulo, $obj);
+			$form->$obj->style='text-align:right';
+			$form->$obj->css_class='efectivo';
+			$form->$obj->rule='numeric';
+			$form->$obj->size=10;
+			$form->$obj->autocomplete=false;
+		}
+		//Fin del efectivo
+
+		//Inicio otras formas de pago
+		$c_otrp=0;
+		$mSQL='SELECT a.tipo,a.nombre FROM tarjeta a WHERE a.tipo NOT IN (\'EF\',\'NC\',\'ND\', \'DE\',\'IR\',\'DP\')';
+		$query = $this->db->query($mSQL);
+		foreach ($query->result() as $i=>$row){
+			$c_otrp++;
+			$arr=array('cOTR');
+			foreach($arr AS $o=>$nobj){
+				$obj = $nobj.$row->tipo;
+				$form->$obj = new inputField($row->nombre, $obj);
+				$form->$obj->style='text-align:right';
+				$form->$obj->size=10;
+				$form->$obj->rule='numeric';
+				$form->$obj->autocomplete=false;
+			}
+		}
+		//Fin otras formas de pago
+
+		//Inicio tabla Resumen
+		$arr=array('TOTR'=>'Total de otras formas de pago','TEFE'=>'Total Efectivo','TGLOB'=>'Total Global');
+		foreach($arr AS $obj=>$titulo){
+			$form->$obj = new inputField($titulo, $obj);
+			$form->$obj->style='text-align:right';
+			$form->$obj->css_class='efectivo';
+			$form->$obj->rule='numeric';
+			$form->$obj->readonly=true;
+			$form->$obj->size=10;
+			$form->$obj->autocomplete=false;
+		}
+
+		$form->$obj->readonly=false;
+		//$form->$obj->rule='required';
+		$form->$obj->insertValue='';
+		//fin Resumen
+
+		$form->submit('btnsubmit','Hacer retiro');
+		$form->build_form();
+
+		$this->rapyd->jquery[]='var denomi='.json_encode($denomi).';';
+		$this->rapyd->jquery[]='$(":input:not(input[name^=\'cOT\'])").numeric(".");';
+		$this->rapyd->jquery[]='$(\'input[name^="cEFE"]\').bind("keyup",function() { gtotal(); });';
+		$this->rapyd->jquery[]='$(\'input[name^="cOTR"]\').bind("keyup",function() { gtotal(); });';
+		$this->rapyd->jquery[]='$(\'input[name^="FEFE"]\').bind("keyup",function() { gtotal(); });';
+		$this->rapyd->jquery[]='$(\'input[name^="OEFE"]\').bind("keyup",function() { gtotal(); });';
+		$this->rapyd->jquery[]='$("#df1").submit(function() { return confirm("Estas seguro de realizar el Retiro de caja?"); })';
+		$this->rapyd->jquery[]='$(".cefectivo").bind("keyup",
+			function() {
+				obj=this.name;
+				mul=eval("denomi."+obj);
+				valor=$(this).val();
+				$("#c"+obj).val(roundNumber(mul*valor,2));
+				gtotal();
+			});';
+		$this->rapyd->jquery[]='function gtotal(){
+			TEFE=TOTR=0;
+			$(\'input[name^="cEFE"]\').each(function(i,e){
+				if($(this).val().length>0)    TEFE = TEFE+parseFloat($(this).val());
+			});
+			$(\'input[name^="cOTR"]\').each(function(i,e){
+				if($(this).val().length>0)
+					TOTR = TOTR+parseFloat($(this).val());
+			});
+			if($("#OEFE").val().length>0) TEFE=TEFE+parseFloat($("#OEFE").val());
+
+			$("#TEFE").val(roundNumber(TEFE,2));
+			$("#TOTR").val(roundNumber(TOTR,2));
+			$("#TGLOB").val(roundNumber(TOTR+TEFE,2));
+		}';
+		$this->rapyd->jquery[]='$("input[name^=\'cOT\']").calculator( {showOn: "button",useThemeRoller:true,onClose: function(value, inst) { gtotal(); }, onClose: function(value, inst) { gtotal(); }} );';
+
+		//hace el retiro de caja
+		if ($form->on_success()){
+			//$dbcajero = $this->db->escape($cajero);
+			$fecha=date('Ymd');
+			$arr=array();
+
+			$mSQL='SELECT tipo FROM tarjeta';
+			$query = $this->db->query($mSQL);
+			if ($query->num_rows() > 0){
+				$monto=0;
+
+				foreach ($query->result() as $row){
+					if($row->tipo == 'EF'){
+						$nobj='TEFE';
+					}else{
+						$nobj='cOTR'.$row->tipo;
+					}
+
+					if(isset($form->$nobj)){
+						$recibido=$form->$nobj->newValue;
+						if($recibido>0){
+							$arr['tipo']   = $row->tipo;
+							$arr['cajero'] = $cajero;
+							$arr['monto']  = $recibido;
+							$arr['fecha']  = $fecha;
+							$monto+=$recibido;
+
+							$mSQL = $this->db->insert_string('rret', $arr);
+							$this->db->simple_query($mSQL);
+						}
+					}
+				}
+			}
+			redirect('ventas/rret/retirohecho/'.$cajero.'/'.$monto);
+		}
+
+		$attr=array(
+			'class'  => 'ui-state-default ui-corner-all',
+			'onclick'=> "javascript:window.location='".site_url('ventas/rret/retirocaj')."'",
+			'value'  => 'Regresar'
+		);
+
+		$cont['form']    = &$form;
+		$cont['c_efe']   = $c_efe*2;
+		$cont['c_otrp']  = $c_otrp;
+		$cont['regresa'] = form_button($attr,'Regresar');
+		$data['content'] = $this->load->view('view_rret',$cont, true);
+		$data['title']   = heading('Retiro para el cajero '.$cajero);
+		$data['head']    = $this->rapyd->get_head().phpscript('nformat.js').style('jquery.calculator.css');
+		$data['head']   .= script('plugins/jquery.numeric.pack.js').script('plugins/jquery.floatnumber.js');
+		$data['head']   .= script('plugins/jquery.calculator.min.js');
+		$data['head']   .= script('plugins/jquery.calculator-es.js');
+
+		$this->load->view('view_ventanas', $data);
+	}
+
 }
