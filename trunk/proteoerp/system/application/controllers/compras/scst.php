f41759157651006115d3eb718b206ca48424efc0
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2012-08-17 15:48:31 +0000

    -Arreglo en la redireccion de compras

--- trunk/proteoerp/system/application/controllers/compras/scst.php
+++ trunk/proteoerp/system/application/controllers/compras/scst.php
@@ -68,19 +68,19 @@ jQuery("#boton1").click( function(){
 	var id = jQuery("#newapi'. $param['grids'][0]['gridname'].'").jqGrid(\'getGridParam\',\'selrow\');
 	if (id)	{
 		var ret = jQuery("#newapi'. $param['grids'][0]['gridname'].'").jqGrid(\'getRowData\',id);
-		window.open(\'/proteoerp/formatos/ver/COMPRA/\'+id+"/id", \'_blank\', \'width=900,height=800,scrollbars=yes,status=yes,resizable=yes,screenx=((screen.availHeight/2)-450), screeny=((screen.availWidth/2)-400)\');
+		window.open(\''.site_url('formatos/ver/COMPRA').'/\'+id+"/id", \'_blank\', \'width=900,height=800,scrollbars=yes,status=yes,resizable=yes,screenx=((screen.availHeight/2)-450), screeny=((screen.availWidth/2)-400)\');
 	} else { $.prompt("<h1>Por favor Seleccione un Movimiento</h1>");}
 });
 
 jQuery("#boton2").click( function(){
-	window.open(\'/proteoerp/compras/scst/dataedit/create\', \'_blank\', \'width=800,height=600,scrollbars=yes,status=yes,resizable=yes,screenx=((screen.availHeight/2)-400), screeny=((screen.availWidth/2)-300)\');
+	window.open(\''.site_url('/compras/scst/dataedit/create').'\', \'_blank\', \'width=800,height=600,scrollbars=yes,status=yes,resizable=yes,screenx=((screen.availHeight/2)-400), screeny=((screen.availWidth/2)-300)\');
 });
 
 jQuery("#boton3").click( function(){
 	var id = jQuery("#newapi'. $param['grids'][0]['gridname'].'").jqGrid(\'getGridParam\',\'selrow\');
 	if (id)	{
 		var ret = jQuery("#newapi'. $param['grids'][0]['gridname'].'").jqGrid(\'getRowData\',id);
-		window.open(\'/proteoerp/compras/scst/dataedit/show/\'+id, \'_blank\', \'width=1000,height=600,scrollbars=yes,status=yes,resizable=yes,screenx=((screen.availHeight/2)-500), screeny=((screen.availWidth/2)-300)\');
+		window.open(\''.site_url('compras/scst/dataedit/show').'/\'+id, \'_blank\', \'width=1000,height=600,scrollbars=yes,status=yes,resizable=yes,screenx=((screen.availHeight/2)-500), screeny=((screen.availWidth/2)-300)\');
 	} else { $.prompt("<h1>Por favor Seleccione un Movimiento</h1>");}
 });
 
