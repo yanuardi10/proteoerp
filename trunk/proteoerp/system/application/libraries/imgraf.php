2d9ebd9d3a90b5c8859f90c2b37621fd57d69b34
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2012-09-04 03:31:35 +0000

    -Grafico de lineas

--- trunk/proteoerp/system/application/libraries/imgraf.php
+++ trunk/proteoerp/system/application/libraries/imgraf.php
@@ -5,7 +5,7 @@ include('pChart/pChart.class');
 class imgraf{
 
 	function imgraf(){
-		
+
 	}
 
 	function pie($valores,$label,$titulo){
@@ -14,7 +14,7 @@ class imgraf{
 		$DataSet = new pData;
 		$DataSet->AddPoint($valores,'Serie1');
 		$DataSet->AddPoint($label  ,'Serie2');
-		$DataSet->AddAllSeries(); 
+		$DataSet->AddAllSeries();
 		$DataSet->SetAbsciseLabelSerie('Serie2');
 
 		$Test = new pChart(300,200);
@@ -62,6 +62,48 @@ class imgraf{
 		$Test->drawLegend(220,7,$DataSet->GetDataDescription(),255,255,255);
 		$Test->setFontProperties(APPPATH.'libraries/pChart/Fonts/tahoma.ttf',10);
 		$Test->drawTitle(10,22,$titulo,50,50,50,300);
+		$Test->Render($nombre);
+
+		return $nombre;
+	}
+
+	function line($valores,$titulo,$label=array()){
+		$nombre=tempnam('/tmp', 'g').'.png';
+
+		$DataSet = new pData;
+
+		$ind=1;
+		$DataSet->AddPoint($valores,'Serie'.$ind);
+
+		$DataSet->AddAllSeries();
+		$DataSet->SetAbsciseLabelSerie();
+		$DataSet->SetSerieName($titulo ,'Serie'.$ind);
+
+		if(count($label)>0){
+			$ID=0;
+			foreach($label AS $val){
+				$DataSet->Data[$ID]["Name"]=$val;
+				$ID++;
+			}
+		}
+
+		$Test = new pChart(300,200);
+		$Test->setFontProperties(APPPATH.'libraries/pChart/Fonts/tahoma.ttf',8);
+		$Test->setGraphArea(70,30,280,170);
+		$Test->drawFilledRoundedRectangle(7,7,293,193,5,240,240,240);
+		$Test->drawRoundedRectangle(5,5,295,195,5,230,230,230);
+		$Test->drawGraphArea(255,255,255,TRUE);
+		$Test->drawScale($DataSet->GetData(),$DataSet->GetDataDescription(),SCALE_NORMAL,150,150,150,true,0,0);
+		$Test->drawGrid(4,TRUE,200,200,200,50);
+
+		$Test->setFontProperties(APPPATH.'libraries/pChart/Fonts/tahoma.ttf',6);
+		$Test->drawTreshold(0,143,55,72,TRUE,TRUE);
+		$Test->drawCubicCurve($DataSet->GetData(),$DataSet->GetDataDescription());
+
+		$Test->setFontProperties(APPPATH.'libraries/pChart/Fonts/tahoma.ttf',8);
+		$Test->drawLegend(200,9,$DataSet->GetDataDescription(),255,255,255);
+		$Test->setFontProperties(APPPATH.'libraries/pChart/Fonts/tahoma.ttf',10);
+		//$Test->drawTitle(10,22,$titulo,50,50,50,300);
 		$Test->Render($nombre);
 
 		return $nombre;
