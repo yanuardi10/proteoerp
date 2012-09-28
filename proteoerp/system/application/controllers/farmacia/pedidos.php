9733779f4654d955bd45f343ffad35df8ed6c012
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2012-09-28 17:22:24 +0000

    -Pedidos por negociacion

--- proteoerp/system/application/controllers/farmacia/pedidos.php
+++ proteoerp/system/application/controllers/farmacia/pedidos.php
@@ -287,6 +287,191 @@ class Pedidos extends Controller {
 		}
 	}
 
+	function especial(){
+		$this->rapyd->load('datafilter2','datagrid','fields');
+
+		$link2=site_url('inventario/common/get_linea');
+		$link3=site_url('inventario/common/get_grupo');
+
+		$script='
+		$(document).ready(function(){
+			$(".inputnum").numeric(".");
+			$("#depto").change(function(){
+				depto();
+				$.post("'.$link2.'",{ depto:$(this).val() },function(data){$("#linea").html(data);})
+				$.post("'.$link3.'",{ linea:"" },function(data){$("#grupo").html(data);})
+			});
+			$("#linea").change(function(){
+				linea();
+				$.post("'.$link3.'",{ linea:$(this).val() },function(data){$("#grupo").html(data);})
+			});
+			$("#grupo").change(function(){
+				grupo();
+			});
+			$("#sinvprecioc").submit(function() {
+				return confirm("Se van a actualizar todos los precios en pantalla \nEstas seguro de que quieres seguir??");
+			});
+			depto();
+			linea();
+			grupo();
+
+			$(\'input[name^="monto"]\').keyup(function(){
+				var val=$(this).val();
+				alert(val);
+
+			});
+		});
+
+		function depto(){
+			if($("#depto").val()!=""){
+				$("#nom_depto").attr("disabled","disabled");
+			}
+			else{
+				$("#nom_depto").attr("disabled","");
+			}
+		}
+
+		function linea(){
+			if($("#linea").val()!=""){
+				$("#nom_linea").attr("disabled","disabled");
+			}
+			else{
+				$("#nom_linea").attr("disabled","");
+			}
+		}
+
+		function grupo(){
+			if($("#grupo").val()!=""){
+				$("#nom_grupo").attr("disabled","disabled");
+			}
+			else{
+				$("#nom_grupo").attr("disabled","");
+			}
+		}';
+
+		$filter = new DataFilter2($this->titp);
+
+		$select=array(
+			'IF(formcal=\'U\',ultimo,IF(a.formcal=\'P\',pond,IF(formcal=\'S\',standard,GREATEST(ultimo,pond)))) AS costo',
+			'a.existen','a.marca','a.tipo','a.id',
+			'TRIM(codigo) AS codigo',
+			'a.descrip','precio1','precio2','precio3','precio4','b.nom_grup','b.grupo','a.barras',
+			'c.descrip AS nom_linea','c.linea','d.descrip AS nom_depto','d.depto AS depto',
+			'a.base1','a.base2','a.base3','a.base4','e.cobeca','e.dronena','e.drolanca','e.mafarta'
+		);
+
+		$filter->db->select($select);
+		$filter->db->from('sinv AS a');
+		$filter->db->join('grup AS b','a.grupo=b.grupo');
+		$filter->db->join('line AS c','b.linea=c.linea');
+		$filter->db->join('dpto AS d','c.depto=d.depto');
+		$filter->db->join('droguerias.inventarios AS e','a.barras=e.barras','left');
+		$filter->db->where('a.activo','S');
+		$filter->script($script);
+
+		$filter->codigo = new inputField("C&oacute;digo", "codigo");
+		$filter->codigo-> size=15;
+		$filter->codigo->group = "Uno";
+
+		$filter->descrip = new inputField("Descripci&oacute;n", "descrip");
+		$filter->descrip->db_name='CONCAT_WS(" ",a.descrip,a.descrip2)';
+		$filter->descrip-> size=30;
+		$filter->descrip->group = "Uno";
+
+		$filter->depto2 = new inputField("Departamento", "nom_depto");
+		$filter->depto2->db_name="d.descrip";
+		$filter->depto2 -> size=5;
+		$filter->depto2->group = "Dos";
+
+		$filter->depto = new dropdownField("Departamento","depto");
+		$filter->depto->db_name="d.depto";
+		$filter->depto->option("","Seleccione un Departamento");
+		$filter->depto->options("SELECT depto, CONCAT(depto,'-',descrip) descrip FROM dpto WHERE tipo='I' ORDER BY depto");
+		$filter->depto->in="depto2";
+		$filter->depto->group = "Dos";
+		$filter->depto->style='width:190px;';
+
+		$filter->linea = new inputField("Linea", "nom_linea");
+		$filter->linea->db_name="c.descrip";
+		$filter->linea -> size=5;
+		$filter->linea->group = "Dos";
+
+		$filter->linea2 = new dropdownField("L&iacute;nea","linea");
+		$filter->linea2->db_name="c.linea";
+		$filter->linea2->option("","Seleccione un Departamento primero");
+		$filter->linea2->in="linea";
+		$filter->linea2->group = "Dos";
+		$filter->linea2->style='width:190px;';
+
+		$depto=$filter->getval('depto');
+		if($depto!==FALSE){
+			$filter->linea2->options("SELECT linea, CONCAT(linea,'-',descrip) descrip FROM line WHERE depto='$depto' ORDER BY descrip");
+		}else{
+			$filter->linea2->option("","Seleccione un Departamento primero");
+		}
+
+		$filter->grupo2 = new inputField("Grupo", "nom_grupo");
+		$filter->grupo2->db_name="b.nom_grup";
+		$filter->grupo2 -> size=5;
+		$filter->grupo2->group = "Dos";
+
+		$filter->grupo = new dropdownField("Grupo", "grupo");
+		$filter->grupo->db_name="b.grupo";
+		$filter->grupo->option("","Seleccione una L&iacute;nea primero");
+		$filter->grupo->in="grupo2";
+		$filter->grupo->group = "Dos";
+		$filter->grupo->style='width:190px;';
+
+		$linea=$filter->getval('linea2');
+		if($linea!==FALSE){
+			$filter->grupo->options("SELECT grupo, CONCAT(grupo,'-',nom_grup) nom_grup FROM grup WHERE linea='$linea' ORDER BY nom_grup");
+		}else{
+			$filter->grupo->option("","Seleccione un Departamento primero");
+		}
+
+		$filter->marca = new dropdownField('Laboratorio', "marca");
+		$filter->marca->option('','Todas');
+		$filter->marca->options("SELECT TRIM(marca) AS clave, TRIM(marca) AS valor FROM marc ORDER BY marca");
+		$filter->marca->style='width:220px;';
+		$filter->marca->group = "Dos";
+
+		$filter->buttons('reset', 'search');
+		$filter->build();
+
+		$uri = anchor($this->url.'dataedit/show/<raencode><#id#></raencode>','<#id#>');
+
+		$grid = new DataGrid('');
+		$grid->order_by('id');
+		$grid->per_page = 40;
+
+		$monto = new inputField('Monto', 'monto');
+		$monto->grid_name='monto[<#barras#>]';
+		$monto->status   ='modify';
+		$monto->size     =8;
+		$monto->css_class='inputnum';
+
+		$grid->column_orderby('C&oacute;digo','barras','barras');
+		$grid->column_orderby('Descripci&oacute;n','descrip','descrip');
+		$grid->column_orderby('Laboratorio','marca','marca');
+		$grid->column('Monto' , $monto  ,'align=\'right\'');
+		$grid->column('Dias'  , '<span id=dias_<#barras#>></span>','align=\'right\'');
+		$grid->column('Costo'     ,'<nformat><#costo#></nformat>'   ,'align=right');
+		$grid->column('Existencia','<nformat><#existen#></nformat>' ,'align=right');
+		$grid->column('Merida'    ,'<nformat><#cobeca#></nformat>'  ,'align=right');
+		$grid->column('Dronena'   ,'<nformat><#dronena#></nformat>' ,'align=right');
+		$grid->column('Drolanca'  ,'<nformat><#drolanca#></nformat>','align=right');
+		$grid->column('Mafarta'   ,'<nformat><#mafarta#></nformat>' ,'align=right');
+
+		$grid->add($this->url.'dataedit/create');
+		$grid->build();
+
+		$data['filtro']  = $filter->output;
+		$data['content'] = $grid->output;
+		$data['head']    = $this->rapyd->get_head().script('jquery.js');
+		$data['title']   = heading($this->titp);
+		$this->load->view('view_ventanas', $data);
+
+	}
 
 	function _farmaurl($opt='farmax'){
 		$uri='drogueria/pedidos';
