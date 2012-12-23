900c7759b0121710f12036d601431b318719898b
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2012-12-23 19:17:08 +0000

    -Avances en ajustes de inventario

--- proteoerp/system/application/views/view_ssal.php
+++ proteoerp/system/application/views/view_ssal.php
@@ -9,15 +9,14 @@ if ($form->_status=='delete' || $form->_action=='delete' || $form->_status=='unk
 else:
 
 $campos=$form->template_details('itssal');
-$scampos  ='<tr id="tr_itssal_<#i#>">';
-$scampos .='<td class="littletablerow" align="left" >'.$campos['codigo']['field'].'</td>';
-$scampos .='<td class="littletablerow" align="left" >'.$campos['itdescrip']['field'].'</td>';
-$scampos .='<td class="littletablerow" align="right">'.$campos['cantidad']['field'].  '</td>';
-$scampos .='<td class="littletablerow" align="right">'.$campos['costo']['field']. '</td>';
+$scampos  = '<tr id="tr_itssal_<#i#>">';
+$scampos .= '<td class="littletablerow" align="left" >'.$campos['codigo']['field'].'</td>';
+$scampos .= '<td class="littletablerow" align="left" >'.$campos['itdescrip']['field'].'</td>';
+$scampos .= '<td class="littletablerow" align="right">'.$campos['cantidad']['field'].  '</td>';
+$scampos .= '<td class="littletablerow" align="right">'.$campos['costo']['field']. '</td>';
+$scampos .= '<td class="littletablerow" align="left" >'.$campos['concepto']['field']. '</td>';
 $scampos .= '<td class="littletablerow"><a href=# onclick="del_itssal(<#i#>);return false;">'.img("images/delete.jpg").'</a></td></tr>';
 $campos=$form->js_escape($scampos);
-
-
 
 if(isset($form->error_string)) echo '<div class="alert">'.$form->error_string.'</div>';
 
@@ -27,10 +26,97 @@ if($form->_status!='show'){ ?>
 
 <script language="javascript" type="text/javascript">
 var itssal_cont=<?php echo $form->max_rel_count['itssal']; ?>;
-
 $(function(){
+	$("#fecha").datepicker({   dateFormat: "dd/mm/yy" });
 	$(".inputnum").numeric(".");
+	for(var i=0;i < <?php echo $form->max_rel_count['itssal']; ?>;i++){
+		autocod(i.toString());
+		autoicon(i.toString());
+	}
+
 });
+
+//Agrega el autocomplete para el codigo
+function autocod(id){
+	$('#codigo_'+id).autocomplete({
+		source: function( req, add){
+			$.ajax({
+				url:  "<?php echo site_url('ajax/buscasinvart'); ?>",
+				type: "POST",
+				dataType: "json",
+				data: "q="+req.term,
+				success:
+					function(data){
+						var sugiere = [];
+
+						if(data.length==0){
+							$('#codigo_'+id).val("");
+							$('#itdescrip_'+id).val("");
+							$('#costo_'+id).val(0);
+							$('#cantidad_'+id).val('');
+						}else{
+							$.each(data,
+								function(i, val){
+									sugiere.push( val );
+								}
+							);
+						}
+						add(sugiere);
+					},
+			})
+		},
+		minLength: 2,
+		select: function( event, ui ) {
+			$('#codigo_'+id).attr("readonly", "readonly");
+
+			var cana=Number($("#cantidad_"+id).val());
+			$('#codigo_'+id).val(ui.item.codigo);
+			$('#itdescrip_'+id).val(ui.item.descrip);
+			$('#costo_'+id).val(ui.item.pond);
+			if(cana<=0) $("#cantidad_"+id).val('1');
+			$('#cantidad_'+id).focus();
+			$('#cantidad_'+id).select();
+
+			setTimeout(function() {  $('#codigo_'+id).removeAttr("readonly"); }, 1500);
+		}
+	});
+}
+
+//Agrega el autocomplete para el cocepto
+function autoicon(id){
+	$('#concepto_'+id).autocomplete({
+		source: function( req, add){
+			$.ajax({
+				url:  "<?php echo site_url('ajax/buscaicon'); ?>",
+				type: "POST",
+				dataType: "json",
+				data: "q="+req.term+"&tipo="+$('#tipo').val(),
+				success:
+					function(data){
+						var sugiere = [];
+
+						if(data.length==0){
+							$('#concepto_'+id).val("");
+						}else{
+							$.each(data,
+								function(i, val){
+									sugiere.push( val );
+								}
+							);
+						}
+						add(sugiere);
+					},
+			})
+		},
+		minLength: 2,
+		select: function( event, ui ) {
+			$('#concepto_'+id).attr("readonly", "readonly");
+
+			setTimeout(function() {  $('#concepto_'+id).removeAttr("readonly"); }, 1500);
+		}
+	});
+}
+
 function add_itssal(){
 	var htm = <?php echo $campos; ?>;
 	can = itssal_cont.toString();
@@ -39,6 +125,7 @@ function add_itssal(){
 	htm = htm.replace(/<#o#>/g,con);
 	$("#__UTPL__").before(htm);
 	$("#cantidad_"+can).numeric(".");
+	autocod(can);
 	itssal_cont=itssal_cont+1;
 }
 function del_itssal(id){
@@ -53,28 +140,30 @@ function del_itssal(id){
 		<td>
 		<table width="100%" style="margin: 0; width: 100%;">
 			<tr>
-				<td class="littletableheader"><?php echo $form->fecha->label;    ?>*&nbsp;</td>
-				<td class="littletablerow">   <?php echo $form->fecha->output;   ?>&nbsp;</td>
-				<td class="littletableheader"><?php echo $form->tipo->label;  ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->tipo->label;   ?>*&nbsp;</td>
+				<td class="littletablerow">   <?php echo $form->tipo->output;   ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->fecha->label;  ?>&nbsp;</td>
 				<td>
-					<table width="100%" cellpadding="0" cellspacing="0"><tr>
-						<td class="littletablerow">   <?php echo $form->tipo->output; ?>&nbsp;</td>
-						<td class="littletableheader"><?php echo $form->almacen->label;  ?>&nbsp;</td>
-						<td class="littletablerow">   <?php echo $form->almacen->output,$form->caububides->output; ?>&nbsp;</td>
-					</tr></table>
+					<table width="100%" cellpadding="0" cellspacing="0">
+						<tr>
+							<td class="littletablerow">   <?php echo $form->fecha->output; ?></td>
+							<td class="littletableheader"><?php echo $form->almacen->label; ?>&nbsp;</td>
+							<td class="littletablerow">   <?php echo $form->almacen->output.$form->caububides->output; ?>&nbsp;</td>
+						</tr>
+					</table>
 				</td>
 			</tr>
 			<tr>
-				<td class="littletableheader"><?php echo $form->depto->label;  ?>&nbsp;</td>
-				<td class="littletablerow">   <?php echo $form->depto->output; ?>&nbsp;</td>
-				<td class="littletableheader"><?php echo $form->descrip->label; ?>&nbsp;</td>
-				<td class="littletablerow"   ><?php echo $form->descrip->output;   ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->depto->label;    ?>&nbsp;</td>
+				<td class="littletablerow">   <?php echo $form->depto->output;   ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->descrip->label;  ?>&nbsp;</td>
+				<td class="littletablerow"   ><?php echo $form->descrip->output; ?>&nbsp;</td>
 			</tr>
 			<tr>
-				<td class="littletableheader"><?php echo $form->cargo->label;  ?>&nbsp;</td>
-				<td class="littletablerow">   <?php echo $form->cargo->output; ?>&nbsp;</td>
-				<td class="littletableheader"><?php echo $form->motivo->label; ?>&nbsp;</td>
-				<td class="littletablerow"   ><?php echo $form->motivo->output;   ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->cargo->label;   ?>&nbsp;</td>
+				<td class="littletablerow">   <?php echo $form->cargo->output;  ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->motivo->label;  ?>&nbsp;</td>
+				<td class="littletablerow"   ><?php echo $form->motivo->output; ?>&nbsp;</td>
 			</tr>
 		</table><br>
 		</td>
@@ -88,27 +177,29 @@ function del_itssal(id){
 				<td bgcolor='#7098D0'>Descripci&oacute;n</td>
 				<td bgcolor='#7098D0'>Cantidad</td>
 				<td bgcolor='#7098D0'>Costo</td>
-				<?php if($form->_status!='show') {?>
+				<td bgcolor='#7098D0'>Conceptos</td>
+				<?php if($form->_status!='show'){?>
 					<td bgcolor='#7098D0'>&nbsp;</td>
 				<?php } ?>
 			</tr>
 
 			<?php for($i=0;$i<$form->max_rel_count['itssal'];$i++) {
-				$it_codigo  = "codigo_$i";
-				$it_descrip   = "itdescrip_$i";
-				$it_cant    = "cantidad_$i";
-				$it_costo   = "costo_$i";
+				$it_codigo   = "codigo_$i";
+				$it_descrip  = "itdescrip_$i";
+				$it_cant     = "cantidad_$i";
+				$it_costo    = "costo_$i";
+				$it_concepto = "concepto_$i";
 			?>
 
 			<tr id='tr_itssal_<?php echo $i; ?>'>
-				<td class="littletablerow" align="left" ><?php echo $form->$it_codigo->output; ?></td>
-				<td class="littletablerow" align="left" ><?php echo $form->$it_descrip->output;  ?></td>
-				<td class="littletablerow" align="right"><?php echo $form->$it_cant->output;   ?></td>
-				<td class="littletablerow" align="right"><?php echo $form->$it_costo->output;  ?></td>
-
+				<td class="littletablerow" align="left" ><?php echo $form->$it_codigo->output;  ?></td>
+				<td class="littletablerow" align="left" ><?php echo $form->$it_descrip->output; ?></td>
+				<td class="littletablerow" align="right"><?php echo $form->$it_cant->output;    ?></td>
+				<td class="littletablerow" align="right"><?php echo $form->$it_costo->output;   ?></td>
+				<td class="littletablerow" align="left" ><?php echo $form->$it_concepto->output;?></td>
 				<?php if($form->_status!='show') {?>
 				<td class="littletablerow">
-					<a href='#' onclick='del_itssal(<?=$i ?>);return false;'><?php echo img("images/delete.jpg"); ?></a>
+					<a href='#' onclick='del_itssal(<?php echo $i; ?>);return false;'><?php echo img("images/delete.jpg"); ?></a>
 				</td>
 				<?php } ?>
 			</tr>
