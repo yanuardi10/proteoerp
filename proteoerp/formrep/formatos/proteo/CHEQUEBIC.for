75b6d93ad1e66968c374a657f66fdd5ed2b3d77a
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2013-02-10 19:31:48 +0000

    -Imprime cheques en gastos -Formatos de cheques

--- proteoerp/formrep/formatos/proteo/CHEQUEBIC.for
+++ proteoerp/formrep/formatos/proteo/CHEQUEBIC.for
@@ -2,7 +2,7 @@
 $nombre = strtoupper($nombre);
 $dia    = date('d'    ,$mkt);
 $mes    = date('j'    ,$mkt);
-$anio   = date('Y'    ,$mkt);
+$anio   = date('y'    ,$mkt);
 $nmes   = $meses[$mes];
 $tmonto = '#'.htmlnformat($monto);
 $smonto = '##'.htmlnformat($monto).'##';
@@ -30,11 +30,11 @@ div.absolute {
 
 <div class="absolute" style="top: -20px; right: 0px; font-weight:bold; font-size: 12pt"><?php echo $tmonto; ?></div>
 
-<div class="absolute" style="top: 25px; left: 65px; right: -10px;"><?php echo $nombre; ?></div>
-<div class="absolute" style="top: 50px; left: 10px; right: -10px; text-align:left;text-indent: 50px;font-size: 0.8em;line-height: 1.8em"><?php echo $smonto; ?></div>
+<div class="absolute" style="top: 35px; left: 65px; right: -10px;"><?php echo $nombre; ?></div>
+<div class="absolute" style="top: 60px; left: 10px; right: -10px; text-align:left;text-indent: 50px;font-size: 0.8em;line-height: 1.8em"><?php echo $smonto; ?></div>
 
-<div class="absolute" style="top: 90px; left: 10px; right: 350px text-align: right;"><?php echo $ffecha; ?></div>
-<div class="absolute" style="top: 90px; left: 265px;"><?php echo $anio; ?></div>
+<div class="absolute" style="top: 100px; left: 10px; right: 350px text-align: right;"><?php echo $ffecha; ?></div>
+<div class="absolute" style="top: 100px; left: 265px;"><?php echo $anio; ?></div>
 
 <?php if($endosable){ ?>
 <div class="absolute" style="top: 180px; right:30px;font-size: 1.3em;">NO ENDOSABLE</div>
