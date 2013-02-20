3745c04b6e902bbfbffd3c613580c46137fb1590
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2013-02-20 19:10:26 +0000

    -Arreglos en los formatos

--- trunk/proteoerp/formrep/formatos/proteo/X_CINTILLO.for
+++ trunk/proteoerp/formrep/formatos/proteo/X_CINTILLO.for
@@ -1,13 +1,22 @@
 <!-- CINTILLO -->
+<?php
+$rif             = trim($this->datasis->traevalor('RIF'));
+$cintillo_titulo1= trim($this->datasis->traevalor('TITULO1'));
+$cintillo_titulo2= trim($this->datasis->traevalor('TITULO2'));
+$cintillo_titulo3= trim($this->datasis->traevalor('TITULO3'));
+
+$cintillo_titulo2= preg_replace('/[Rr][Ii][Ff] *:? *[VJPGvjpg][0-9\-]+/', ' ', $cintillo_titulo2);
+$cintillo_titulo3= preg_replace('/[Rr][Ii][Ff] *:? *[VJPGvjpg][0-9\-]+/', ' ', $cintillo_titulo3);
+?>
 <div id="section_header">
 	<table style="width: 100%;" >
 		<tr>
 			<td width="130" rowspan="3"><img src="<?php echo $this->_direccion.'/images/logo.jpg'; ?>" width="127" alt="Logo"></td>
-			<td><span style="text-align:left;font-size:1.4em;font-style:italic;font-weight: bold;"><?php echo trim($this->datasis->traevalor("TITULO1")) ?></span></td>
+			<td><span style="text-align:left;font-size:1.4em;font-style:italic;font-weight: bold;"><?php echo $cintillo_titulo1; ?></span></td>
 		</tr><tr>
-			<td style="font-size: 8pt"><b>RIF: <?php echo $this->datasis->traevalor("RIF") ?></b></td>
+			<td style="font-size: 8pt"><b>RIF: <?php echo $rif; ?></b></td>
 		</tr><tr>
-			<td><div style="font-size: 8pt"> <?php echo trim($this->datasis->traevalor("TITULO2")).' '.trim($this->datasis->traevalor("TITULO3")) ?></div></td>
+			<td><div style="font-size: 8pt"> <?php echo $cintillo_titulo2.' '.$cintillo_titulo3; ?></div></td>
 		</tr>
 	</table>
 </div>
