82ac3c8bece3ce5abb04ffd8f426b4013d6f639c
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2013-05-21 23:20:04 +0000

    -Avances en otin

--- proteoerp/system/application/views/view_otin.php
+++ proteoerp/system/application/views/view_otin.php
@@ -10,8 +10,9 @@ else:
 $campos=$form->template_details('itotin');
 $scampos  ='<tr id="tr_itotin_<#i#>">';
 $scampos .='<td class="littletablerow" align="left" >'.$campos['codigo']['field'].   '</td>';
-$scampos .='<td class="littletablerow" align="left" >'.$campos['descrip']['field']. '</td>';
+$scampos .='<td class="littletablerow" align="left" >'.$campos['descrip']['field'].$campos['larga']['field']. '</td>';
 $scampos .='<td class="littletablerow" align="right">'.$campos['precio']['field'].   '</td>';
+$scampos .='<td class="littletablerow" align="right">'.$campos['tasaiva']['field'].   '</td>';
 $scampos .='<td class="littletablerow" align="right">'.$campos['impuesto']['field']. '</td>';
 $scampos .='<td class="littletablerow" align="right">'.$campos['importe']['field'].  '</td>';
 $scampos .='<td class="littletablerow"><a href="#" onclick="del_itotin(<#i#>);return false;">'.img('images/delete.jpg').'</a></td>';
@@ -54,6 +55,8 @@ var sfpa_cont  =<?php echo $form->max_rel_count['sfpa'];   ?>;
 $(function(){
 	$("#fecha").datepicker({ dateFormat: "dd/mm/yy" });
 	$("#vence").datepicker({ dateFormat: "dd/mm/yy" });
+	$("#fafecta").datepicker({ dateFormat: "dd/mm/yy" });
+	$('input[name^="sfpafecha_"]').datepicker({ dateFormat: "dd/mm/yy" });
 	$(".inputnum").numeric(".");
 	totalizar();
 
@@ -66,8 +69,13 @@ $(function(){
 		var tipo = $(this).val();
 		if(tipo=='OT'){
 			$('#tsfpa').show();
+			$('#ffafecta').hide();
+		}else if(tipo=='OC'){
+			$('#tsfpa').hide();
+			$('#ffafecta').hide();
 		}else{
 			$('#tsfpa').hide();
+			$('#ffafecta').show();
 		}
 	});
 
@@ -122,6 +130,42 @@ $(function(){
 		}
 	});
 
+	$('#afecta').autocomplete({
+		delay: 600,
+		autoFocus: true,
+		source: function(req, add){
+			$.ajax({
+				url:  "<?php echo site_url('ajax/buscaafecta'); ?>",
+				type: "POST",
+				dataType: "json",
+				data: {"q":req.term},
+				success:
+					function(data){
+						var sugiere = [];
+						if(data.length==0){
+							$('#afecta').val('');
+							$('#fafecta').val('');
+						}else{
+							$.each(data,
+								function(i, val){
+									sugiere.push( val );
+								}
+							);
+						}
+						add(sugiere);
+					},
+			})
+		},
+		minLength: 2,
+		select: function( event, ui ) {
+			$('#afecta').attr("readonly", "readonly");
+			$('#afecta').val(ui.item.value);
+			$('#fafecta').val(ui.item.fecha);
+			setTimeout(function() {  $("#afecta").removeAttr("readonly"); }, 1500);
+		}
+	});
+
+	$('#tipo_doc').change();
 });
 
 function add_sfpa(){
@@ -227,8 +271,6 @@ function totalizar(){
 	$("#totals").val(roundNumber(totals,2));
 	$("#iva").val(roundNumber(iva,2));
 
-
-
 	var tipo = $('#tipo_doc').val();
 	if(tipo=='OT'){
 		resto=faltante();
@@ -280,6 +322,7 @@ function autocod(id){
 						if(data.length==0){
 							$('#codigo_'+id).val('')
 							$('#descrip_'+id).val('');
+							$('#larga_'+id).val('');
 							//post_modbus_sinv(id);
 						}else{
 							$.each(data,
@@ -298,6 +341,7 @@ function autocod(id){
 
 			$('#codigo_'+id).val(ui.item.codigo);
 			$('#descrip_'+id).val(ui.item.descrip);
+			$('#larga_'+id).val(ui.item.descrip);
 			$('#precio_'+id).focus();
 			$('#precio_'+id).select();
 
@@ -317,12 +361,12 @@ function autocod(id){
 		<fieldset style='border: 1px outset #9AC8DA;background: #FFFFFF;'>
 		<table width="100%" style="margin: 0; width: 100%;">
 			<tr>
-				<th colspan='5' class="littletableheader">Otros Ingresos <b><?php if($form->_status=='show' or $form->_status=='modify' ) echo str_pad($form->numero->output,8,0,0); ?></b></th>
+				<th colspan='4' class="littletableheader">Otros Ingresos <b><?php if($form->_status=='show' or $form->_status=='modify' ) echo str_pad($form->numero->output,8,0,0); ?></b></th>
 			</tr>
 			<tr>
-				<td class="littletableheader"><?php echo $form->tipo_doc->label  ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->tipo_doc->label  ?>*&nbsp;</td>
 				<td class="littletablerow"   ><?php echo $form->tipo_doc->output ?>&nbsp;</td>
-				<td class="littletableheader"><?php echo $form->cliente->label;  ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->cliente->label;  ?>*&nbsp;</td>
 				<td class="littletablerow"   ><?php echo $form->cliente->output.$form->rifci->output; ?>&nbsp;</td>
 			</tr>
 			<tr>
@@ -338,14 +382,13 @@ function autocod(id){
 				<td class="littletablerow"   ><?php echo $form->direc->output    ?>&nbsp;</td>
 			</tr>
 			<tr>
-				<td class="littletableheader"><?php echo $form->dpto->label      ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->dpto->label      ?>*&nbsp;</td>
 				<td class="littletablerow"   ><?php echo $form->dpto->output     ?>&nbsp;</td>
-				<td class="littletableheader"><?php echo $form->sucu->label      ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->sucu->label      ?>*&nbsp;</td>
 				<td class="littletablerow"   ><?php echo $form->sucu->output     ?>&nbsp;</td>
 			</tr>
 		</table>
 		</fieldset>
-		<br>
 		</td>
 	</tr>
 	<tr>
@@ -363,7 +406,6 @@ function autocod(id){
 				<td class="littletableheaderdet">&nbsp;</td>
 				<?php } ?>
 			</tr>
-
 			<?php for($i=0;$i<$form->max_rel_count['itotin'];$i++) {
 				$it_codigo   = "codigo_${i}";
 				$it_descrip  = "descrip_${i}";
@@ -371,6 +413,7 @@ function autocod(id){
 				$it_precio   = "precio_${i}";
 				$it_importe  = "importe_${i}";
 				$it_tasaiva  = "tasaiva_${i}";
+				$it_larga    = "larga_${i}";
 
 				if($form->_status=='show'){
 					$ivaval=nformat(round($form->$it_impuesto->value/$form->$it_precio->value,2)*100,2);
@@ -378,10 +421,9 @@ function autocod(id){
 					$ivaval=$form->$it_tasaiva->output;
 				}
 			?>
-
 			<tr id='tr_itotin_<?php echo $i; ?>'>
 				<td class="littletablerow" align="left" nowrap><?php echo $form->$it_codigo->output;  ?></td>
-				<td class="littletablerow" align="left" ><?php echo $form->$it_descrip->output; ?></td>
+				<td class="littletablerow" align="left" ><?php echo $form->$it_descrip->output.$form->$it_larga->output; ?></td>
 				<td class="littletablerow" align="right"><?php echo $form->$it_precio->output;  ?></td>
 				<td class="littletablerow" align="right"><?php echo $ivaval;                    ?></td>
 				<td class="littletablerow" align="right"><?php echo $form->$it_impuesto->output;?></td>
@@ -428,9 +470,27 @@ function autocod(id){
 			</tr>
 			<?php } ?>
 			<tr id='__UTPL__sfpa'>
-				<td colspan='<?php echo ($form->_status!='show')? '6':'5';  ?>' class="littletableheaderdet">&nbsp;</td>
+				<td colspan='<?php echo ($form->_status!='show')? '6':'5';  ?>' class="littletableheaderdet"  align='center'>
+					<?php if($form->_status!='show'){
+						echo $form->cajero->label.'* '.$form->cajero->output;
+					}else{
+						echo '&nbsp;';
+					} ?>
+				</td>
 			</tr>
 		</table>
+
+
+		<fieldset style='border: 1px outset #9AC8DA;background: #FFFFFF;' id='ffafecta'>
+		<table width="100%" style="margin: 0; width: 100%;">
+			<tr>
+				<td class="littletableheader"><?php echo $form->afecta->label;   ?>*&nbsp;</td>
+				<td class="littletablerow"   ><?php echo $form->afecta->output;  ?>&nbsp;</td>
+				<td class="littletableheader"><?php echo $form->fafecta->label;  ?>*&nbsp;</td>
+				<td class="littletablerow"   ><?php echo $form->fafecta->output; ?>&nbsp;</td>
+			</tr>
+		</table>
+		</fieldset>
 
 
 		</td>
