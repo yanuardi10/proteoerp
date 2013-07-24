146146f4ca4b4d7828b3e9f78dcd41b4ff9ef77b
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2013-07-24 21:53:20 +0000

    -Arreglo en los reportes

--- trunk/proteoerp/system/application/libraries/PDFReporte.php
+++ trunk/proteoerp/system/application/libraries/PDFReporte.php
@@ -564,9 +564,9 @@ class PDFReporte extends Fpdf {
 					}
 				}
 
-				if    ($this->DBfieldsType[$key]=='real' || in_array($this->DBfieldsType[$key], array(4,5,246))  ) $row[$key]=nformat($row[$key]);
-				elseif($this->DBfieldsType[$key]=='date' || in_array($this->DBfieldsType[$key], array(10,12,7))  ) $row[$key]=dbdate_to_human($row[$key]);
-				elseif($this->DBfieldsType[$key]=='int'  || in_array($this->DBfieldsType[$key], array(1,2,9,3,8))) $row[$key]=intval($row[$key]);
+				if    (in_array($this->DBfieldsType[$key], array('real',4,5,246))  ) $row[$key]=nformat($row[$key]);
+				elseif(in_array($this->DBfieldsType[$key], array('date',10,12,7))  ) $row[$key]=dbdate_to_human($row[$key]);
+				elseif(in_array($this->DBfieldsType[$key], array('int' ,1,2,9,3,8))) $row[$key]=intval($row[$key]);
 			}
 			$this->Row($row);
 		}
