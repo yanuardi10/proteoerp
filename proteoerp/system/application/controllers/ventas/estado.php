80e1c2cf194379e1b47995ada3b7a75e0e4d1972
datasql <datasql@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2013-08-29 00:18:21 +0000

    Estados

--- proteoerp/system/application/controllers/ventas/estado.php
+++ proteoerp/system/application/controllers/ventas/estado.php
@@ -5,7 +5,7 @@ class Estado extends Controller {
 	var $tits    = 'ENTIDADES';
 	var $url     = 'ventas/estado/';
 
-	function Estados(){
+	function Estado(){
 		parent::Controller();
 		$this->load->library('rapyd');
 		$this->load->library('jqdatagrid');
@@ -13,6 +13,7 @@ class Estado extends Controller {
 	}
 
 	function index(){
+		$this->instalar();
 		//$this->datasis->creaintramenu(array('modulo'=>'000','titulo'=>'<#titulo#>','mensaje'=>'<#mensaje#>','panel'=>'<#panal#>','ejecutar'=>'<#ejecuta#>','target'=>'popu','visible'=>'S','pertenece'=>'<#pertenece#>','ancho'=>900,'alto'=>600));
 		$this->datasis->modintramenu( 800, 600, substr($this->url,0,-1) );
 		redirect($this->url.'jqdatag');
@@ -158,7 +159,6 @@ class Estado extends Controller {
 									apprise("Registro Guardado");
 									$( "#fedita" ).dialog( "close" );
 									grid.trigger("reloadGrid");
-									'.$this->datasis->jwinopen(site_url('formatos/ver/ESTADOS').'/\'+res.id+\'/id\'').';
 									return true;
 								} else {
 									apprise(json.mensaje);
@@ -564,22 +564,62 @@ class Estado extends Controller {
 
 	function instalar(){
 		if (!$this->db->table_exists('estado')) {
-			$mSQL="CREATE TABLE `estado` (
-			  `id` int(10) NOT NULL AUTO_INCREMENT,
-			  `codigo` int(10) NOT NULL DEFAULT '0',
-			  `entidad` varchar(80) DEFAULT NULL,
-			  `capital` varchar(80) DEFAULT NULL,
-			  `superficie` decimal(10,2) DEFAULT NULL,
-			  `poblacion` int(11) DEFAULT NULL,
-			  `municipios` int(11) DEFAULT NULL,
-			  `parroquias` int(11) DEFAULT NULL,
-			  PRIMARY KEY (`id`)
-			) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8";
-			$this->db->simple_query($mSQL);
+			$mSQL="
+			CREATE TABLE IF NOT EXISTS `estado` (
+				id          int(10) NOT NULL AUTO_INCREMENT,
+				codigo      int(10) NOT NULL DEFAULT '0',
+				entidad     varchar(80) DEFAULT NULL,
+				capital     varchar(80) DEFAULT NULL,
+				superficie  decimal(10,2) DEFAULT NULL,
+				poblacion   int(11) DEFAULT NULL,
+				municipios  int(11) DEFAULT NULL,
+				parroquias  int(11) DEFAULT NULL,
+				PRIMARY KEY (id),
+				UNIQUE KEY `codigo` (`codigo`)
+			) ENGINE=MyISAM  DEFAULT CHARSET=utf8;";
+			$this->db->query($mSQL);
+
+			$mSQL="
+			INSERT INTO `estado` (`id`, `codigo`, `entidad`, `capital`, `superficie`, `poblacion`, `municipios`, `parroquias`) VALUES
+				( 1, 22, 'AMAZONAS ', 'Puerto Ayacucho', 180145.00, 144398, 7, 23),
+				( 2,  2, 'ANZOÁTEGUI', 'Barcelona', 43000.00, 1464578, 21, 49),
+				( 3,  3, 'APURE', 'San Fernando de Apure', 76500.00, 458369, 7, 26),
+				( 4,  4, 'ARAGUA', 'Maracay', 7014.00, 1627141, 18, 44),
+				( 5,  5, 'BARINAS', 'Barinas', 35200.00, 814288, 12, 52),
+				( 6,  6, 'BOLÍVAR ', 'Bolívar ', 238000.00, 1405064, 11, 44),
+				( 7,  7, 'CARABOBO', 'Valencia ', 4650.00, 2239222, 14, 38),
+				( 8,  8, 'COJEDES', 'San Carlos', 14800.00, 322843, 9, 15),
+				( 9, 23, 'DELTA AMACURO ', 'Tucupita', 40200.00, 167522, 4, 21),
+				(26, 99, 'EMBAJADAS', '', 0.00, 0, 0, 0),
+				(11,  1, 'DISTRITO CAPITAL ', 'Caracas', 433.00, 1933186, 1, 22),
+				(12,  9, 'FALCÓN ', 'Coro ', 24800.00, 900211, 25, 78),
+				(13, 10, 'GUÁRICO', 'San Juan de los Morros ', 64986.00, 746174, 15, 38),
+				(14, 11, 'LARA ', 'Barquisimeto', 19800.00, 1769763, 9, 58),
+				(15, 12, 'MÉRIDA', 'Mérida', 11300.00, 826720, 23, 55),
+				(16, 13, 'MIRANDA', 'Los Teques', 7950.00, 2665596, 21, 31),
+				(17, 14, 'MONAGAS ', 'Maturín ', 28900.00, 901161, 13, 67),
+				(18, 15, 'NUEVA ESPARTA ', 'La Asunción', 1150.00, 490494, 11, 11),
+				(19, 16, 'PORTUGUESA', 'Guanare ', 15200.00, 875000, 14, 27),
+				(20, 17, 'SUCRE ', 'Cumaná ', 11800.00, 892990, 15, 55),
+				(21, 18, 'TÁCHIRA ', 'San Cristóbal', 11100.00, 1163593, 29, 93),
+				(22, 19, 'TRUJILLO ', 'Trujillo ', 7400.00, 684555, 20, 38),
+				(23, 24, 'VARGAS ', 'La Guaira ', 1496.00, 352087, 1, 11),
+				(24, 20, 'YARACUY ', 'San Felipe ', 7100.00, 599345, 14, 7),
+				(25, 21, 'ZULIA ', 'Maracaibo ', 63100.00, 3703640, 21, 106),
+				(27, 98, 'FRONTERA', '', 0.00, 0, 0, 0);";
+			$this->db->query($mSQL);
+
+
+
 		}
 		//$campos=$this->db->list_fields('estados');
 		//if(!in_array('<#campo#>',$campos)){ }
+
+
 	}
 }
 
 ?>
+
+
+
