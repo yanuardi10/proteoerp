40a236417f7cb9b8ead133a446d62a966395284a
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2013-09-11 21:07:49 +0000

    -Colores

--- proteoerp/system/application/views/view_farmax_compras.php
+++ proteoerp/system/application/views/view_farmax_compras.php
@@ -23,8 +23,8 @@ $atts = array(
 
 ?>
 <style>
-	.bien:link, .bien:visited,.bien:hover,.bien:active {color:#21CF00;}
-	.regu:link, .regu:visited,.bien:hover,.regu:active {color:#FFDD00;}
+	.bien:link, .bien:visited,.bien:hover,.bien:active {color:#1E890A;}
+	.regu:link, .regu:visited,.bien:hover,.regu:active {color:#DFBD00;}
 	.malo:link, .malo:visited,.bien:hover,.malo:active {color:#CF0000;}
 </style>
 <table align='center'>
