5cc35709f1ac2cb5017b62b89b9580355feaeb52
datasql <datasql@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2013-10-01 16:53:31 +0000

    Regenera Prestamos

--- proteoerp/system/application/controllers/finanzas/rete.php
+++ proteoerp/system/application/controllers/finanzas/rete.php
@@ -19,10 +19,9 @@ class Rete extends Controller {
 		redirect($this->url.'jqdatag');
 	}
 
-	//***************************
-	//Layout en la Ventana
+	//******************************************************************
+	// Layout en la Ventana
 	//
-	//***************************
 	function jqdatag(){
 
 		$grid = $this->defgrid();
@@ -55,9 +54,9 @@ class Rete extends Controller {
 		$this->load->view('jqgrid/crud2',$param);
 	}
 
-	//***************************
-	//Funciones de los Botones
-	//***************************
+	//******************************************************************
+	// Funciones de los Botones
+	//
 	function bodyscript( $grid0 ){
 		$bodyscript = '<script type="text/javascript">';
 		$ngrid      = '#newapi'.$grid0;
@@ -79,9 +78,9 @@ class Rete extends Controller {
 		return $bodyscript;
 	}
 
-	//***************************
-	//Definicion del Grid y la Forma
-	//***************************
+	//******************************************************************
+	// Definicion del Grid y la Forma
+	//
 	function defgrid( $deployed = false ){
 		$i      = 1;
 		$editar = 'false';
@@ -175,8 +174,6 @@ class Rete extends Controller {
 			'editable'      => $editar,
 			'width'         => 100,
 			'edittype'      => "'text'",
-			//'editrules'     => '{ required:true}',
-			//'editoptions'   => '{ size:30, maxlength: 15 }',
 			'editoptions' => '{'.$grid->autocomplete($link, 'cuenta','cucucu','<div id=\"cucucu\"><b>"+ui.item.descrip+"</b></div>').'}',
 		));
 
@@ -252,7 +249,7 @@ class Rete extends Controller {
 		}
 	}
 
-	/**
+	/*******************************************************************
 	* Busca la data en el Servidor por json
 	*/
 	function getdata(){
@@ -336,8 +333,6 @@ class Rete extends Controller {
 		$edit->script($script, 'create');
 		$edit->script($script, 'modify');
 
-		//$edit->pre_process( 'insert','_pre_insert' );
-		//$edit->pre_process( 'update','_pre_update' );
 		$edit->pre_process( 'delete','_pre_delete' );
 		$edit->post_process('insert','_post_insert');
 		$edit->post_process('update','_post_update');
@@ -488,430 +483,7 @@ class Rete extends Controller {
 			$this->db->query($mSQL);
 		}
 
-
 	}
 }
 
-
-
-/*
- class Rete extends validaciones {
-
-	var $data_type = null;
-	var $data = null;
-
-	function rete (){
-		parent::Controller();
-		$this->load->library('pi18n');
-		$this->load->library('rapyd');
-	}
-
-	function index(){
-		if ( !$this->datasis->iscampo('rete','id') ) {
-			$this->db->simple_query('ALTER TABLE rete DROP PRIMARY KEY');
-			$this->db->simple_query('ALTER TABLE rete ADD COLUMN id INT(11) NULL AUTO_INCREMENT, ADD PRIMARY KEY (id) ');
-			$this->db->simple_query('ALTER TABLE rete ADD UNIQUE INDEX codigo (codigo)');
-			$this->db->simple_query('ALTER TABLE rete ADD COLUMN concepto VARCHAR(5) NULL ');
-		}
-
-		$this->datasis->modulo_id(515,1);
-		if($this->pi18n->pais=='COLOMBIA'){
-			redirect('finanzas/retecol/filteredgrid');
-		}else{
-			//redirect("finanzas/rete/filteredgrid");
-			$this->reteextjs();
-		}
-	}
-
-	function filteredgrid(){
-		$this->rapyd->load('datafilter','datagrid');
-		$this->rapyd->uri->keep_persistence();
-
-		$filter = new DataFilter('Filtro por C&oacute;digo', 'rete');
-		$filter->codigo = new inputField('C&oacute;digo', 'codigo');
-		$filter->codigo->size=15;
-
-		$filter->buttons("reset","search");
-		$filter->build();
-
-		$uri = anchor('finanzas/rete/dataedit/show/<#codigo#>','<#codigo#>');
-
-		$grid = new DataGrid("Lista de Retenciones");
-		$grid->order_by("codigo","asc");
-		$grid->per_page = 20;
-
-		$grid->column("C&oacute;digo",$uri);
-		$grid->column("Pago de","activida");
-		$grid->column("Base Imponible","base1");
-		$grid->column("Porcentaje de Retenci&oacute;n","tari1");
-		$grid->column("Para pagos mayores a","pama1");
-		$grid->column("Tipo","tipo");
-
-		$grid->add("finanzas/rete/dataedit/create");
-		$grid->build();
-
-		$data['content'] = $filter->output.$grid->output;
-		$data['title']   = "<h1>Retenciones</h1>";
-		$data["head"]    = $this->rapyd->get_head();
-		$this->load->view('view_ventanas', $data);
-	}
-
-	function dataedit(){
-		$this->rapyd->load('dataedit');
-
-		$qformato=$this->datasis->formato_cpla();
-
-		$mCPLA=array(
-			'tabla'   =>'cpla',
-			'columnas'=>array(
-				'codigo' =>'C&oacute;digo',
-				'descrip'=>'Descripci&oacute;n'),
-			'filtro'  =>array('codigo'=>'C&oacute;digo','descrip'=>'Descripci&oacute;n'),
-			'retornar'=>array('codigo'=>'cuenta'),
-			'titulo'  =>'Buscar Cuenta',
-			'where'=>"codigo LIKE \"$qformato\"",
-			);
-
-		$bcpla =$this->datasis->modbus($mCPLA);
-
-		$script ='
-		$(function() {
-			$(".inputnum").numeric(".");
-		});
-		';
-
-		$edit = new DataEdit("Retenciones", "rete");
-		$edit->back_url = site_url("finanzas/rete/filteredgrid");
-		$edit->script($script, "create");
-		$edit->script($script, "modify");
-
-		$edit->post_process('insert','_post_insert');
-		$edit->post_process('update','_post_update');
-		$edit->post_process('delete','_post_delete');
-
-		$edit->codigo = new inputField("C&oacute;digo", "codigo");
-		$edit->codigo->mode="autohide";
-		$edit->codigo->size =7;
-		$edit->codigo->maxlength=4;
-		$edit->codigo->rule ="required|callback_chexiste";
-
-		$edit->activida = new inputField("Pago de", "activida");
-		$edit->activida->size =55;
-		$edit->activida->maxlength=45;
-		$edit->activida->rule= "strtoupper|required";
-
-		$edit->tipo =  new dropdownField("Tipo", "tipo");
-		$edit->tipo->option("JD","JD");
-		$edit->tipo->option("JN","JN");
-		$edit->tipo->option("NN","NN");
-		$edit->tipo->option("NR","NR");
-		$edit->tipo->style='width:60px';
-
-		$edit->base1 = new inputField("Base Imponible", "base1");
-		$edit->base1->size =13;
-		$edit->base1->maxlength=9;
-		$edit->base1->css_class='inputnum';
-		$edit->base1->rule='numeric';
-
-		$edit->tari1 =new inputField("Porcentaje de Retenci&oacute;n", "tari1");
-		$edit->tari1->size =13;
-		$edit->tari1->maxlength=10;
-		$edit->tari1->css_class='inputnum';
-		$edit->tari1->rule='numeric';
-
-		$edit->pama1 = new inputField("Para pagos mayores a", "pama1");
-		$edit->pama1->size =13;
-		$edit->pama1->maxlength=13;
-		$edit->pama1->css_class='inputnum';
-		$edit->pama1->rule='numeric';
-
-		$edit->concepto = new inputField('C&oacute;digo Concepto', 'concepto');
-		$edit->concepto->size =5;
-		$edit->concepto->maxlength=5;
-
-		$edit->cuenta = new inputField('Contable', 'cuenta');
-		$edit->cuenta->rule='trim|existecpla';
-		$edit->cuenta->append($bcpla);
-		$edit->cuenta->size=15;
-		$edit->cuenta->maxlength =15;
-
-		$edit->buttons('modify', 'save', 'undo', 'delete', 'back');
-		$edit->build();
-
-
-		$smenu['link']=barra_menu('515');
-		$data['smenu'] = $this->load->view('view_sub_menu', $smenu,true);
-		$data['content'] = $edit->output;
-		$data['title']   = "<h1>Retenciones</h1>";
-		$data["head"]    = script("jquery.pack.js").script("plugins/jquery.numeric.pack.js").script("plugins/jquery.floatnumber.js").$this->rapyd->get_head();
-		$this->load->view('view_ventanas', $data);
-	 }
-
-	 function _post_insert($do){
-		$codigo=$do->get('codigo');
-		$nombre=$do->get('activida');
-		logusu('rete',"RETENCION $codigo $nombre CREADO");
-	 }
-
-	 function _post_update($do){
-		$codigo=$do->get('codigo');
-		$nombre=$do->get('activida');
-		logusu('rete',"RETENCION $codigo $nombre MODIFICADO");
-	 }
-
-	 function _post_delete($do){
-		$codigo=$do->get('codigo');
-		$nombre=$do->get('activida');
-		logusu('rete',"RETENCION $codigo $nombre  ELIMINADO ");
-	 }
-
-	 function chexiste($codigo){
-		$codigo=$this->input->post('codigo');
-		//echo 'aquiii'.$fecha;
-		$check=$this->datasis->dameval("SELECT COUNT(*) FROM rete WHERE codigo='$codigo'");
-		if ($check > 0){
-		 $activida=$this->datasis->dameval("SELECT activida FROM rete WHERE codigo='$codigo'");
-		 $this->validation->set_message('chexiste',"La retencion $codigo ya existe para la actividad $activida");
-		 return FALSE;
-		}else {
-		return TRUE;
-		}
-	 }
-
-	 function instalar(){
-		if (!$this->db->field_exists('ut','rete')) {
-		 $mSQL="ALTER TABLE rete CHANGE COLUMN tipocol tipocol CHAR(2) NULL DEFAULT '0.0' COLLATE 'utf8_unicode_ci' AFTER cuenta, ADD COLUMN ut DECIMAL(12,2) NULL DEFAULT NULL AFTER tipocol";
-		 $this->db->simple_query($mSQL);
-		}
-	 }
-
-
-	function grid(){
-		$start   = isset($_REQUEST['start'])  ? $_REQUEST['start']   :  0;
-		$limit   = isset($_REQUEST['limit'])  ? $_REQUEST['limit']   : 50;
-		$sort    = isset($_REQUEST['sort'])   ? $_REQUEST['sort']    : '[{"property":"tipo","direction":"ASC"},{"property":"codigo","direction":"ASC"}]';
-		$filters = isset($_REQUEST['filter']) ? $_REQUEST['filter']  : null;
-
-		$where = $this->datasis->extjsfiltro($filters);
-
-		$this->db->_protect_identifiers=false;
-		$this->db->select("*, IF(tipo='JD','Juridico Domiciliado', IF(tipo='JN','Juridico No Domiciliado', IF(tipo='NR','Natural Residente','Natural No Residente'))) persona");
-		$this->db->from('rete');
-		if (strlen($where)>1) $this->db->where($where, NULL, FALSE);
-
-		$sort = json_decode($sort, true);
-		for ( $i=0; $i<count($sort); $i++ ) {
-			$this->db->order_by($sort[$i]['property'],$sort[$i]['direction']);
-		}
-
-		$this->db->limit($limit, $start);
-		$query = $this->db->get();
-		$results = $this->db->count_all('rete');
-
-		$arr = $this->datasis->codificautf8($query->result_array());
-		echo '{success:true, message:"Loaded data", results:'. $results.', data:'.json_encode($arr).'}';
-	}
-
-	function crear(){
-		$js= file_get_contents('php://input');
-		$data= json_decode($js,true);
-		$campos   = $data['data'];
-		$codigo = $campos['codigo'];
-
-		if ( !empty($codigo) ) {
-			unset($campos['id']);
-			unset($campos['persona']);
-			// Revisa si existe ya ese contrato
-			if ($this->datasis->dameval("SELECT COUNT(*) FROM rete WHERE codigo='$codigo'") == 0)
-			{
-				$mSQL = $this->db->insert_string("rete", $campos );
-				$this->db->simple_query($mSQL);
-				logusu('rete',"RETENCION $codigo CREADO");
-				echo "{ success: true, message: 'Retencion Agregada'}";
-			} else {
-				echo "{ success: false, message: 'Ya existe una retencion con ese codigo!!'}";
-			}
-
-		} else {
-			echo "{ success: false, message: 'Falta el campo codigo!!'}";
-		}
-	}
-
-	function modificar(){
-		$js= file_get_contents('php://input');
-		$data= json_decode($js,true);
-		$campos = $data['data'];
-
-		$codigo = $campos['codigo'];
-		unset($campos['codigo']);
-		unset($campos['id']);
-		unset($campos['persona']);
-
-		$mSQL = $this->db->update_string("rete", $campos,"id=".$data['data']['id'] );
-		$this->db->simple_query($mSQL);
-		logusu('rete',"RETENCION $codigo ID ".$data['data']['id']." MODIFICADO");
-		echo "{ success: true, message: 'Retencion Modificada -> ".$data['data']['codigo']."'}";
-	}
-
-	function eliminar(){
-		$js= file_get_contents('php://input');
-		$data= json_decode($js,true);
-		$campos = $data['data'];
-
-		$codigo = $campos['codigo'];
-		$check =  $this->datasis->dameval("SELECT COUNT(*) FROM gser WHERE creten='$codigo'");
-
-		if ($check > 0){
-			echo "{ success: false, message: 'rete no puede ser Borrada'}";
-		} else {
-			$this->db->simple_query("DELETE FROM rete WHERE codigo='$codigo'");
-			logusu('rete',"rete $codigo ELIMINADO");
-			echo "{ success: true, message: 'rete Eliminada'}";
-		}
-	}
-
-
-
-
-//****************************************************************8
-//
-//
-//
-//****************************************************************8
-	function reteextjs(){
-		$encabeza='RETENCION';
-		$listados= $this->datasis->listados('rete');
-		$otros=$this->datasis->otros('rete', 'finanzas/rete');
-
-		$urlajax = 'finanzas/rete/';
-		$variables = "var mcuenta='';";
-
-		$tipos="['JD', 'Juridico Domiciliado'],['JN','Juridico No Domiciliado'],['NR','Natural Residente'],['NN','Natural No Res.']";
-
-
-		$funciones = "";
-
-		$valida = "
-		{ type: 'length', field: 'codigo',   min: 1 },
-		{ type: 'length', field: 'activida', min: 1 }
-		";
-
-		$columnas = "
-		{ header: 'codigo',     width:  50, sortable: true, dataIndex: 'codigo',   field: { type: 'textfield' }, filter: { type: 'string' }},
-		{ header: 'Actividad',  width: 300, sortable: true, dataIndex: 'activida', field: { type: 'textfield' }, filter: { type: 'string' }},
-		{ header: 'Base Imp.',  width:  50, sortable: true, dataIndex: 'base1',    field: { type: 'numeric'   }, filter: { type: 'numeric' }, align: 'right',renderer : Ext.util.Format.numberRenderer('0,000.00') },
-		{ header: 'Ret.%',      width:  50, sortable: true, dataIndex: 'tari1',    field: { type: 'numeric'   }, filter: { type: 'numeric' }, align: 'right',renderer : Ext.util.Format.numberRenderer('0,000.00') },
-		{ header: 'Exencion',   width:  70, sortable: true, dataIndex: 'pama1',    field: { type: 'numeric'   }, filter: { type: 'numeric' }, align: 'right',renderer : Ext.util.Format.numberRenderer('0,000.00') },
-		{ header: 'Cuenta',     width:  80, sortable: true, dataIndex: 'cuenta',   field: { type: 'textfield' }, filter: { type: 'string'  }},
-		{ header: 'Concepto',   width:  80, sortable: true, dataIndex: 'concepto', field: { type: 'textfield' }, filter: { type: 'string'  }},
-	";
-
-		$campos = "'id', 'codigo','activida','base1','tari1','pama1','tipo','cuenta', 'persona','concepto'";
-
-		$camposforma = "
-							{
-							xtype:'fieldset',
-							frame: false,
-							border: false,
-							labelAlign: 'right',
-							defaults: { xtype:'fieldset', labelWidth:70 },
-							style:'padding:4px',
-							layout: 'column',
-							items: [
-									{ xtype: 'textfield',   fieldLabel: 'Codigo',     name: 'codigo',   width:110, allowBlank: false, id: 'codigo' },
-									{ xtype: 'combo',       fieldLabel: 'Tipo',       name: 'tipo',     width:270, store: [".$tipos."], labelWidth:70},
-									{ xtype: 'textfield',   fieldLabel: 'Actividad',  name: 'activida', width:400, allowBlank: false },
-									{ xtype: 'combo',       fieldLabel: 'C.Contable', name: 'cuenta',   width:400, store: cplaStore, id: 'cuenta', mode: 'remote', hideTrigger: true, typeAhead: true, forceSelection: true, valueField: 'item', displayField: 'valor'},
-									{ xtype: 'textfield',   fieldLabel: 'Concepto',   name: 'concepto', width:120, allowBlank: true },
-								]
-							},{
-							xtype:'fieldset',
-							title: 'Valores',
-							frame: false,
-							border: false,
-							labelAlign: 'right',
-							defaults: { xtype:'fieldset', labelWidth:170 },
-							style:'padding:4px',
-							layout: 'column',
-							items: [
-									{ xtype: 'numberfield', fieldLabel: 'Base Imponible',  name: 'base1', width:260, hideTrigger: true, fieldStyle: 'text-align: right',  renderer : Ext.util.Format.numberRenderer('0,000.00') },
-									{ xtype: 'numberfield', fieldLabel: 'Retencion %',     name: 'tari1', width:260, hideTrigger: true, fieldStyle: 'text-align: right',  renderer : Ext.util.Format.numberRenderer('0,000.00') },
-									{ xtype: 'numberfield', fieldLabel: 'Pagos mayores a', name: 'pama1', width:260, hideTrigger: true, fieldStyle: 'text-align: right',  renderer : Ext.util.Format.numberRenderer('0,000.00') },
-								]
-							}
-		";
-
-		$titulow = 'Formas de Pago';
-
-		$winwidget = "
-				closable: false,
-				closeAction: 'destroy',
-				width: 450,
-				height: 340,
-				resizable: false,
-				modal: true,
-				items: [writeForm],
-				listeners: {
-					beforeshow: function() {
-						var form = this.down('writerform').getForm();
-						this.activeRecord = registro;
-						if (registro) {
-							mcuenta  = registro.data.cuenta;
-							cplaStore.proxy.extraParams.cuenta  = mcuenta  ;
-							cplaStore.load({ params: { 'cuenta': registro.data.cuenta,  'origen': 'beforeform' } });
-							form.loadRecord(registro);
-							form.findField('codigo').setReadOnly(true);
-						} else {
-							form.findField('codigo').setReadOnly(false);
-						}
-					}
-				}
-";
-
-		$stores = "
-var cplaStore = new Ext.data.Store({
-	fields: [ 'item', 'valor'],
-	autoLoad: false,
-	autoSync: false,
-	pageSize: 50,
-	pruneModifiedRecords: true,
-	totalProperty: 'results',
-	proxy: {
-		type: 'ajax',
-		url : urlApp + 'contabilidad/cpla/cplabusca',
-		extraParams: {  'cuenta': mcuenta, 'origen': 'store' },
-		reader: {type: 'json',totalProperty: 'results',root: 'data'}
-	},
-	method: 'POST'
-});
-		";
-
-		$features = "features: [{ ftype: 'grouping', groupHeaderTpl: '{name}' },{ ftype: 'filters', encode: 'json', local: false }],";
-
-		$agrupar = "		remoteSort: true,
-		groupField: 'persona',";
-
-		$data['listados']    = $listados;
-		$data['otros']       = $otros;
-		$data['encabeza']    = $encabeza;
-		$data['urlajax']     = $urlajax;
-		$data['variables']   = $variables;
-		$data['funciones']   = $funciones;
-		$data['valida']      = $valida;
-		$data['columnas']    = $columnas;
-		$data['campos']      = $campos;
-		$data['stores']      = $stores;
-		$data['camposforma'] = $camposforma;
-		$data['titulow']     = $titulow;
-		$data['winwidget']   = $winwidget;
-		$data['features']    = $features;
-		$data['agrupar']     = $agrupar;
-
-		$data['title']  = heading('Retenciones');
-		$this->load->view('extjs/extjsven',$data);
-
-	}
-
-}
-*/
 ?>
--- proteoerp/system/application/views/editorcm.php
+++ proteoerp/system/application/views/editorcm.php
@@ -23,6 +23,7 @@ echo script('codemirror/php.js');
 echo script('codemirror/search.js');
 echo script('codemirror/searchcursor.js');
 echo script('codemirror/dialog.js');
+echo script('codemirror/fullscreen.js');
 
 echo style('codemirror/dialog.css');
 echo style('codemirror/docs.css');
@@ -31,38 +32,87 @@ echo style('codemirror/docs.css');
 .CodeMirror-fullscreen {
 	display: block;
 	position: absolute;
-	top: 10; left: 0;
-	height: 100%;
-	width: 100%;
+	top: 10; left: 0;bottom: 10;
+	height: 100%; width: 100%;
 	z-index: 9999;
 }
+.CodeMirror { border: 1px solid black; font-size:13px;height: auto; }
+.ui-tabs .ui-tabs-nav li a { padding: 0 0.8em; font-size: 0.7em; }
 </style>
+<script>
+$(function() { $( "#tabs" ).tabs();	});
+</script>
 </head>
 <body >
 
-<form id='fprog' method='POST'>
-<table width='100%'>
+<div id="tabs">
+<ul>
+	<li><a href="#tabs-1">Programa</a></li>
+	<li><a href="#tabs-2">Vista</a></li>
+	<li><a href="#tabs-3">Reporte</a></li>
+</ul>
+
+<div id="tabs-1">
+	<form id='fprog' method='POST'>
+	<table width='100%'>
 	<tr>
 		<td>Tabla: <input id='bd' name='bd' value="<?php echo $bd; ?>" /></td>
 		<td>Controlador: <input id='contro' name='contro' value="<?php echo $controlador; ?>"/></td>
-		<td align='right'><a href='#' id='generar' style='color:black;font-size:10pt;' >Generar</a></td>
-		<td align='right'><a href='#' id='guardar' style='color:black;font-size:10pt;' >Guardar al Archivo</a></td>
-		<td align='right'><a href='#' id='cargar'  style='color:black;font-size:10pt;' >Cargar del Archivo</a></td>
+		<td align='right'><a href='#' id='generar' style='color:black;font-size:10pt;'>Generar</a></td>
+		<td align='right'><a href='#' id='guardar' style='color:black;font-size:10pt;'>Guardar</a></td>
+		<td align='right'><a href='#' id='cargar'  style='color:black;font-size:10pt;'>Cargar </a></td>
 	</tr>
-</table>
-<div >
-<textarea id="code" name="code">
-<?php echo $programa; ?>
-</textarea>
-</form>
+	</table>
+	<div>
+		<textarea id="code" name="code"><?php echo $programa; ?></textarea>
+	</div>
+	</form>
 </div>
+
+<div id="tabs-2">
+	<form id='vprog' method='POST'>
+	<table width='100%'>
+	<tr>
+		<td>Vista: <input id='vbd' name='vbd' value="<?php echo $vbd; ?>" /></td>
+		<td align='right'><a href='#' id='vgenerar' style='color:black;font-size:10pt;' >Generar</a></td>
+		<td align='right'><a href='#' id='vguardar' style='color:black;font-size:10pt;' >Guardar</a></td>
+		<td align='right'><a href='#' id='vcargar'  style='color:black;font-size:10pt;' >Cargar </a></td>
+	</tr>
+	</table>
+	<div>
+		<textarea id="view" name="view"><?php echo $vista; ?></textarea>
+	</div>
+	</form>
+</div>
+
+<div id="tabs-3">
+	<form id='rprog' method='POST'>
+	<table width='100%'>
+	<tr>
+		<td>Reporte: <input id='rbd' name='rbd' value="<?php echo $rbd; ?>" /></td>
+		<td align='right'><a href='#' id='rgenerar' style='color:black;font-size:10pt;' >Generar</a></td>
+		<td align='right'><a href='#' id='rguardar' style='color:black;font-size:10pt;' >Guardar</a></td>
+		<td align='right'><a href='#' id='rcargar'  style='color:black;font-size:10pt;' >Cargar </a></td>
+	</tr>
+	</table>
+	<div>
+		<textarea id="repo" name="repo"><?php echo $reporte; ?></textarea>
+	</div>
+	</form>
+
+</div>
+</div>
+
 <script>
+/*
 function isFullScreen(cm) {
 	return /\bCodeMirror-fullscreen\b/.test(cm.getWrapperElement().className);
 }
+*/
 function winHeight() {
 	return window.innerHeight || (document.documentElement || document.body).clientHeight;
 }
+/*
 function setFullScreen(cm, full) {
 	var wrap = cm.getWrapperElement();
 	if (full) {
@@ -76,6 +126,8 @@ function setFullScreen(cm, full) {
 	}
 	cm.refresh();
 }
+*/
+
 CodeMirror.on(window, "resize", function() {
 	var showing = document.body.getElementsByClassName("CodeMirror-fullscreen")[0];
 	if (!showing) return;
@@ -87,16 +139,54 @@ var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
 	theme: "solarized",
 	matchBrackets: true,
 	mode: "application/x-httpd-php",
-	indentUnit: 4,
+	indentUnit: 3,
 	indentWithTabs: true,
 	enterMode: "keep",
-	tabMode: "shift",
+	//tabMode: "shift",
 	extraKeys: {
-		"F6": function(cm) {
-			setFullScreen(cm, !isFullScreen(cm));
+		"F11": function(cm) {
+			cm.setOption("fullScreen", !cm.getOption("fullScreen"));
 		},
 		"Esc": function(cm) {
-			if (isFullScreen(cm)) setFullScreen(cm, false);
+			if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
+		}
+	}
+});
+
+var vista = CodeMirror.fromTextArea(document.getElementById("view"), {
+	lineNumbers: true,
+	theme: "solarized",
+	matchBrackets: true,
+	mode: "application/x-httpd-php",
+	indentUnit: 3,
+	indentWithTabs: true,
+	enterMode: "keep",
+	//tabMode: "shift",
+	extraKeys: {
+		"F11": function(cm) {
+			cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+		},
+		"Esc": function(cm) {
+			if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
+		}
+	}
+});
+
+var reporte = CodeMirror.fromTextArea(document.getElementById("repo"), {
+	lineNumbers: true,
+	theme: "solarized",
+	matchBrackets: true,
+	mode: "application/x-httpd-php",
+	indentUnit: 3,
+	indentWithTabs: true,
+	enterMode: "keep",
+	//tabMode: "shift",
+	extraKeys: {
+		"F11": function(cm) {
+			cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+		},
+		"Esc": function(cm) {
+			if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
 		}
 	}
 });
@@ -106,8 +196,32 @@ $(function() {
 });
 
 $('#guardar').click(function(){
-	$('#fprog').attr("action","<?php echo site_url('/desarrollo/jqguarda') ?>");
-	$('#fprog').submit();
+
+	var forma = $('#fprog');
+	$('#code').val(editor.getValue()); 
+
+	forma.submit(function(event) {
+		event.preventDefault();
+		$.ajax({
+			type: forma.attr('method'),
+			url: '<?php echo site_url('desarrollo/jqguarda'); ?>',
+			data: forma.serialize(),
+			success: function(resulta){ alert(resulta);}		
+		}).done( function() {
+			alert('Guardado');
+		}).fail( function(){
+			alert('Error');
+		});
+		return false;
+	});
+
+	forma.submit();
+
+
+//	forma.attr("action","<?php echo site_url('/desarrollo/jqguarda') ?>");
+//	forma.submit();
+//	alert('Hola');
+	
 });
 
 $('#generar').click(function(){
@@ -117,7 +231,6 @@ $('#generar').click(function(){
 $('#cargar').click(function(){
 	window.location = "<?php echo site_url('desarrollo/jqcargar'); ?>"+"/"+$('#bd').val()+"/"+$('#contro').val();
 });
-
 
 </script>
 </body>
