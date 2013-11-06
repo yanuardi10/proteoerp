d6f4f22bc6cadf845783b10f54d43d798046daa4
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2013-11-06 22:25:16 +0000

    -Title en el checkbox

--- trunk/proteoerp/system/application/rapyd/fields/checkbox.php
+++ trunk/proteoerp/system/application/rapyd/fields/checkbox.php
@@ -8,8 +8,8 @@
  * @license http://www.fsf.org/licensing/licenses/lgpl.txt LGPL
  * @version 0.9.6
  */
- 
- 
+
+
 /**
  * selectField
  *
@@ -19,86 +19,88 @@
  */
 class checkboxField extends objField{
 
-  var $checked = false;
-  var $type = "checkbox";
-  var $style = "vertical-align:middle";
-  
-  //costruttore
-  function checkboxField($label, $name, $true_value, $false_value=""){
-  
-    parent::objField($label, $name);
-    
-    $this->true_value = $true_value;
-    $this->false_value = $false_value;
-  }
-  
-  function _getValue(){
-    parent::_getValue();
+	var $checked = false;
+	var $type  = 'checkbox';
+	var $style = 'vertical-align:middle';
 
-    if ($_POST && !isset($this->request[$this->name])){
-     $this->value = $this->false_value;
-    }
-    
-    if ($this->value == $this->true_value){
-      $this->checked = true;
-    } else {
-      $this->checked = false;    
-    }
+	//costruttore
+	function checkboxField($label, $name, $true_value, $false_value=''){
 
-  }
-  
-  function _getNewValue(){
-    parent::_getNewValue();
-    if (!isset($this->request[$this->name])){
-     $this->newValue = $this->false_value;
-    }
+		parent::objField($label, $name);
 
-  }
+		$this->true_value = $true_value;
+		$this->false_value = $false_value;
+	}
+
+	function _getValue(){
+		parent::_getValue();
+
+		if ($_POST && !isset($this->request[$this->name])){
+			$this->value = $this->false_value;
+		}
+
+		if($this->value == $this->true_value){
+			$this->checked = true;
+		}else{
+			$this->checked = false;
+		}
+
+	}
+
+	function _getNewValue(){
+		parent::_getNewValue();
+		if(!isset($this->request[$this->name])){
+		 $this->newValue = $this->false_value;
+		}
+	}
 
 
-  function build(){
-    
-    $output = "";
-    
-    $this->_getValue();
-  
-    switch ($this->status){
-      case "disabled":
-      case "show":
-        if (!isset($this->value)){
-          $output = RAPYD_FIELD_SYMBOL_NULL;
-        } elseif ($this->value == ""){
-          $output = "";
-        } else {  
-          $output = $this->value;
-        }
-        break;
-        
-      case "create":
-      case "modify":
-	      $onchange = "";
-        $onclick = "";
-        if ($this->onchange!=""){
-          $onchange = ' onchange="'.$this->onchange.'"';
-        }
-        if ($this->onclick!=""){
-          $onclick = ' onclick="'.$this->onclick.'"';
-        }
-        $id = ' id="'.$this->name.'"';
-        
-        $output = form_checkbox($this->name, $this->true_value , $this->checked, $id.$onchange.$onclick).$this->extra_output;
-        break;
-        
-      case "hidden":
-      
-        $output = form_hidden($this->name, $this->value);
-        
-        break;
-        
-      default:
-    }
-    $this->output = $output;
-  }
-    
+	function build(){
+		$output = '';
+		$this->_getValue();
+
+		switch ($this->status){
+			case 'disabled':
+			case 'show':
+				if (!isset($this->value)){
+					$output = RAPYD_FIELD_SYMBOL_NULL;
+				} elseif ($this->value == ""){
+					$output = "";
+				} else {
+					$output = $this->value;
+				}
+				break;
+
+			case 'create':
+			case 'modify':
+				$onchange = '';
+				$onclick = '';
+				if ($this->onchange!=''){
+					$onchange = ' onchange="'.$this->onchange.'"';
+				}
+				if ($this->onclick!=''){
+					$onclick = ' onclick="'.$this->onclick.'"';
+				}
+				$id = ' id="'.$this->name.'"';
+
+				if(!empty($this->title)){
+					$title=' title = "'.form_prep($this->title).'" ';
+				}else{
+					$title='';
+				}
+
+				$output = form_checkbox($this->name, $this->true_value , $this->checked, $id.$onchange.$title.$onclick).$this->extra_output;
+				break;
+
+			case 'hidden':
+
+				$output = form_hidden($this->name, $this->value);
+
+				break;
+
+			default:
+		}
+		$this->output = $output;
+	}
 }
 ?>
