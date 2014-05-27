f466261009521df8eb6edb7e132818dd4de33ca8
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2014-05-27 13:10:48 +0000

    -Mejora en los reportes

--- trunk/proteoerp/system/application/libraries/HTMLReporte.php
+++ trunk/proteoerp/system/application/libraries/HTMLReporte.php
@@ -605,20 +605,53 @@ border-bottom-right-radius:5px;
 		$rt =trim($str);
 
 		//Convierte los caracteres de us-ascii
-		$rt =str_replace(utf8_encode(chr(165)),'Ñ',$rt);
-		$rt =str_replace(utf8_encode(chr(164)),'ñ',$rt);
-		$rt =str_replace(utf8_encode(chr(166)),'º',$rt);
+		if($this->is_utf8($rt)){
+			$rt =str_replace(utf8_encode(chr(165)),'Ñ',$rt);
+			$rt =str_replace(utf8_encode(chr(164)),'ñ',$rt);
+			$rt =str_replace(utf8_encode(chr(166)),'º',$rt);
 
-		//if($this->DBcharset=='latin1'){
-		//	$rt=utf8_encode($rt);
-		//}
+			$rt =htmlspecialchars($rt,ENT_COMPAT,'UTF-8');
 
-		$rt =htmlspecialchars($rt,ENT_COMPAT,'UTF-8');
-		if($this->DBcharset=='latin1'){
-			$rt= utf8_decode($rt);
+			if($this->DBcharset=='latin1'){
+				$rt= utf8_decode($rt);
+			}
+		}else{
+			$rt =str_replace(chr(165),'Ñ',$rt);
+			$rt =str_replace(chr(164),'ñ',$rt);
+			$rt =str_replace(chr(166),'º',$rt);
+
+			$rt =htmlspecialchars($rt,ENT_COMPAT,'ISO-8859-1');
 		}
+
 		return $rt;
 	}
+
+	function is_utf8($str){
+		$c=0; $b=0;
+		$bits=0;
+		$len=strlen($str);
+		for($i=0; $i<$len; $i++){
+			$c=ord($str[$i]);
+			if($c > 128){
+				if(($c >= 254)) return false;
+				elseif($c >= 252) $bits=6;
+				elseif($c >= 248) $bits=5;
+				elseif($c >= 240) $bits=4;
+				elseif($c >= 224) $bits=3;
+				elseif($c >= 192) $bits=2;
+				else return false;
+				if(($i+$bits) > $len) return false;
+				while($bits > 1){
+					$i++;
+					$b=ord($str[$i]);
+					if($b < 128 || $b > 191) return false;
+					$bits--;
+				}
+			}
+		}
+		return true;
+	}
+
 }
 
 class PDFReporte extends HTMLReporte{
