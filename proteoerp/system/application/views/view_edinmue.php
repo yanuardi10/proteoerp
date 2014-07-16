1b3570708fa0e824f9b1d52d1257c7fce629119f
datasql@gmail.com <datasql@gmail.com@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2014-07-16 14:30:49 +0000

    Condominio

---
+++ proteoerp/system/application/views/view_edinmue.php
@@ -0,0 +1,85 @@
+<?php
+/**
+* ProteoERP
+*
+* @autor    Andres Hocevar
+* @license  GNU GPL v3
+*/
+echo $form_scripts;
+echo $form_begin;
+
+if(isset($form->error_string)) echo '<div class="alert">'.$form->error_string.'</div>';
+if($form->_status <> 'show'){ ?>
+
+<script language="javascript" type="text/javascript">
+</script>
+<?php } ?>
+
+<fieldset  style='border: 1px outset #FEB404;background: #FFFCE8;'>
+<table width='100%'>
+	<tr>
+		<td class="littletablerowth"><?php echo $form->codigo->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->codigo->output; ?></td>
+	</tr>
+	<tr>
+		<td class="littletablerowth"><?php echo $form->descripcion->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->descripcion->output; ?></td>
+	</tr>
+</table>
+</fieldset>
+
+<fieldset  style='border: 1px outset #FEB404;background: #FFFCE8;'>
+<table width='100%'>
+	<tr>
+		<td class="littletablerowth"><?php echo $form->edificacion->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->edificacion->output; ?></td>
+		<td class="littletablerowth"><?php echo $form->status->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->status->output; ?></td>
+	</tr>
+	<tr>
+		<td class="littletablerowth"><?php echo $form->ubicacion->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->ubicacion->output; ?></td>
+		<td class="littletablerowth"><?php echo $form->uso->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->uso->output; ?></td>
+	</tr>
+	<tr>
+		<td class="littletablerowth"><?php echo $form->objeto->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->objeto->output; ?></td>
+		<td class="littletablerowth"><?php echo $form->usoalter->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->usoalter->output; ?></td>
+	</tr>
+</table>
+</fieldset>
+
+<fieldset  style='border: 1px outset #FEB404;background: #FFFCE8;'>
+<table width='100%'>
+	<tr>
+		<td class="littletablerowth"><?php echo $form->caracteristicas->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->caracteristicas->output; ?></td>
+	</tr>
+</table>
+</fieldset>
+
+<fieldset  style='border: 1px outset #FEB404;background: #FFFCE8;'>
+<table width='100%'>
+	<tr>
+		<td class="littletablerowth"><?php echo $form->area->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->area->output; ?></td>
+		<td class="littletablerowth"><?php echo $form->preciomt2c->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->preciomt2c->output; ?></td>
+	</tr>
+	<tr>
+		<td class="littletablerowth"><?php echo $form->deposito->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->deposito->output; ?></td>
+		<td class="littletablerowth"><?php echo $form->preciomt2e->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->preciomt2e->output; ?></td>
+	</tr>
+	<tr>
+		<td class="littletablerowth"><?php echo $form->estaciona->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->estaciona->output; ?></td>
+		<td class="littletablerowth"><?php echo $form->preciomt2a->label;  ?></td>
+		<td class="littletablerow"  ><?php echo $form->preciomt2a->output; ?></td>
+	</tr>
+</table>
+</fieldset>
+<?php echo $form_end; ?>
