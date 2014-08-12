5fceb2a4d4a0337596b5ee1a732326e1792ab176
datasql@gmail.com <datasql@gmail.com@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2014-08-12 01:30:21 +0000

    Tabulador de Historias

---
+++ proteoerp/system/application/controllers/medico/medhtab.php
@@ -0,0 +1,527 @@
+<?php
+/**
+* ProteoERP
+*
+* @autor    Andres Hocevar
+* @license  GNU GPL v3
+*/
+class Medhtab extends Controller {
+	var $mModulo = 'MEDHTAB';
+	var $titp    = 'TABULADOR DE HISTORIAS';
+	var $tits    = 'TABULADOR DE HISTORIAS';
+	var $url     = 'medico/medhtab/';
+
+	function Medhtab(){
+		parent::Controller();
+		$this->load->library('rapyd');
+		$this->load->library('jqdatagrid');
+		$this->datasis->modulo_nombre( 'MEDHTAB', $ventana=0 );
+	}
+
+	function index(){
+		$this->datasis->creaintramenu(array('modulo'=>'171','titulo'=>'Tabulador','mensaje'=>'Tabulador','panel'=>'SALUD','ejecutar'=>'medico/medhtab','target'=>'popu','visible'=>'S','pertenece'=>'1','ancho'=>900,'alto'=>600));
+		$this->datasis->modintramenu( 800, 600, substr($this->url,0,-1) );
+		redirect($this->url.'jqdatag');
+	}
+
+	//******************************************************************
+	// Layout en la Ventana
+	//
+	function jqdatag(){
+
+		$grid = $this->defgrid();
+		$param['grids'][] = $grid->deploy();
+
+		//Funciones que ejecutan los botones
+		$bodyscript = $this->bodyscript( $param['grids'][0]['gridname']);
+
+		//Botones Panel Izq
+		$grid->wbotonadd(array("id"=>"grupos",   "img"=>"images/pdf_logo.gif",  "alt" => "Grupos", "label"=>"Grupos"));
+		$WestPanel = $grid->deploywestp();
+
+		$adic = array(
+			array('id'=>'fedita',  'title'=>'Agregar/Editar Registro'),
+			array('id'=>'fshow' ,  'title'=>'Mostrar Registro'),
+			array('id'=>'fborra',  'title'=>'Eliminar Registro')
+		);
+		$SouthPanel = $grid->SouthPanel($this->datasis->traevalor('TITULO1'), $adic);
+
+		$param['WestPanel']   = $WestPanel;
+		//$param['EastPanel'] = $EastPanel;
+		$param['SouthPanel']  = $SouthPanel;
+		$param['listados']    = $this->datasis->listados('MEDHTAB', 'JQ');
+		$param['otros']       = $this->datasis->otros('MEDHTAB', 'JQ');
+		$param['temas']       = array('proteo','darkness','anexos1');
+		$param['bodyscript']  = $bodyscript;
+		$param['tabs']        = false;
+		$param['encabeza']    = $this->titp;
+		$param['tamano']      = $this->datasis->getintramenu( substr($this->url,0,-1) );
+		$this->load->view('jqgrid/crud2',$param);
+	}
+
+	//******************************************************************
+	// Funciones de los Botones
+	//
+	function bodyscript( $grid0 ){
+		$bodyscript = '<script type="text/javascript">';
+		$ngrid = '#newapi'.$grid0;
+
+		// Grupos
+		$bodyscript .= '
+		$("#grupos").click(function(){
+			$.post("'.site_url('medico/medhtab/grupoform').'",
+			function(data){
+				$("#fshow").html(data);
+				$("#fshow").dialog( { title:"GRUPOS", width: 430, height: 400, modal: true } );
+				$("#fshow").dialog( "open" );
+			});
+		});';
+
+
+		$bodyscript .= $this->jqdatagrid->bsshow('medhtab', $ngrid, $this->url );
+		$bodyscript .= $this->jqdatagrid->bsadd( 'medhtab', $this->url );
+		$bodyscript .= $this->jqdatagrid->bsdel( 'medhtab', $ngrid, $this->url );
+		$bodyscript .= $this->jqdatagrid->bsedit('medhtab', $ngrid, $this->url );
+
+		//Wraper de javascript
+		$bodyscript .= $this->jqdatagrid->bswrapper($ngrid);
+
+		$bodyscript .= $this->jqdatagrid->bsfedita( $ngrid, '300', '500' );
+		$bodyscript .= $this->jqdatagrid->bsfshow( '300', '400' );
+		$bodyscript .= $this->jqdatagrid->bsfborra( $ngrid, '300', '400' );
+
+		$bodyscript .= '});';
+
+		$bodyscript .= '</script>';
+
+		return $bodyscript;
+	}
+
+	//******************************************************************
+	// Definicion del Grid o Tabla 
+	//
+	function defgrid( $deployed = false ){
+		$i      = 1;
+		$editar = "false";
+
+		$grid  = new $this->jqdatagrid;
+
+
+		$grid->addField('grupo');
+		$grid->label('Grupo');
+		$grid->params(array(
+			'search'        => 'true',
+			'editable'      => $editar,
+			'align'         => "'right'",
+			'edittype'      => "'text'",
+			'width'         => 100,
+			'editrules'     => '{ required:true }',
+			'editoptions'   => '{ size:10, maxlength: 10, dataInit: function (elem) { $(elem).numeric(); }  }',
+			'formatter'     => "'number'",
+			'formatoptions' => '{decimalSeparator:".", thousandsSeparator: ",", decimalPlaces: 0 }'
+		));
+
+		$grid->addField('indice');
+		$grid->label('Indice');
+		$grid->params(array(
+			'search'        => 'true',
+			'editable'      => $editar,
+			'align'         => "'right'",
+			'edittype'      => "'text'",
+			'width'         => 60,
+			'editrules'     => '{ required:true }',
+			'editoptions'   => '{ size:10, maxlength: 10, dataInit: function (elem) { $(elem).numeric(); }  }',
+			'formatter'     => "'number'",
+			'formatoptions' => '{decimalSeparator:".", thousandsSeparator: ",", decimalPlaces: 0 }'
+		));
+
+
+		$grid->addField('nombre');
+		$grid->label('Nombre');
+		$grid->params(array(
+			'search'        => 'true',
+			'editable'      => $editar,
+			'width'         => 200,
+			'edittype'      => "'text'",
+			'editrules'     => '{ required:true}',
+			'editoptions'   => '{ size:80, maxlength: 80 }',
+		));
+
+
+		$grid->addField('descripcion');
+		$grid->label('Descripcion');
+		$grid->params(array(
+			'search'        => 'true',
+			'editable'      => $editar,
+			'width'         => 250,
+			'edittype'      => "'textarea'",
+			'editoptions'   => "'{rows:2, cols:60}'",
+		));
+
+		$grid->addField('id');
+		$grid->label('ID');
+		$grid->params(array(
+			'align'         => "'center'",
+			'frozen'        => 'true',
+			'width'         => 40,
+			'editable'      => 'false',
+			'search'        => 'false'
+		));
+
+
+		$grid->showpager(true);
+		$grid->setWidth('');
+		$grid->setHeight('290');
+		$grid->setTitle($this->titp);
+		$grid->setfilterToolbar(true);
+		$grid->setToolbar('false', '"top"');
+
+		$grid->setFormOptionsE('closeAfterEdit:true, mtype: "POST", width: 520, height:300, closeOnEscape: true, top: 50, left:20, recreateForm:true, afterSubmit: function(a,b){if (a.responseText.length > 0) $.prompt(a.responseText); return [true, a ];},afterShowForm: function(frm){$("select").selectmenu({style:"popup"});} ');
+		$grid->setFormOptionsA('closeAfterAdd:true,  mtype: "POST", width: 520, height:300, closeOnEscape: true, top: 50, left:20, recreateForm:true, afterSubmit: function(a,b){if (a.responseText.length > 0) $.prompt(a.responseText); return [true, a ];},afterShowForm: function(frm){$("select").selectmenu({style:"popup"});} ');
+		$grid->setAfterSubmit("$('#respuesta').html('<span style=\'font-weight:bold; color:red;\'>'+a.responseText+'</span>'); return [true, a ];");
+
+		$grid->setOndblClickRow('');		#show/hide navigations buttons
+		$grid->setAdd(    $this->datasis->sidapuede('MEDHTAB','INCLUIR%' ));
+		$grid->setEdit(   $this->datasis->sidapuede('MEDHTAB','MODIFICA%'));
+		$grid->setDelete( $this->datasis->sidapuede('MEDHTAB','BORR_REG%'));
+		$grid->setSearch( $this->datasis->sidapuede('MEDHTAB','BUSQUEDA%'));
+		$grid->setRowNum(30);
+		$grid->setShrinkToFit('false');
+
+		$grid->setBarOptions("addfunc: medhtabadd, editfunc: medhtabedit, delfunc: medhtabdel, viewfunc: medhtabshow");
+
+		#Set url
+		$grid->setUrlput(site_url($this->url.'setdata/'));
+
+		#GET url
+		$grid->setUrlget(site_url($this->url.'getdata/'));
+
+		if ($deployed) {
+			return $grid->deploy();
+		} else {
+			return $grid;
+		}
+	}
+
+	//******************************************************************
+	// Busca la data en el Servidor por json
+	//
+	function getdata(){
+		$grid       = $this->jqdatagrid;
+
+		// CREA EL WHERE PARA LA BUSQUEDA EN EL ENCABEZADO
+		$mWHERE = $grid->geneTopWhere('medhtab');
+
+		$response   = $grid->getData('medhtab', array(array()), array(), false, $mWHERE );
+		$rs = $grid->jsonresult( $response);
+		echo $rs;
+	}
+
+	//******************************************************************
+	// Guarda la Informacion del Grid o Tabla
+	//
+	function setData(){
+		$this->load->library('jqdatagrid');
+		$oper   = $this->input->post('oper');
+		$id     = $this->input->post('id');
+		$data   = $_POST;
+		$mcodp  = "??????";
+		$check  = 0;
+
+		unset($data['oper']);
+		unset($data['id']);
+		if($oper == 'add'){
+			if(false == empty($data)){
+				$check = $this->datasis->dameval("SELECT count(*) FROM medhtab WHERE $mcodp=".$this->db->escape($data[$mcodp]));
+				if ( $check == 0 ){
+					$this->db->insert('medhtab', $data);
+					echo "Registro Agregado";
+
+					logusu('MEDHTAB',"Registro ????? INCLUIDO");
+				} else
+					echo "Ya existe un registro con ese $mcodp";
+			} else
+				echo "Fallo Agregado!!!";
+
+		} elseif($oper == 'edit') {
+			$nuevo  = $data[$mcodp];
+			$anterior = $this->datasis->dameval("SELECT $mcodp FROM medhtab WHERE id=$id");
+			if ( $nuevo <> $anterior ){
+				//si no son iguales borra el que existe y cambia
+				$this->db->query("DELETE FROM medhtab WHERE $mcodp=?", array($mcodp));
+				$this->db->query("UPDATE medhtab SET $mcodp=? WHERE $mcodp=?", array( $nuevo, $anterior ));
+				$this->db->where("id", $id);
+				$this->db->update("medhtab", $data);
+				logusu('MEDHTAB',"$mcodp Cambiado/Fusionado Nuevo:".$nuevo." Anterior: ".$anterior." MODIFICADO");
+				echo "Grupo Cambiado/Fusionado en clientes";
+			} else {
+				unset($data[$mcodp]);
+				$this->db->where("id", $id);
+				$this->db->update('medhtab', $data);
+				logusu('MEDHTAB',"Grupo de Cliente  ".$nuevo." MODIFICADO");
+				echo "$mcodp Modificado";
+			}
+
+		} elseif($oper == 'del') {
+			$meco = $this->datasis->dameval("SELECT $mcodp FROM medhtab WHERE id=$id");
+			//$check =  $this->datasis->dameval("SELECT COUNT(*) FROM medhtab WHERE id='$id' ");
+			if ($check > 0){
+				echo " El registro no puede ser eliminado; tiene movimiento ";
+			} else {
+				$this->db->query("DELETE FROM medhtab WHERE id=$id ");
+				logusu('MEDHTAB',"Registro ????? ELIMINADO");
+				echo "Registro Eliminado";
+			}
+		};
+	}
+
+	//******************************************************************
+	// Forma de Marcas
+	//
+	function grupoform(){
+		$grid  = new $this->jqdatagrid;
+
+		$grid->addField('id');
+		$grid->label('ID');
+		$grid->params(array(
+			'hidden'      => 'true',
+			'align'       => "'center'",
+			'width'       => 20,
+			'editable'    => 'false',
+			'editoptions' => '{readonly:true,size:10}'
+			)
+		);
+
+		$grid->addField('nombre');
+		$grid->label('Grupo');
+		$grid->params(array(
+			'width'     => 180,
+			'editable'  => 'true',
+			'edittype'  => "'text'",
+			'editrules' => '{required:true}'
+			)
+		);
+
+		$grid->showpager(true);
+		$grid->setViewRecords(false);
+		$grid->setWidth('410');
+		$grid->setHeight('220');
+
+		$grid->setUrlget(site_url('medico/medhtab/grupogd/'));
+		$grid->setUrlput(site_url('medico/medhtab/gruposd/'));
+
+		$mgrid = $grid->deploy();
+
+		$msalida  = '<script type="text/javascript">'."\n";
+		$msalida .= '
+		$("#newapi'.$mgrid['gridname'].'").jqGrid({
+			ajaxGridOptions : {type:"POST"}
+			,jsonReader : { root:"data", repeatitems: false }
+			'.$mgrid['table'].'
+			,scroll: true
+			,pgtext: null, pgbuttons: false, rowList:[]
+		})
+		$("#newapi'.$mgrid['gridname'].'").jqGrid(\'navGrid\',  "#pnewapi'.$mgrid['gridname'].'",{edit:false, add:false, del:true, search: false});
+		$("#newapi'.$mgrid['gridname'].'").jqGrid(\'inlineNav\',"#pnewapi'.$mgrid['gridname'].'");
+		$("#newapi'.$mgrid['gridname'].'").jqGrid(\'filterToolbar\');
+		';
+
+		$msalida .= "\n</script>\n";
+		$msalida .= '<id class="anexos"><table id="newapi'.$mgrid['gridname'].'"></table>';
+		$msalida .= '<div   id="pnewapi'.$mgrid['gridname'].'"></div></div>';
+
+		echo $msalida;
+
+	}
+
+	//******************************************************************
+	// Busca la data en el Servidor por json
+	//
+	function grupogd(){
+		$grid       = $this->jqdatagrid;
+		// CREA EL WHERE PARA LA BUSQUEDA EN EL ENCABEZADO
+		$mWHERE = $grid->geneTopWhere('medhgrup');
+		$response   = $grid->getData('medhgrup', array(array()), array(), false, $mWHERE );
+		$rs = $grid->jsonresult( $response);
+		echo $rs;
+	}
+
+	//******************************************************************
+	// Guarda la Informacion del Grid o Tabla
+	//
+	function gruposd(){
+		$this->load->library('jqdatagrid');
+		$oper   = $this->input->post('oper');
+		$id     = $this->input->post('id');
+		$data   = $_POST;
+		$mcodp  = "??????";
+		$check  = 0;
+
+		unset($data['oper']);
+		unset($data['id']);
+		if($oper == 'add'){
+			if(false == empty($data)){
+				$check = $this->datasis->dameval("SELECT count(*) FROM medhgrup WHERE nombre=".$this->db->escape($data['nombre']));
+				if ( $check == 0 ){
+					$this->db->insert('medhgrup', $data);
+					echo "Registro Agregado";
+					logusu('MEDHGRUP',"Registro  INCLUIDO");
+				} else
+					echo "Ya existe un registro con ese nombre";
+			} else
+				echo "Fallo Agregado!!!";
+
+		} elseif($oper == 'edit') {
+			$nuevo  = $data[$mcodp];
+			$anterior = $this->datasis->dameval("SELECT $mcodp FROM medhgrup WHERE id=$id");
+			if ( $nuevo <> $anterior ){
+				//si no son iguales borra el que existe y cambia
+				$this->db->query("DELETE FROM medhgrup WHERE $mcodp=?", array($mcodp));
+				$this->db->query("UPDATE medhgrup SET $mcodp=? WHERE $mcodp=?", array( $nuevo, $anterior ));
+				$this->db->where("id", $id);
+				$this->db->update("medhgrup", $data);
+				logusu('MEDHGRUP',"$mcodp Cambiado/Fusionado Nuevo:".$nuevo." Anterior: ".$anterior." MODIFICADO");
+				echo "Grupo Cambiado/Fusionado en clientes";
+			} else {
+				unset($data[$mcodp]);
+				$this->db->where("id", $id);
+				$this->db->update('medhgrup', $data);
+				logusu('MEDHGRUP',"Grupo de Cliente  ".$nuevo." MODIFICADO");
+				echo "$mcodp Modificado";
+			}
+
+		} elseif($oper == 'del') {
+			$meco = $this->datasis->dameval("SELECT $mcodp FROM medhgrup WHERE id=$id");
+			//$check =  $this->datasis->dameval("SELECT COUNT(*) FROM medhgrup WHERE id='$id' ");
+			if ($check > 0){
+				echo " El registro no puede ser eliminado; tiene movimiento ";
+			} else {
+				$this->db->query("DELETE FROM medhgrup WHERE id=$id ");
+				logusu('MEDHGRUP',"Registro ????? ELIMINADO");
+				echo "Registro Eliminado";
+			}
+		};
+	}
+
+
+
+
+
+	//******************************************************************
+	// Edicion 
+	function dataedit(){
+		
+		$idanterior = intval($this->uri->segment(5));
+		$grupo = 0;
+		if ( $idanterior ){
+			$grupo = $this->datasis->dameval("SELECT grupo FROM medhtab WHERE id=$idanterior");
+		}
+
+		$this->rapyd->load('dataedit');
+		$script= '
+		$(function() {
+			$("#fecha").datepicker({dateFormat:"dd/mm/yy"});
+			$(".inputnum").numeric(".");
+		});
+		';
+
+		$edit = new DataEdit('', 'medhtab');
+
+		$edit->script($script,'modify');
+		$edit->script($script,'create');
+		$edit->on_save_redirect=false;
+
+		$edit->back_url = site_url($this->url.'filteredgrid');
+
+		$edit->post_process('insert','_post_insert');
+		$edit->post_process('update','_post_update');
+		$edit->post_process('delete','_post_delete');
+		$edit->pre_process('insert', '_pre_insert' );
+		$edit->pre_process('update', '_pre_update' );
+		$edit->pre_process('delete', '_pre_delete' );
+
+		$edit->grupo = new dropdownField('Grupo','grupo');
+		$edit->grupo->option('','Seleccionar');
+		$edit->grupo->options('SELECT id, nombre FROM medhgrup ORDER BY nombre');
+		$edit->grupo->rule ='required';
+		$edit->grupo->style='width:180px;';
+		$edit->grupo->insertValue = $grupo;
+
+		$edit->indice = new inputField('Indice','indice');
+		$edit->indice->rule='integer';
+		$edit->indice->css_class='inputonlynum';
+		$edit->indice->size =6;
+		$edit->indice->maxlength =11;
+
+		$edit->nombre = new inputField('Nombre','nombre');
+		$edit->nombre->rule='';
+		$edit->nombre->size =52;
+		$edit->nombre->maxlength =80;
+
+		$edit->descripcion = new textareaField('Descripcion','descripcion');
+		$edit->descripcion->rule='';
+		$edit->descripcion->cols = 50;
+		$edit->descripcion->rows = 4;
+
+		$edit->build();
+
+		if($edit->on_success()){
+			$rt=array(
+				'status' =>'A',
+				'mensaje'=>'Registro guardado',
+				'pk'     =>$edit->_dataobject->pk
+			);
+			echo json_encode($rt);
+		}else{
+			echo $edit->output;
+		}
+	}
+
+	function _pre_insert($do){
+		$do->error_message_ar['pre_ins']='';
+		return true;
+	}
+
+	function _pre_update($do){
+		$do->error_message_ar['pre_upd']='';
+		return true;
+	}
+
+	function _pre_delete($do){
+		$do->error_message_ar['pre_del']='';
+		return false;
+	}
+
+	function _post_insert($do){
+		$primary =implode(',',$do->pk);
+		logusu($do->table,"Creo $this->tits $primary ");
+	}
+
+	function _post_update($do){
+		$primary =implode(',',$do->pk);
+		logusu($do->table,"Modifico $this->tits $primary ");
+	}
+
+	function _post_delete($do){
+		$primary =implode(',',$do->pk);
+		logusu($do->table,"Elimino $this->tits $primary ");
+	}
+
+	function instalar(){
+		if (!$this->db->table_exists('medhtab')) {
+			$mSQL="CREATE TABLE `medhtab` (
+			  `id` int(11) NOT NULL AUTO_INCREMENT,
+			  `grupo` int(11) NOT NULL DEFAULT '0',
+			  `nombre` varchar(80) DEFAULT NULL,
+			  `descripcion` text,
+			  PRIMARY KEY (`id`)
+			) ENGINE=MyISAM DEFAULT CHARSET=latin1 ROW_FORMAT=FIXED";
+			$this->db->query($mSQL);
+		}
+		//$campos=$this->db->list_fields('medhtab');
+		//if(!in_array('<#campo#>',$campos)){ }
+	}
+
+
+}
+?>
