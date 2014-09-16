2f6ab7560594775b8a59b4c567048cb5931818b9
datasql@gmail.com <datasql@gmail.com@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2014-09-16 21:02:50 +0000

    electores

---
+++ trunk/proteoerp/system/application/controllers/electores.php
@@ -0,0 +1,54 @@
+<?php
+/** ProteoERP
+ *  Busca Electores
+ *
+*/
+class Electores extends Controller {
+	var $autolimit=50; //Limite en el autocomplete;
+
+	function Electores(){
+		parent::Controller();
+		session_write_close();
+	}
+
+	function index(){
+
+	}
+
+	//***************************************
+	//           Auto complete
+	//***************************************
+	function busca(){
+		$papellido  = $this->uri->segment($this->uri->total_segments());
+		$pnombre    = $this->uri->segment($this->uri->total_segments()-1);
+
+		$dbpn = $this->db->escape($pnombre.'%');
+		$dbpa = $this->db->escape($papellido.'%');
+		
+		$data = 'Consulta Vacia';
+		$mSQL="SELECT CONCAT(nacionalidad,cedula) cedula, pnombre, snombre, papellido, sapellido, fecha, sexo
+			FROM matloca_vente.electores WHERE papellido LIKE ${dbpa} AND pnombre like ${dbpn} LIMIT 100";
+		$query = $this->db->query($mSQL);
+		if ($query->num_rows() > 0){
+			$data = '<table>';
+			foreach( $query->result_array() as  $row ) {
+				$data .= '<tr>';
+				$data .= '<td>'.$row['cedula'].'</td>';
+				$data .= '<td>'.$row['pnombre'].'</td>';
+				$data .= '<td>'.$row['snombre'].'</td>';
+				$data .= '<td>'.$row['papellido'].'</td>';
+				$data .= '<td>'.$row['sapellido'].'</td>';
+				$data .= '<td>'.$row['fecha'].'</td>';
+				$data .= '<td>'.$row['sexo'].'</td>';
+				$data .= '</tr>';
+			}
+			$data .= '</table>';
+		}
+		
+		echo $data;
+		//return true;
+
+	}
+
+
+}
