489698c0fe816bbe2f85bcd5961dba5df2b3a58c
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2014-09-29 15:52:51 +0000

    -Mejoras en sfac

--- trunk/proteoerp/system/application/rapyd/fields/dropdown.php
+++ trunk/proteoerp/system/application/rapyd/fields/dropdown.php
@@ -104,7 +104,7 @@ class dropdownField extends objField{
 					$output.='<span id=\''.$this->name."_val'  >$this->description</span>";
 				}else{
 					$title  = ' title="'.$this->title.'"';
-					$output = form_dropdown($this->name, $this->options, $this->value, $id.$onchange.$style.$class.$title.$multiple). $this->extra_output;
+					$output = $this->form_dropdown($this->name, $this->options, $this->value, $id.$onchange.$style.$class.$title.$multiple). $this->extra_output;
 				}
 
 				break;
@@ -116,4 +116,49 @@ class dropdownField extends objField{
 		}
 		$this->output = $output;
 	}
+
+	function form_dropdown($name = '', $options = array(), $selected = array(), $extra = ''){
+		if (!is_array($selected)){
+			$selected = array(trim($selected));
+		}
+
+		// If no selected state was submitted we will attempt to set it automatically
+		if (count($selected) === 0){
+			// If the form name appears in the $_POST array we have a winner!
+			if (isset($_POST[$name])){
+				$selected = array($_POST[$name]);
+			}
+		}
+
+		if ($extra != '') $extra = ' '.$extra;
+
+		$multiple = (count($selected) > 1 && strpos($extra, 'multiple') === FALSE) ? ' multiple="multiple"' : '';
+
+		$form = '<select name="'.$name.'"'.$extra.$multiple.">\n";
+
+		foreach ($options as $key => $val){
+			$key = (string) $key;
+
+			if (is_array($val)){
+				$form .= '<optgroup label="'.$key.'">'."\n";
+
+				foreach ($val as $optgroup_key => $optgroup_val){
+					$sel = (in_array($optgroup_key, $selected)) ? ' selected="selected"' : '';
+
+					$form .= '<option value="'.$optgroup_key.'"'.$sel.'>'.(string) $optgroup_val."</option>\n";
+				}
+
+				$form .= '</optgroup>'."\n";
+			}else{
+				$sel = (in_array(trim($key), $selected,true)) ? ' selected="selected"' : '';
+
+				$form .= '<option value="'.$key.'"'.$sel.'>'.(string) $val."</option>\n";
+			}
+		}
+
+		$form .= '</select>';
+
+		return $form;
+	}
+
 }
