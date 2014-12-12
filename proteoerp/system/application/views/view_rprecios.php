c88aa8e79c331b2d584f6fc444243114822508a7
datasql@gmail.com <datasql@gmail.com@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2014-12-12 00:02:27 +0000

    Consultas

--- proteoerp/system/application/views/view_rprecios.php
+++ proteoerp/system/application/views/view_rprecios.php
@@ -14,14 +14,14 @@
 	<tr>
 		<td width='100%' >
 			<table width='100%' border='0'>
-				<tr><td align='center'><b STYLE="font-size:24pt;color:blue">Precio al P&uacute;blico: <?php echo $precio1 ?></b> <b><?php echo $moneda  ?></b></td></tr>
+				<tr><td align='center'><b STYLE="font-size:24pt;color:blue">Precio de Venta Justo: <?php echo round($precio2*100/(100+$iva),2); ?></b> <b><?php echo $moneda  ?></b></td></tr>
 				<?php $descufijo=(isset($descufijo)?$descufijo:0);
 				if($descufijo>0){?>
 				<tr><td align='center'><b STYLE="font-size:24pt;color:red">Descuento <?php echo $descufijo; ?>%:</b> &nbsp;<b STYLE="font-size:30pt;color:red"><?php echo $pdescu; ?></b><b STYLE="font-size:16pt;color:red"> <?php echo $moneda;?></b></td></tr>
 				<tr><td align='center'><b STYLE="font-size:8pt;color:red"> <?php echo $descurazon;?></td></tr>
 				<?php }else{ ?>
-				<tr><td align='center'><b STYLE="font-size:18pt;color:black">I.V.A.: <?php echo  $precio2 - round($precio2*100/(100+$iva),2); ?><b>Bs.F.</b> Precio Base : <?php echo round($precio2*100/(100+$iva),2); ?></b><b><?php echo $moneda;  ?></b></td></tr>
-				<tr><td align='center'><b STYLE="font-size:32pt;color:red">Precio de venta : <?php echo $precio2; ?></b><b><?php echo $moneda;  ?></b></td></tr>
+				<tr><td align='center'><b STYLE="font-size:18pt;color:black">I.V.A.: <?php echo  $precio2 - round($precio2*100/(100+$iva),2); ?></b><b><?php echo $moneda;  ?></b></td></tr>
+				<tr><td align='center'><b STYLE="font-size:32pt;color:red">Precio con IVA : <?php echo $precio2; ?></b><b><?php echo $moneda;  ?></b></td></tr>
 				<?php } ?>
 				<?php if(isset($dvolum1) && $dvolum1>0){ ?>
 				<tr><td align='right' colspan=2 ><b STYLE="font-size:20pt;color:green">+ de <?php echo $dvolum1 ?> unidades: <?php echo $precio3 ?></b> <b><?php echo $moneda  ?></b></td></tr>
@@ -43,8 +43,8 @@
 					<td style="font-size:14pt;"><b>Referencia:</b> <?php echo $referen ?></td>
 				</tr>
 				<tr>
-					<td style="font-size:18pt;" align='left'><b>Existencia:</b> <?php echo $existen ?></td>
-					<td style="font-size:14pt;" align='left'><b>Fecha:</b> <?php echo dbdate_to_human($fecha) ?></td>
+					<td style="font-size:18pt;" align='left'><!--b>Existencia:</b> <?php echo $existen ?>--></td>
+					<td style="font-size:14pt;" align='left'><b>Fecha:</b> <?php echo substr(dbdate_to_human($fecha),3,8); ?></td>
 				</tr>
 			</table>
 		</td>
