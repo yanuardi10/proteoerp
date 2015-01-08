6623b5a3e666a9c8bc8d5d337ad9abcaf1d4bca1
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2015-01-08 20:44:17 +0000

    -Mejoras

--- proteoerp/system/application/controllers/supermercado/actlocali.php
+++ proteoerp/system/application/controllers/supermercado/actlocali.php
@@ -19,15 +19,24 @@ class actlocali extends Controller {
 	}
 
 	function index(){
-		$salida =anchor($this->url.'/locali','Modificar Localizaciones');
-		$salida.='</br>';
-		$salida.=anchor($this->url.'/mfisicocero','Colocar Productos no contados en Cero(0)');
+		$this->instalar();
+		$data['content'] = '<p style="text-align:center;"><div style="margin-left: auto;margin-right: auto;" id="maso" >';
 
-		$data['content'] = $salida;
-		$data['title']   = '<h1>Menu</h1>';
-		$data['script']  = '';
+		$data['content'].= '<div class="box" style="width:230px;height:255px;background-color: #F9F7F9;"><h3>Colocar Productos no contados en Cero(0)</h3>'.br();
+		$data['content'].= '<a href="'.site_url($this->url.'/mfisicocero').'"><img border=0 src="'.base_url().'images/despachoexp1.png'.'"></a>'.br();
+		$data['content'].= '<p>Seleccione esta opci&oacute;n para colocar en cero los productos no contados en un inventario f&iacute;sico</p>.'.br();
+		$data['content'].= '</div>';
+
+		$data['content'].= '<div class="box" style="width:230px;height:255px;background-color: #F9F7F9;"><h3>Modificar Localizaciones</h3>'.br();
+		$data['content'].= '<a href="'.site_url($this->url.'/locali').'"><img border=0 src="'.base_url().'images/despachoexp2.png'.'"></a>'.br();
+		$data['content'].= '<p>Seleccione esta opci&oacute;n para hacer cambiar en masa las localizaciones de los productos en un inventario f&iacute;sico.</p>'.br();
+		$data['content'].= '</div>';
+
+		$data['content'].= '</div></p>';
+
+		$data['title']   = heading('Opciones de inventario para supermercado');
 		$data['head']    = $this->rapyd->get_head();
-		$this->load->view('view_ventanas', $data);
+		$this->load->view('view_ventanas_masonry', $data);
 	}
 
 	function locali(){
@@ -41,22 +50,23 @@ class actlocali extends Controller {
 		$form->numero->minlength = 8;*/
 
 		$form->numero = new dropdownField('Inventario F&iacute;sico', 'numero');
-		$form->numero->style='width:230px;';
+		$form->numero->style='width:300px;';
 		$form->numero->option('','Seleccionar');
 		$form->numero->rule ='callback_chinvfis|required';
 		$form->numero->options('SELECT numero,CONCAT_WS(\'-\',numero,ubica,DATE_FORMAT(fecha,\'%d/%m/%Y\')) AS val FROM maesfisico  GROUP BY numero ORDER BY fecha DESC LIMIT '.$this->limite);
 		$form->numero->append('N&uacute;mero-Almac&eacute;n-Fecha');
 
-		$form->locali = new inputField('Localizaci&oacute;n a colocar', 'locali');
-		$form->locali->size = 10;
-		$form->locali->rule = 'required';
-
 		$form->oper = new dropdownField('Tomar en cuenta ','oper');
+		$form->oper->style='width:300px;';
 		$form->oper->option('' ,'Seleccionar');
 		$form->oper->option('2','Todos');
 		$form->oper->option('1','Solo los que fraccion sea mayor a cero (0)');
 		$form->oper->option('0','Solo los que fraccion son igual a cero (0)');
-		$form->open->rule = 'required|enum[0,1,2]';
+		$form->oper->rule = 'required|enum[0,1,2]';
+
+		$form->locali = new inputField('Localizaci&oacute;n a colocar', 'locali');
+		$form->locali->size = 10;
+		$form->locali->rule = 'required';
 
 		$back_url=site_url($this->url.'/index');
 		$form->button('btn_regre','Regresar',"javascript:window.location='${back_url}'",'BL');
@@ -101,7 +111,7 @@ class actlocali extends Controller {
 		$form = new DataForm($this->url.'/mfisicocero/process');
 
 		$form->numero = new dropdownField('Inventario F&iacute;sico', 'numero');
-		$form->numero->style='width:230px;';
+		$form->numero->style='width:300px;';
 		$form->numero->option('','Seleccionar');
 		$form->numero->rule ='callback_chinvfis|required';
 		$form->numero->options('SELECT numero,CONCAT_WS(\'-\',numero,ubica,DATE_FORMAT(fecha,\'%d/%m/%Y\')) AS val FROM maesfisico  GROUP BY numero ORDER BY fecha DESC LIMIT '.$this->limite);
@@ -130,7 +140,7 @@ class actlocali extends Controller {
 				SELECT
 					NULL AS id,
 					a.codigo,
-					${ubica}    AS ubica,
+					${dbubica}  AS ubica,
 					'BLANCO'    AS locali,
 					0           AS cantidad,
 					0           AS fraccion,
@@ -173,5 +183,33 @@ class actlocali extends Controller {
 		}
 		$this->validation->set_message('chinvfis', "No existe el numero de inventario indicado ${numero}");
 		return false;
+	}
+
+	function instalar(){
+		if(!$this->db->table_exists('maesfisico')){
+			$mSQL="CREATE TABLE `maesfisico` (
+				`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+				`codigo` VARCHAR(15) NULL DEFAULT NULL,
+				`ubica` VARCHAR(4) NULL DEFAULT NULL,
+				`locali` VARCHAR(5) NULL DEFAULT NULL,
+				`cantidad` INT(11) NOT NULL DEFAULT '0',
+				`fraccion` INT(11) NOT NULL DEFAULT '0',
+				`acantidad` INT(11) NOT NULL DEFAULT '0',
+				`afraccion` INT(11) NOT NULL DEFAULT '0',
+				`fecha` DATE NULL DEFAULT NULL,
+				`numero` VARCHAR(8) NULL DEFAULT NULL,
+				`usuario` VARCHAR(12) NULL DEFAULT NULL,
+				`estanpa` DATE NULL DEFAULT NULL,
+				`hora` VARCHAR(8) NULL DEFAULT NULL,
+				PRIMARY KEY (`id`),
+				INDEX `codigo` (`codigo`, `ubica`),
+				INDEX `ubic` (`ubica`, `codigo`),
+				INDEX `fecha` (`fecha`, `codigo`)
+			)
+			COLLATE='latin1_swedish_ci'
+			ENGINE=MyISAM
+			AUTO_INCREMENT=1";
+			$this->db->simple_query($mSQL);
+		}
 	}
 }
