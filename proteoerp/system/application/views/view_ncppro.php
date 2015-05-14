3e2a4a019fe2be38a92b9e426e00c3de19dcb4e3
aahahocevar <aahahocevar@49c27cf6-c7e1-c0a6-9d64-e0059e9774a1>
2015-05-14 03:55:22 +0000

    -Avances nota de credito a factura pagada

--- proteoerp/system/application/views/view_ncppro.php
+++ proteoerp/system/application/views/view_ncppro.php
@@ -23,6 +23,7 @@ if($form->getstatus()!='show'){
 ?>
 <script type="text/javascript">
 var objptasa=<?php echo $json_ptasa; ?>;
+var retepasa=true;
 
 $(function(){
 	$(".inputnum").numeric(".");
@@ -35,55 +36,55 @@ $(function(){
 	$('#fecha').datepicker({   dateFormat: "dd/mm/yy" });
 	$('#posdata').datepicker({ dateFormat: "dd/mm/yy" });
 
-	$('#montasa'  ).focus(function (){ invasdif('montasa','tasa'     ,Number($("#ptasa"     ).val())/100); });
-	$('#monredu'  ).focus(function (){ invasdif('monredu','reducida' ,Number($("#preducida" ).val())/100); });
-	$('#monadic'  ).focus(function (){ invasdif('monadic','sobretasa',Number($("#padicional").val())/100); });
-	$('#tasa'     ).focus(function (){ invasdif('montasa','tasa'     ,Number($("#ptasa"     ).val())/100); });
-	$('#reducida' ).focus(function (){ invasdif('monredu','reducida' ,Number($("#preducida" ).val())/100); });
-	$('#sobretasa').focus(function (){ invasdif('monadic','sobretasa',Number($("#padicional").val())/100); });
-	$('#exento'   ).focus(function (){ invasdif('exento' ,'E'        ,0);     });
+	$('#montasa'  ).focus(function (){ totaliza('montasa','tasa'     ,Number($("#ptasa"     ).val())/100); });
+	$('#monredu'  ).focus(function (){ totaliza('monredu','reducida' ,Number($("#preducida" ).val())/100); });
+	$('#monadic'  ).focus(function (){ totaliza('monadic','sobretasa',Number($("#padicional").val())/100); });
+	$('#tasa'     ).focus(function (){ totaliza('montasa','tasa'     ,Number($("#ptasa"     ).val())/100); });
+	$('#reducida' ).focus(function (){ totaliza('monredu','reducida' ,Number($("#preducida" ).val())/100); });
+	$('#sobretasa').focus(function (){ totaliza('monadic','sobretasa',Number($("#padicional").val())/100); });
+	$('#exento'   ).focus(function (){ totaliza('exento' ,'E'        ,0);     });
 
 
 	$('#montasa').keyup(function (){
 		var ptasa = Number($("#ptasa").val())/100;
 		var base  = Number($('#montasa').val());
 		$('#tasa').val(roundNumber(base*ptasa,2));
-		calretiva();
+		totaliza();
 	});
 
 	$('#monredu').keyup(function (){
 		var ptasa = Number($("#preducida").val())/100;
 		var base  = Number($('#monredu').val());
 		$('#reducida').val(roundNumber(base*ptasa,2));
-		calretiva();
+		totaliza();
 	});
 
 	$('#monadic').keyup(function (){
 		var ptasa = Number($("#padicional").val())/100;
 		var base  = Number($('#monadic').val());
 		$('#sobretasa').val(roundNumber(base*ptasa,2));
-		calretiva();
+		totaliza();
 	});
 
 	$('#tasa').keyup(function (){
 		var ptasa    = Number($("#ptasa").val())/100;
 		var impuesto = Number($('#tasa').val());
 		$('#montasa').val(roundNumber(impuesto*ptasa,2));
-		calretiva();
+		totaliza();
 	});
 
 	$('#reducida').keyup(function (){
 		var ptasa    = Number($("#preducida").val())/100;
 		var impuesto = Number($('#reducida').val());
 		$('#monredu').val(roundNumber(impuesto*ptasa,2));
-		calretiva();
+		totaliza();
 	});
 
 	$('#sobretasa').keyup(function (){
 		var ptasa    = Number($("#padiciona").val())/100;
 		var impuesto = Number($('#sobretasa').val());
 		$('#monadic').val(roundNumber(impuesto*ptasa,2));
-		calretiva();
+		totaliza();
 	});
 
 	$('#cod_prv').autocomplete({
@@ -120,7 +121,7 @@ $(function(){
 			$('#nombre').val(ui.item.nombre);
 			$('#nombre_val').text(ui.item.nombre);
 			$('#cod_prv').val(ui.item.proveed);
-			//$('#sprvreteiva').val(ui.item.reteiva);
+			$('#sprvreteiva').val(ui.item.reteiva);
 			setTimeout(function(){ $('#cod_prv').removeAttr('readonly'); }, 1500);
 		}
 	});
@@ -174,6 +175,24 @@ $(function(){
 			$('#reducida' ).val(ui.item.reducida );
 			$('#sobretasa').val(ui.item.sobretasa);
 
+			if(ui.item.aplrete=='N'){
+				$('#reteiva').css('background-color', '#FFFF28');
+				$('#reteiva').attr('title', 'No se le realizo retención');
+				$('#reteiva').val('0');
+				retepasa=false;
+			}else if(ui.item.aplrete=='V'){
+				$('#reteiva').css('background-color', '#FFCF62');
+				$('#reteiva').attr('title', 'Período vencido para devolver retención');
+				$('#reteiva').val('0');
+				retepasa=false;
+			}else{
+				$('#reteiva').css('background-color', '#FFFFFF');
+				$('#reteiva').attr('title', '');
+				$('#reteiva').val(ui.item.reteiva);
+				retepasa=true;
+			}
+
+			totaliza();
 			setTimeout(function(){ $('#afecta').removeAttr('readonly'); }, 1500);
 		}
 	});
@@ -181,117 +200,27 @@ $(function(){
 	totaliza();
 });
 
-function marcariva(){
-	var arr  = $('input[name^="riva_"]');
-	jQuery.each(arr, function(){
-		nom=this.name;
-		pos=this.name.lastIndexOf('_');
-		if(pos>0){
-			ind   = this.name.substring(pos+1);
-			val   = this.value;
-			if(val=='N'){
-				$('#tr_itccli_'+ind).css("background-color", "#FFFF28");
-				$('#tr_itccli_'+ind).attr("title", 'No se le realizo retención');
-			}else if(val=='V'){
-				$('#tr_itccli_'+ind).css("background-color", "#FFCF62");
-				$('#tr_itccli_'+ind).attr("title", 'Período vencido para devolver retención');
-			}else{
-				$('#tr_itccli_'+ind).css("background-color",  'transparent');
-			}
-		}
-	});
-}
-
-function desmarcariva(){
-	var arr  = $('input[name^="riva_"]');
-	jQuery.each(arr, function(){
-		nom=this.name;
-		pos=this.name.lastIndexOf('_');
-		if(pos>0){
-			ind   = this.name.substring(pos+1);
-			$('#tr_itccli_'+ind).css("background-color",  'transparent');
-			$('#tr_itccli_'+ind).removeAttr('title');
-		}
-	});
-}
-
 function calretiva(){
-	var montasa   = Number($('#montasa'  ).val());
-	var monredu   = Number($('#monredu'  ).val());
-	var monadic   = Number($('#monadic'  ).val());
-	var tasa      = Number($('#tasa'     ).val());
-	var reducida  = Number($('#reducida' ).val());
-	var sobretasa = Number($('#sobretasa').val());
-	var noret     = 0;
-	var tasatot   = 0;
+	var reiva=0;
+	if(retepasa){
+		var montasa   = Number($('#montasa'  ).val());
+		var monredu   = Number($('#monredu'  ).val());
+		var monadic   = Number($('#monadic'  ).val());
+		var tasa      = Number($('#tasa'     ).val());
+		var reducida  = Number($('#reducida' ).val());
+		var sobretasa = Number($('#sobretasa').val());
+		var preteiva  = Number($('#sprvreteiva').val());
 
-	var arr  = $('input[name^="riva_"]');
-	jQuery.each(arr, function(){
-		nom=this.name;
-		pos=this.name.lastIndexOf('_');
-		if(pos>0){
-			ind  = this.name.substring(pos+1);
-			val  = this.value;
-			i    = parseInt(ind);
-			num  = Number($('#abono_'+ind).val());
-			monto= Number($('#monto_'+ind).val());
-
-			if(val=='N' || val=='V'){
-				//noret = noret+num*objivas[i].tasa     /monto;
-				//noret = noret+num*objivas[i].reducida /monto;
-				//noret = noret+num*objivas[i].sobretasa/monto;
-			}
-		}
-	});
-
-	if(noret>=tasa+reducida+sobretasa){
-		tasatot=0;
+		reiva=roundNumber((tasa+reducida+sobretasa)*(preteiva/100),2);
 	}else{
-		tasatot=tasa+reducida+sobretasa-noret;
+		reiva=0;
 	}
-
-	//$('#reteiva').val(roundNumber(tasatot*1,2));
-}
-
-function invasdif(base,iva,ptasa){
-	var total     = Number($('#monto').val());
-	var exento    = Number($('#exento').val());
-	var sobretasa = Number($('#sobretasa').val());
-	var reducida  = Number($('#reducida').val());
-	var tasa      = Number($('#tasa').val());
-	var monadic   = Number($('#monadic').val());
-	var monredu   = Number($('#monredu').val());
-	var montasa   = Number($('#montasa').val());
-	var basactual = Number($('#'+base).val());
-	var ivaactual = Number($('#'+iva).val());
-	if(isNaN(exento   )) exento   =0;
-	if(isNaN(sobretasa)) sobretasa=0;
-	if(isNaN(reducida )) reducida =0;
-	if(isNaN(tasa     )) tasa     =0;
-	if(isNaN(monadic  )) monadic  =0;
-	if(isNaN(monredu  )) monredu  =0;
-	if(isNaN(montasa  )) montasa  =0;
-	if(isNaN(ptasa    )) ptasa    =0;
-	if(isNaN(basactual)) basactual=0;
-	if(isNaN(ivaactual)) ivaactual=0;
-
-	var itota = sobretasa+exento+reducida+tasa+monadic+monredu+montasa-basactual-ivaactual;
-	var diff  = total-itota;
-
-	var bbase = diff/(1+ptasa);
-	var iiva  = bbase*ptasa;
-
-	$('#'+base).val(roundNumber(bbase,2));
-	if(iva!='E'){
-		$('#'+iva).val(roundNumber(iiva,2));
-	}
-
-	calretiva();
+	$('#reteiva').val(reiva);
 }
 
 function totaliza(){
 	var stota =0;
-	var mascara= "PAGA "+$('#afecta').val();
+	var mascara= "APLICA A"+$('#afecta').val();
 
 	var actualmontasa  = Number($('#montasa'  ).val());
 	var actualmonredu  = Number($('#monredu'  ).val());
@@ -309,6 +238,7 @@ function totaliza(){
 	}else{
 		$("#observa1").val('');
 	}
+	 calretiva()
 }
 
 function chapltasa(){
@@ -346,7 +276,7 @@ echo $title;
 ?>
 <table align='center' width="100%">
 	<tr>
-		<td><?php echo $form->cod_prv->label.'*'.$form->cod_prv->output.$form->nombre->output; ?></td>
+		<td><?php echo $form->cod_prv->label.'*'.$form->cod_prv->output.$form->nombre->output.$form->sprvreteiva->output; ?></td>
 	</tr>
 </table>
 
